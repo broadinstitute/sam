66d649174d1d60253703669d795519953ae5efe3
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-07-17 12:54:38 -0400

    link test container to opendj container

--- docker/build.sh
+++ docker/build.sh
@@ -23,7 +23,7 @@ function make_jar()
     GIT_MODEL_HASH=$(git log -n 1 --pretty=format:%h)
     
     # make jar.  cache sbt dependencies.
-    docker run --rm -e DIRECTORY_URL=$DIRECTORY_URL -e GIT_MODEL_HASH=$GIT_MODEL_HASH -e DIRECTORY_PASSWORD=$DIRECTORY_PASSWORD -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 broadinstitute/scala-baseimage /working/docker/install.sh /working
+    docker run --rm --link opendj:opendj -e DIRECTORY_URL=$DIRECTORY_URL -e GIT_MODEL_HASH=$GIT_MODEL_HASH -e DIRECTORY_PASSWORD=$DIRECTORY_PASSWORD -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 broadinstitute/scala-baseimage /working/docker/install.sh /working
 }
 
 function docker_cmd()
