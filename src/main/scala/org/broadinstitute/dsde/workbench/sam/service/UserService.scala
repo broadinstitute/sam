9f7604a5979680f6d0012712d778079d880fc9fa
Matt Bemis <mbemis@broadinstitute.org>
2017-09-15 19:16:19 -0400

    revert testing

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/service/UserService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/service/UserService.scala
@@ -21,12 +21,12 @@ class UserService(val directoryDAO: DirectoryDAO, val googleDirectoryDAO: Google
   def createUser(user: SamUser): Future[Option[SamUserStatus]] = {
     for {
       _ <- directoryDAO.createUser(user)
-//      _ <- googleDirectoryDAO.createGroup(WorkbenchGroupName(user.email.value), WorkbenchGroupEmail(toProxyFromUser(user.id.value)))
-//      _ <- googleDirectoryDAO.addMemberToGroup(WorkbenchGroupEmail(toProxyFromUser(user.id.value)), WorkbenchUserEmail(user.email.value))
+      _ <- googleDirectoryDAO.createGroup(WorkbenchGroupName(user.email.value), WorkbenchGroupEmail(toProxyFromUser(user.id.value)))
+      _ <- googleDirectoryDAO.addMemberToGroup(WorkbenchGroupEmail(toProxyFromUser(user.id.value)), WorkbenchUserEmail(user.email.value))
       _ <- directoryDAO.enableUser(user.id)
       _ <- createAllUsersGroup
-//      _ <- directoryDAO.addGroupMember(allUsersGroupName, user.id)
-//      _ <- googleDirectoryDAO.addMemberToGroup(WorkbenchGroupEmail(toGoogleGroupName(allUsersGroupName.value)), WorkbenchUserEmail(toProxyFromUser(user.id.value))) //TODO: For now, do a manual add to the All_Users Google group (undo this in Phase II)
+      _ <- directoryDAO.addGroupMember(allUsersGroupName, user.id)
+      _ <- googleDirectoryDAO.addMemberToGroup(WorkbenchGroupEmail(toGoogleGroupName(allUsersGroupName.value)), WorkbenchUserEmail(toProxyFromUser(user.id.value))) //TODO: For now, do a manual add to the All_Users Google group (undo this in Phase II)
       userStatus <- getUserStatus(user)
     } yield {
       userStatus
