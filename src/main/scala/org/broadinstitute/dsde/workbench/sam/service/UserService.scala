0bbbb16129aa150f3b8a2d57f6a1762f5b0f56c8
Rob Title <rtitle@broadinstitute.org>
2017-10-25 14:39:38 -0400

    TEMPORARY: disable pet sa cleanup to debug some Google groups issue

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/service/UserService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/service/UserService.scala
@@ -99,9 +99,10 @@ class UserService(val directoryDAO: DirectoryDAO, val googleDirectoryDAO: Google
           // Set up the service account with the necessary permissions
           setUpServiceAccount(user, petServiceAccount) andThen { case Failure(_) =>
             // If anything fails with setup, clean up any created resources to ensure we don't end up with orphaned pets.
-            removePetServiceAccount(user, petServiceAccount).failed.foreach { e =>
-              logger.warn(s"Error occurred cleaning up pet service account [$petSa] [$petSaDisplayName]", e)
-            }
+            // TODO temporarily disabling cleanup to debug
+//            removePetServiceAccount(user, petServiceAccount).failed.foreach { e =>
+//              logger.warn(s"Error occurred cleaning up pet service account [$petSa] [$petSaDisplayName]", e)
+//            }
         }
       }
     }
