1a68a00e44db8772f686f56e695b30c2bee7700d
Rob Title <rtitle@broadinstitute.org>
2017-10-25 17:59:31 -0400

    Update wb-libs again for fix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,9 +7,9 @@ object Dependencies {
   val scalaLoggingV = "3.5.0"
   val scalaTestV    = "3.0.1"
 
-  val workbenchUtilV   = "0.2-204a9fe-SNAP"
-  val workbenchModelV  = "0.3-204a9fe-SNAP"
-  val workbenchGoogleV = "0.4-204a9fe-SNAP"
+  val workbenchUtilV   = "0.2-ea2355f-SNAP"
+  val workbenchModelV  = "0.3-ea2355f-SNAP"
+  val workbenchGoogleV = "0.4-ea2355f-SNAP"
 
   val excludeAkkaActor =        ExclusionRule(organization = "com.typesafe.akka", name = "akka-actor_2.12")
   val excludeWorkbenchUtil =    ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-util_2.12")
