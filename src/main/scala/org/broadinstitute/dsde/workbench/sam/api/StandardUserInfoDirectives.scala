b065f47c24d7fbca3dd13d98b480d63362a3a6a4
Rob Title <rtitle@broadinstitute.org>
2017-11-06 16:22:30 -0500

    Add debug logging to switcheroo

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/StandardUserInfoDirectives.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/StandardUserInfoDirectives.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives.{headerValueByName, onSuccess}
 import org.broadinstitute.dsde.workbench.model._
 import org.broadinstitute.dsde.workbench.sam.model.UserInfo
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -11,6 +12,7 @@ import scala.concurrent.Future
 
 
 trait StandardUserInfoDirectives extends UserInfoDirectives {
+  private final val logger = LoggerFactory.getLogger(classOf[StandardUserInfoDirectives])
   val petSAdomain = "\\S+@\\S+.iam.gserviceaccount.com"
 
   def requireUserInfo: Directive1[UserInfo] = (
@@ -31,9 +33,13 @@ trait StandardUserInfoDirectives extends UserInfoDirectives {
   private def isPetSA(email:String) = email.matches(petSAdomain)
 
   private def getWorkbenchUserEmailId(email:String):Future[Option[WorkbenchUser]] = {
-    if (isPetSA(email))
-      directoryDAO.getUserFromPetServiceAccount(WorkbenchUserServiceAccountEmail(email))
-      else
-      Future(None)
+    if (isPetSA(email)) {
+      logger.info(s"Email $email is a pet service account. Looking up user...")
+      val future  = directoryDAO.getUserFromPetServiceAccount(WorkbenchUserServiceAccountEmail(email))
+      future.recover { case e =>
+        logger.error(s"Error occurred looking up user from pet service account $email", e)
+        throw e
+      }
+    } else Future(None)
   }
 }
