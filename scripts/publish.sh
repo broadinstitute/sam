8672aae22176b4ba058dc8aeadefbbb3c7155439
ansingh7115 <singh.an@husky.neu.edu>
2017-12-05 16:05:29 -0500

    adding script to generate Java API Client, probs doesn't work

---
+++ scripts/publish.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+timestamp() {
+  date +"%T"
+}
+
+function publish ()
+{
+    echo "getting swagger codegen jar..."
+    wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.3/swagger-codegen-cli-2.2.3.jar -O swagger-codegen-cli.jar
+
+    echo "generating Sam Java Client API..."
+    swagger-codegen generate -l java --input-spec ./src/main/resources/swagger/api-docs.yaml --output generated
+
+    echo "adding plugins file..."
+    cd generated
+    echo "addSbtPlugin(\"com.eed3si9n\" % \"sbt-assembly\" % \"0.14.3\")" > /project/plugins.sbt
+
+    echo "compiling Java Client API..."
+    sbt assembly
+    sbt compile
+
+    echo "pushing to Artifactory..."
+    cd ..
+#    curl -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-snapshot-build.timestamp=" + timestamp -T Desktop/myNewFile.txt
+    curl -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-snapshot-local/org/broadinstitute/sam" -T generated
+}
+
+publish
+# sbt publish publishes libs to Artifactory for the scala version sbt is running as.
+# sbt +publish publishes libs to Artifactory for all scala versions listed in crossScalaVersions.
+# We only do sbt publish here because Travis runs against 2.11 and 2.12 in separate jobs, so each one publishes its version to Artifactory.
+#if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
+#if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
+#	sbt ++$TRAVIS_SCALA_VERSION publish -Dproject.isSnapshot=false
+#else
+#	sbt ++$TRAVIS_SCALA_VERSION publish -Dproject.isSnapshot=true
+#fi
