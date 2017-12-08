95819b2dddd4902139ce82fef6a4ce8256d18438
ansingh7115 <singh.an@husky.neu.edu>
2017-12-08 03:14:56 -0500

    make it better

--- swagger-api-client/publish.sh
+++ swagger-api-client/publish.sh
@@ -16,7 +16,6 @@ function publish ()
     COMMIT_HASH="${TRAVIS_COMMIT:0:7}"
     VERSION_HASH="1.0-$COMMIT_HASH"
     ORGANIZATION="org.broadinstitute.dsde"
-
     APP_NAME="sam"
     SCALA_11_VERSION="2.11.8"
     SCALA_12_VERSION="2.12.3"
@@ -25,7 +24,6 @@ function publish ()
     rm generated/build.sbt
     cp -f swagger-client-build.txt generated/build.sbt
     cd generated
-
     sed -i "s|\$version|$VERSION_HASH|g" build.sbt
     sed -i "s|\$organization|$ORGANIZATION|g" build.sbt
     sed -i "s|\$name|$APP_NAME|g" build.sbt
@@ -38,35 +36,30 @@ function publish ()
     echo "compiling Java Client API..."
     sbt compile assembly +publish
 
-    echo "what's in publish/org/broadinstitute/dsde/sam_2.11/version"
-    ls publish/org/broadinstitute/dsde/sam_2.11/$VERSION_HASH
-    echo "what's in publish/org/broadinstitute/dsde/sam_2.12/version"
-    ls publish/org/broadinstitute/dsde/sam_2.12/$VERSION_HASH
-
     find publish/org/broadinstitute/dsde/sam_2.11/$VERSION_HASH -name "*.md5" -type f -delete
     find publish/org/broadinstitute/dsde/sam_2.11/$VERSION_HASH -name "*.sha1" -type f -delete
     find publish/org/broadinstitute/dsde/sam_2.12/$VERSION_HASH -name "*.md5" -type f -delete
     find publish/org/broadinstitute/dsde/sam_2.12/$VERSION_HASH -name "*.sha1" -type f -delete
-
-    echo "what's in publish/org/broadinstitute/dsde/sam_2.11/version post removal"
-    ls publish/org/broadinstitute/dsde/sam_2.11/$VERSION_HASH
-    echo "what's in publish/org/broadinstitute/dsde/sam_2.12/version post removal"
-    ls publish/org/broadinstitute/dsde/sam_2.12/$VERSION_HASH
 
     echo "pushing to Artifactory..."
 
     chmod a+wx publish
 
     declare -a SCALA_VERSIONS_ARRAY=("2.11" "2.12")
+    declare -a SUFFIX_ARRAY=(".jar" ".pom" "-javadoc.jar" "-sources.jar")
 
     for scala_version in $SCALA_VERSIONS_ARRAY
-    do
+      do
         ARTIFACTORY_PATH="org/broadinstitute/dsde/${APP_NAME}_$scala_version/$VERSION_HASH/${APP_NAME}_$scala_version-$VERSION_HASH"
-        curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH.jar" -T publish/$ARTIFACTORY_PATH.jar
-        curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH.pom" -T publish/$ARTIFACTORY_PATH.pom
-        curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH-javadoc.jar" -T publish/$ARTIFACTORY_PATH-javadoc.jar
-        curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH-sources.jar" -T publish/$ARTIFACTORY_PATH-sources.jar
-    done
+        for suffix in $SUFFIX_ARRAY
+          do
+            curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH$suffix" -T publish/$ARTIFACTORY_PATH$suffix
+#            curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH.jar" -T publish/$ARTIFACTORY_PATH.jar
+#            curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH.pom" -T publish/$ARTIFACTORY_PATH.pom
+#            curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH-javadoc.jar" -T publish/$ARTIFACTORY_PATH-javadoc.jar
+#            curl -u $ARTIF_USER:$ARTIF_PASSWORD -X PUT "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local/$ARTIFACTORY_PATH-sources.jar" -T publish/$ARTIFACTORY_PATH-sources.jar
+          done
+      done
 
 }
 
