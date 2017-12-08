818149be50fe146d94ad893792d7f7e6e6fac10c
ansingh7115 <singh.an@husky.neu.edu>
2017-12-08 00:14:48 -0500

    throwing things at the wall

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -16,16 +16,42 @@ function publish ()
     echo "generating Sam Java Client API..."
     java -jar swagger-codegen-cli.jar generate -l java --input-spec ./src/main/resources/swagger/api-docs.yaml --output generated
 
+    COMMIT_HASH="${TRAVIS_COMMIT:0:7}"
+    echo "Commit hash:$COMMIT_HASH"
+
+    VERSION_HASH="1.0-$COMMIT_HASH"
+    ORGANIZATION="org.broadinstitute.dsde"
+    APP_NAME="sam"
+    PATH="org/broadinstitute/dsde/sam_2.11/1.0.0/swagger-java-client_2.11-1.0.0"
+
+    SCALA_11_VERSION="2.11.8"
+    SCALA_12_VERSION="2.12.3"
+    SCALA_VERSIONS="List(\"$SCALA_11_VERSION\", \"$SCALA_12_VERSION\")"
+
+    sed -i "s|\$version|$VERSION_HASH|g" sam/swagger-client-build.txt
+    sed -i "s|\$organization|$ORGANIZATION|g" sam/swagger-client-build.txt
+    sed -i "s|\$name|$APP_NAME|g" sam/swagger-client-build.txt
+    sed -i "s|\$scalaVersions|$SCALA_VERSIONS|g" sam/swagger-client-build.txt
+
+
     echo "adding plugins file..."
     cd generated
     mkdir project
-    echo "addSbtPlugin(\"com.eed3si9n\" % \"sbt-assembly\" % \"0.14.3\")" > project/plugins.sbt
-    sed -i 's/publishArtifact in (Compile, packageDoc) := false/publishTo := Some(Resolver.file(\"file\", new File(\"publish\")))/g' build.sbt
+#    echo "addSbtPlugin(\"com.eed3si9n\" % \"sbt-assembly\" % \"0.14.3\")" > project/plugins.sbt
+#    sed -i 's/$version/$/g' build.sbt
+
+
+
+#    sed -i '/organization := /c\    organization := \"org.broadinstitute.dsde\",' build.sbt
+#    sed 's/.*organization := */organization := \"org.broadinstitute.dsde\",/'
+#    sed 's/.*TEXT_TO_BE_REPLACED.*/This line is removed by the admin./'
+#    sed -i '/name := /c\    name := \"sam\",' build.sbt
+#    sed -i '/version := /c\    version := \"1.0.0\",' build.sbt
+#    echo "print build.sbt"
+#    cat build.sbt
 
     echo "compiling Java Client API..."
-    sbt assembly
-    sbt compile
-    sbt publish
+    sbt compile assembly publish
     find publish -name "*.md5" -type f -delete
     find publish -name "*.sha1" -type f -delete
 
@@ -34,9 +60,6 @@ function publish ()
     echo "ls publish/io/swagger/swagger-java-client_2.11/1.0.0"
     ls publish/io/swagger/swagger-java-client_2.11/1.0.0
     chmod a+wx publish
-
-    COMMIT_HASH="${TRAVIS_COMMIT:0:7}"
-    echo "Commit hash:$COMMIT_HASH"
 
 #    Scala version    $TRAVIS_SCALA_VERSION
 #    Version
