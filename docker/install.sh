2d4848c4897c9747f199d857dbc7c39eab2f93cc
ansingh7115 <singh.an@husky.neu.edu>
2018-01-10 11:20:51 -0500

    revert changes for jenkins

--- docker/install.sh
+++ docker/install.sh
@@ -6,8 +6,8 @@ SAM_DIR=$1
 cd $SAM_DIR
 
 # Tests are run in jenkins which has a custom opendj instance just for testing
-sbt -J-Xms2g -J-Xmx2g test -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD
-sbt -J-Xms2g -J-Xmx2g assembly
+sbt -J-Xms4g -J-Xmx4g test -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD
+sbt -J-Xms4g -J-Xmx4g assembly
 SAM_JAR=$(find target | grep 'sam.*\.jar')
 mv $SAM_JAR .
 sbt clean
