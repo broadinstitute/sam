81352c17ff52d99e6ecea52f1fe50df60a6eb22d
dvoet <dvoet@users.noreply.github.com>
2018-03-27 22:03:16 -0400

    Pet name change (#126)

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/SamApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/SamApiSpec.scala
@@ -19,14 +19,6 @@ import scala.concurrent.duration.Duration
 class SamApiSpec extends FreeSpec with BillingFixtures with Matchers with ScalaFutures with CleanUp {
   implicit override val patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)))
 
-  def findSaInGoogle(project: String, name: ServiceAccountName): Option[ServiceAccount] = {
-    googleIamDAO.findServiceAccount(GoogleProject(project), name).futureValue
-  }
-
-  def findPetInGoogle(project: String, userInfo: UserStatusDetails): Option[ServiceAccount] = {
-    findSaInGoogle(project, Sam.petName(userInfo))
-  }
-
   def registerAsNewUser(email: WorkbenchEmail)(implicit authToken: AuthToken): Unit = {
     val newUserProfile = Orchestration.profile.BasicProfile (
       firstName = "Generic",
@@ -98,13 +90,14 @@ class SamApiSpec extends FreeSpec with BillingFixtures with Matchers with ScalaF
         // since projects get reused in tests it is possible that the pet SA is in google but not in ldap
         // and if it is not in ldap sam won't try to remove it from google
         // in order to remove it we need to create it in sam first (and thus ldap) then remove it
-        Sam.user.petServiceAccountEmail(projectName)(userAuthToken)
-        Sam.removePet(projectName, userStatus.userInfo)
-        findPetInGoogle(projectName, userStatus.userInfo) shouldBe None
-
         val petAccountEmail = Sam.user.petServiceAccountEmail(projectName)(userAuthToken)
+        assert(petAccountEmail.value.contains(userStatus.userInfo.userSubjectId))
+        Sam.removePet(projectName, userStatus.userInfo)
+        googleIamDAO.findServiceAccount(GoogleProject(projectName), petAccountEmail).futureValue shouldBe None
+
+        Sam.user.petServiceAccountEmail(projectName)(userAuthToken)
         petAccountEmail.value should not be userStatus.userInfo.userEmail
-        findPetInGoogle(projectName, userStatus.userInfo).map(_.email) shouldBe Some(petAccountEmail)
+        googleIamDAO.findServiceAccount(GoogleProject(projectName), petAccountEmail).futureValue.map(_.email) shouldBe Some(petAccountEmail)
 
         // first call should create pet.  confirm that a second call to create/retrieve gives the same results
         Sam.user.petServiceAccountEmail(projectName)(userAuthToken) shouldBe petAccountEmail
@@ -119,7 +112,7 @@ class SamApiSpec extends FreeSpec with BillingFixtures with Matchers with ScalaF
         // clean up
 
         Sam.removePet(projectName, userStatus.userInfo)
-        findPetInGoogle(projectName, userStatus.userInfo) shouldBe None
+        googleIamDAO.findServiceAccount(GoogleProject(projectName), petAccountEmail).futureValue shouldBe None
       }
     }
 
@@ -173,46 +166,53 @@ class SamApiSpec extends FreeSpec with BillingFixtures with Matchers with ScalaF
       val authToken1: AuthToken = user1.makeAuthToken()
       val authToken2: AuthToken = user2.makeAuthToken()
 
-      val email = WorkbenchEmail(Sam.user.status()(authToken1).get.userInfo.userEmail)
-      val username = email.value.split("@").head
-      val userId = Sam.user.status()(authToken1).get.userInfo.userSubjectId
+      withCleanBillingProject(UserPool.chooseProjectOwner, List(user1.email, user2.email)) { project =>
+        val email = WorkbenchEmail(Sam.user.status()(authToken1).get.userInfo.userEmail)
+        val username = email.value.split("@").head
+        val userId = Sam.user.status()(authToken1).get.userInfo.userSubjectId
 
-      val petSAEmail = Sam.user.petServiceAccountEmail(Config.Projects.default)(authToken1)
+        val petSAEmail = Sam.user.petServiceAccountEmail(project)(authToken1)
 
-      val proxyGroup_1 = Sam.user.proxyGroup(petSAEmail.value)(authToken1)
-      val proxyGroup_2 = Sam.user.proxyGroup(petSAEmail.value)(authToken2)
+        val proxyGroup_1 = Sam.user.proxyGroup(petSAEmail.value)(authToken1)
+        val proxyGroup_2 = Sam.user.proxyGroup(petSAEmail.value)(authToken2)
 
-/* Re-enable this code and remove the temporary code below after fixing rawls for GAWB-2933
+        /* Re-enable this code and remove the temporary code below after fixing rawls for GAWB-2933
       val expectedProxyEmail = s"${username}_$userId@${Config.GCS.appsDomain}"
 */
-      val expectedProxyEmail = s"$userId@${Config.GCS.appsDomain}"
-/**/
-      proxyGroup_1.value should endWith (expectedProxyEmail)
-      proxyGroup_2.value should endWith (expectedProxyEmail)
+        val expectedProxyEmail = s"$userId@${Config.GCS.appsDomain}"
+        /**/
+        proxyGroup_1.value should endWith(expectedProxyEmail)
+        proxyGroup_2.value should endWith(expectedProxyEmail)
+      }
     }
 
 
     "should furnish a new service account key and cache it for further retrievals" in {
       val user = UserPool.chooseStudent
 
-      val key1 = Sam.user.petServiceAccountKey(Config.Projects.default)(user.makeAuthToken)
-      val key2 = Sam.user.petServiceAccountKey(Config.Projects.default)(user.makeAuthToken)
+      withCleanBillingProject(UserPool.chooseProjectOwner, List(user.email)) { project =>
+        val key1 = Sam.user.petServiceAccountKey(project)(user.makeAuthToken)
+        val key2 = Sam.user.petServiceAccountKey(project)(user.makeAuthToken)
 
-      key1 shouldBe key2
+        key1 shouldBe key2
 
-      register cleanUp Sam.user.deletePetServiceAccountKey(Config.Projects.default, getFieldFromJson(key1, "private_key_id"))(user.makeAuthToken)
+        register cleanUp Sam.user.deletePetServiceAccountKey(project, getFieldFromJson(key1, "private_key_id"))(user.makeAuthToken)
+      }
     }
 
     "should furnish a new service account key after deleting a cached key" in {
       val user = UserPool.chooseStudent
 
-      val key1 = Sam.user.petServiceAccountKey(Config.Projects.default)(user.makeAuthToken)
-      Sam.user.deletePetServiceAccountKey(Config.Projects.default, getFieldFromJson(key1, "private_key_id"))(user.makeAuthToken)
+      withCleanBillingProject(UserPool.chooseProjectOwner, List(user.email)) { project =>
 
-      val key2 = Sam.user.petServiceAccountKey(Config.Projects.default)(user.makeAuthToken)
-      register cleanUp Sam.user.deletePetServiceAccountKey(Config.Projects.default, getFieldFromJson(key2, "private_key_id"))(user.makeAuthToken)
+        val key1 = Sam.user.petServiceAccountKey(project)(user.makeAuthToken)
+        Sam.user.deletePetServiceAccountKey(project, getFieldFromJson(key1, "private_key_id"))(user.makeAuthToken)
 
-      key1 shouldNot be(key2)
+        val key2 = Sam.user.petServiceAccountKey(project)(user.makeAuthToken)
+        register cleanUp Sam.user.deletePetServiceAccountKey(project, getFieldFromJson(key2, "private_key_id"))(user.makeAuthToken)
+
+        key1 shouldNot be(key2)
+      }
     }
 
     //this is ignored because there is a permission error with GPAlloc that needs to be looked into.
@@ -220,7 +220,6 @@ class SamApiSpec extends FreeSpec with BillingFixtures with Matchers with ScalaF
     // @mbemis
     "should re-create a pet SA in google even if it still exists in sam" ignore {
       val user = UserPool.chooseStudent
-      val projectName = Config.Projects.default
 
       //this must use a GPAlloc'd project to avoid deleting the pet for a shared project, which
       //may have unexpected side effects
@@ -230,7 +229,7 @@ class SamApiSpec extends FreeSpec with BillingFixtures with Matchers with ScalaF
         val petSaKeyIdOriginal = getFieldFromJson(petSaKeyOriginal, "private_key_id")
         val petSaName = petSaEmailOriginal.split('@').head
 
-        register cleanUp Sam.user.deletePetServiceAccountKey(Config.Projects.default, petSaKeyIdOriginal)(user.makeAuthToken)
+        register cleanUp Sam.user.deletePetServiceAccountKey(projectName, petSaKeyIdOriginal)(user.makeAuthToken)
 
         //act as a rogue process and delete the pet SA without telling sam
         Await.result(googleIamDAO.removeServiceAccount(GoogleProject(projectName), ServiceAccountName(petSaName)), Duration.Inf)
@@ -239,7 +238,7 @@ class SamApiSpec extends FreeSpec with BillingFixtures with Matchers with ScalaF
         val petSaEmailNew = getFieldFromJson(petSaKeyNew, "client_email")
         val petSaKeyIdNew = getFieldFromJson(petSaKeyNew, "private_key_id")
 
-        register cleanUp Sam.user.deletePetServiceAccountKey(Config.Projects.default, petSaKeyIdNew)(user.makeAuthToken)
+        register cleanUp Sam.user.deletePetServiceAccountKey(projectName, petSaKeyIdNew)(user.makeAuthToken)
 
         petSaEmailOriginal should equal(petSaEmailNew) //sanity check to make sure the SA is the same
         petSaKeyIdOriginal should not equal petSaKeyIdNew //make sure we were able to generate a new key and that a new one was returned
