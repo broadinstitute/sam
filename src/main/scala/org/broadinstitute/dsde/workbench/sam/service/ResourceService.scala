e3f0d86f7ebbadfeff5ef13d9e5541f6c82347a4
Matt Bemis <mbemis@broadinstitute.org>
2018-04-13 17:57:21 -0400

    works

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/service/ResourceService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/service/ResourceService.scala
@@ -30,45 +30,29 @@ class ResourceService(private val resourceTypes: Map[ResourceTypeName, ResourceT
     accessPolicyDAO.createResourceType(resourceType.name)
   }
 
-  def createResource(resourceType: ResourceType, resourceId: ResourceId, policies: Option[Map[AccessPolicyName, AccessPolicyMembership]] = None, userInfo: UserInfo): Future[Resource] = {
+  def createResource(resourceType: ResourceType, resourceId: ResourceId, policiesOpt: Option[Map[AccessPolicyName, AccessPolicyMembership]] = None, userInfo: UserInfo): Future[Resource] = {
     accessPolicyDAO.createResource(Resource(resourceType.name, resourceId)) flatMap { resource =>
-      val policiesToCreate = policies match {
-        case Some(policies) => {
-          val x = policies.map { case (accessPolicyName, accessPolicyMembership) =>
-            val resourceAndPolicyName = ResourceIdAndPolicyName(resourceId, accessPolicyName)
-            val subjects = Future.traverse(accessPolicyMembership.memberEmails) { directoryDAO.loadSubjectFromEmail }.map(x => x.collect { case Some(e) => e })
+      policiesOpt match {
+        case Some(policies) =>
+          Future.traverse(policies) { case (accessPolicyName, accessPolicyMembership) =>
+            val resourceAndPolicyName = ResourceAndPolicyName(Resource(resourceType.name, resourceId), accessPolicyName)
+            val loadedSubjects = Future.traverse(accessPolicyMembership.memberEmails) { directoryDAO.loadSubjectFromEmail }.map(subjOpts => subjOpts.collect { case Some(subj) => subj })
             val roles = accessPolicyMembership.roles
             val actions = accessPolicyMembership.actions
 
-
-            subjects.map { x =>
-              (resourceAndPolicyName, x, roles, actions)
+            loadedSubjects.flatMap { subjects =>
+              createPolicy(resourceAndPolicyName, subjects, roles, actions)
             }
-          }
+          }.map {_ => Resource(resourceType.name, resourceId)}
 
-          x
-        }
-
-        case None => {
+        //if the user didn't specify policies to create, default to just creating the owner policy
+        case None =>
           val role = resourceType.roles.find(_.roleName == resourceType.ownerRoleName).getOrElse(throw new WorkbenchException(s"owner role ${resourceType.ownerRoleName} does not exist in $resourceType"))
           val subjects: Set[WorkbenchSubject] = Set(userInfo.userId)
           val resourceAndPolicyName = ResourceAndPolicyName(resource, AccessPolicyName(role.roleName.value))
 
-          Iterable(Future.successful((resourceAndPolicyName, subjects, Set(role.roleName), Set.empty)))
-        }
+          createPolicy(resourceAndPolicyName, subjects, Set(role.roleName), Set.empty).map {_ => Resource(resourceType.name, resourceId)}
       }
-
-      Future.sequence(policiesToCreate)
-    }
-
-
-
-    accessPolicyDAO.createResource(Resource(resourceType.name, resourceId)) flatMap { resource =>
-      val role = resourceType.roles.find(_.roleName == resourceType.ownerRoleName).getOrElse(throw new WorkbenchException(s"owner role ${resourceType.ownerRoleName} does not exist in $resourceType"))
-      val subjects: Set[WorkbenchSubject] = Set(userInfo.userId)
-      val resourceAndPolicyName = ResourceAndPolicyName(resource, AccessPolicyName(role.roleName.value))
-
-      createPolicy(resourceAndPolicyName, subjects, Set(role.roleName), Set.empty).map {_ => Resource(resourceType.name, resourceId)}
     }
   }
 
