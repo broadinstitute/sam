62f1d34c58668fa8c10742665d2ee68419c5589c
Douglas Voet <dvoet@users.noreply.github.com>
2018-06-05 22:06:44 -0400

    restart opendj on jenkins

--- docker/init_schema.sh
+++ docker/init_schema.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+SAM_DIR=$1
+cd $SAM_DIR
+
 sbt -J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD "test-only -- -n org.broadinstitute.tags.SchemaInit"
