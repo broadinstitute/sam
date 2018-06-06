9a87395c948a0a63cad247c34c0e7b4c63c6dc20
Douglas Voet <dvoet@users.noreply.github.com>
2018-06-05 20:50:12 -0400

    restart opendj on jenkins

--- docker/install.sh
+++ docker/install.sh
@@ -6,7 +6,9 @@ SAM_DIR=$1
 cd $SAM_DIR
 
 # Tests are run in jenkins which has a custom opendj instance just for testing
-sbt -J-Xms2g -J-Xmx2g test -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD
+sbt -J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD "test-only -- -n org.broadinstitute.tags.SchemaInit"
+docker restart opendj
+sbt -J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD "test-only -- -l org.broadinstitute.tags.SchemaInit"
 sbt -J-Xms2g -J-Xmx2g assembly
 SAM_JAR=$(find target | grep 'sam.*\.jar')
 mv $SAM_JAR .
