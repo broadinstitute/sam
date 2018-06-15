bb402e0fd6b077565dcdac8b332208b8462f3838
Joel Thibault <thibault@broadinstitute.org>
2018-06-15 17:32:25 -0400

    update to real version

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val workbenchModelV  = "0.10-6800f3a"
   val workbenchGoogleV = "0.16-847c3ff"
-  val workbenchServiceTestV = "0.10-5e957b6-SNAP"
+  val workbenchServiceTestV = "0.10-61981d5"
 
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
   val excludeWorkbenchModel = ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-model_2.11")
