734de7105d9e71423915334b0b2cd8863d5f7b8f
Qi Wang <qi@broadinstitute.org>
2018-09-28 11:19:16 -0400

    upgrade sbt

--- docker/init_schema.sh
+++ docker/init_schema.sh
@@ -5,4 +5,4 @@ set -e
 SAM_DIR=$1
 cd $SAM_DIR
 
-sbt -J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD "test-only -- -n org.broadinstitute.tags.SchemaInit"
+SBT_OPTS="-J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD" sbt "testOnly -- -n org.broadinstitute.tags.SchemaInit"
--- docker/install.sh
+++ docker/install.sh
@@ -6,8 +6,8 @@ SAM_DIR=$1
 cd $SAM_DIR
 
 # Tests are run in jenkins which has a custom opendj instance just for testing
-sbt -J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD "test-only -- -l org.broadinstitute.tags.SchemaInit"
-sbt -J-Xms2g -J-Xmx2g assembly
+SBT_OPTS="-J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD" sbt "testOnly -- -l org.broadinstitute.tags.SchemaInit"
+SBT_OPTS="-J-Xms2g -J-Xmx2g" sbt assembly
 SAM_JAR=$(find target | grep 'sam.*\.jar')
 mv $SAM_JAR .
 sbt clean
