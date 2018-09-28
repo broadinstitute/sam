42279c47d7cd0d6b9754dc5616e137c6541a0a2a
Qi Wang <qi@broadinstitute.org>
2018-09-28 13:36:04 -0400

    fix travis

--- docker/init_schema.sh
+++ docker/init_schema.sh
@@ -5,4 +5,5 @@ set -e
 SAM_DIR=$1
 cd $SAM_DIR
 
-SBT_OPTS="-J-Xms2g -J-Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD" sbt "testOnly -- -n org.broadinstitute.tags.SchemaInit"
+export SBT_OPTS="-Xms2g -Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD"
+sbt "testOnly -- -n org.broadinstitute.tags.SchemaInit"
