622d410fc6c8211f8f402a15fc189ca4ae6e9db9
Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
2018-11-06 13:56:44 -0500

    Changes to try to ensure that the opendj container gets cleaned up in the event of an error when building the .jar (#241)

--- docker/build.sh
+++ docker/build.sh
@@ -96,11 +96,13 @@ function make_jar()
     GIT_MODEL_HASH=$(git log -n 1 --pretty=format:%h)
 
     # make jar.  cache sbt dependencies.
+    set +e # Turn off error detection so that opendj has a chance to get stopped before exiting
     docker run --rm --link $OPENDJ:opendj -e DIRECTORY_URL=$DIRECTORY_URL -e GIT_MODEL_HASH=$GIT_MODEL_HASH -e DIRECTORY_PASSWORD=$DIRECTORY_PASSWORD -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 broadinstitute/scala-baseimage /working/docker/init_schema.sh /working
     docker restart $OPENDJ
     sleep 40
     docker run --rm --link $OPENDJ:opendj -e DIRECTORY_URL=$DIRECTORY_URL -e GIT_MODEL_HASH=$GIT_MODEL_HASH -e DIRECTORY_PASSWORD=$DIRECTORY_PASSWORD -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 broadinstitute/scala-baseimage /working/docker/install.sh /working
     EXIT_CODE=$?
+    set -e # Turn error detection back on for the rest of the script
 
     bash ./docker/run-opendj.sh stop jenkins $OPENDJ
 
