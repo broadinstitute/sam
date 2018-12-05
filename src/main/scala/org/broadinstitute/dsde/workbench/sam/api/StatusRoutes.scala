a4f2bd95628527ca757c87ed5c4bb6e2718fc443
Raphael Luckom <rluckom@broadinstitute.org>
2018-12-04 22:39:36 -0500

    add version endpoint

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/StatusRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/StatusRoutes.scala
@@ -5,9 +5,15 @@ import akka.http.scaladsl.server.Directives._
 import org.broadinstitute.dsde.workbench.sam.service.StatusService
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
+import spray.json.{JsObject, JsString}
 import org.broadinstitute.dsde.workbench.util.health.StatusJsonSupport._
 
 import scala.concurrent.ExecutionContext
+
+object BuildTimeVersion {
+  val version = Option(getClass.getPackage.getImplementationVersion)
+  val versionJson = JsObject(Map("version" -> JsString(version.getOrElse("n/a"))))
+}
 
 trait StatusRoutes {
   val statusService: StatusService
@@ -21,6 +27,13 @@ trait StatusRoutes {
             val httpStatus = if (statusResponse.ok) StatusCodes.OK else StatusCodes.InternalServerError
             (httpStatus, statusResponse)
           })
+        }
+      }
+    } ~
+    pathPrefix("version") {
+      pathEndOrSingleSlash {
+        get {
+          complete((StatusCodes.OK, BuildTimeVersion.versionJson))
         }
       }
     }
