85cb8d49ed90edd3c030aac4e00c618d2b79a54a
Douglas Voet <dvoet@users.noreply.github.com>
2019-01-11 11:10:57 -0500

    integration tests too

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val akkaHttpV = "10.0.10"
 
   val workbenchModelV  = "0.13-7e86fba"
-  val workbenchGoogleV = "0.18-6942040"
+  val workbenchGoogleV = "0.18-4e7f488-SNAP"
   val workbenchServiceTestV = "0.16-f6491ba"
 
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
