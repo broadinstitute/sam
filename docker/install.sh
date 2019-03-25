b2774b5b708678b511da030bf19103bd60eb728a
Hussein Elgridly <helgridly@users.noreply.github.com>
2019-03-25 14:50:41 -0400

    actually, this seems better

--- docker/install.sh
+++ docker/install.sh
@@ -7,7 +7,7 @@ cd $SAM_DIR
 
 # Tests are run in jenkins which has a custom opendj instance just for testing
 export SBT_OPTS="-Xms2g -Xmx2g -Ddirectory.url=$DIRECTORY_URL -Ddirectory.password=$DIRECTORY_PASSWORD"
-sbt "-batch testOnly -- -l org.broadinstitute.tags.SchemaInit"
+sbt -batch "testOnly -- -l org.broadinstitute.tags.SchemaInit"
 sbt -batch assembly
 SAM_JAR=$(find target | grep 'sam.*\.jar')
 mv $SAM_JAR .
