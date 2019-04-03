2b33653ca2a7ce01a404e2c2d9c5fc0d5eb62c6a
Douglas Voet <dvoet@users.noreply.github.com>
2019-04-02 21:35:25 -0400

    handle 400 error removing user that does not exist from group

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   val workbenchUtilV   = "0.5-6942040"
   val workbenchModelV  = "0.13-7e86fba"
-  val workbenchGoogleV = "0.18-5a6b067-SNAP"
+  val workbenchGoogleV = "0.18-5941525"
   val workbenchGoogle2V = "0.1-8328aae"
   val workbenchNotificationsV = "0.1-f2a0020"
   val monocleVersion = "1.5.1-cats"
