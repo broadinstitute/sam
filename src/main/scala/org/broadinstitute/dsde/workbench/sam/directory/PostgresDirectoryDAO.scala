9cfeda1a8c25bf73bf3d3d079a3989f4d8bc42cf
Marc Talbott <mtalbott@broadinstitute.org>
2019-07-17 16:30:28 -0400

    scalike tables, first two queries, start of tests (#314)
    * first effort at setting up scalike tables
    
    * remove unused import
    
    * forgot one of my binders aka middle school nightmare
    
    * remove comment
    
    * rename record objects to table, first query done
    
    * wip so greg can drive
    
    * CA-308 Pushing stuff that's very much WIP so that we can all look at this to figure out what to do to get it to work.
    
    * remove Sam ValueObject in favor of WorkbenchLibs Model ValueObject; finish up loadGroup query; add first unit tests
    
    * add more unit tests, change insert group members query to work, start renaming/refactoring record case classes for clarity and individuality
    
    * some more renaming, move type binders to centralized location, adjust schema for string user ids
    
    * test support changes for doug
    
    * add second subgroup
    
    * some small changes
    
    * a little more cleanup
    
    * address comments

---
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/directory/PostgresDirectoryDAO.scala
@@ -0,0 +1,216 @@
+package org.broadinstitute.dsde.workbench.sam.directory
+
+import java.time.Instant
+import java.util.Date
+
+import cats.effect.{ContextShift, IO}
+import org.broadinstitute.dsde.workbench.model._
+import org.broadinstitute.dsde.workbench.model.google.ServiceAccountSubjectId
+import org.broadinstitute.dsde.workbench.sam.db._
+import org.broadinstitute.dsde.workbench.sam.db.tables._
+import org.broadinstitute.dsde.workbench.sam.model.BasicWorkbenchGroup
+import org.broadinstitute.dsde.workbench.sam.util.DatabaseSupport
+import scalikejdbc._
+import SamParameterBinderFactory._
+
+import scala.concurrent.ExecutionContext
+
+class PostgresDirectoryDAO(protected val dbRef: DbReference,
+                           protected val ecForDatabaseIO: ExecutionContext)(implicit executionContext: ExecutionContext) extends DirectoryDAO with DatabaseSupport {
+  implicit val cs: ContextShift[IO] = IO.contextShift(executionContext)
+
+  override def createGroup(group: BasicWorkbenchGroup, accessInstructionsOpt: Option[String]): IO[BasicWorkbenchGroup] = {
+    runInTransaction { implicit session =>
+      val groupId: GroupPK = insertGroup(group)
+
+      accessInstructionsOpt.map { accessInstructions =>
+        insertAccessInstructions(groupId, accessInstructions)
+      }
+
+      insertGroupMembers(groupId, group.members)
+
+      group
+    }
+  }
+
+  private def insertGroup(group: BasicWorkbenchGroup)(implicit session: DBSession): GroupPK = {
+    val groupTableColumn = GroupTable.column
+    GroupPK(withSQL {
+      insert
+        .into(GroupTable)
+        .namedValues(
+          groupTableColumn.name -> group.id, // the id of a BasicWorkbenchGroup is the name of the group and is a different id from the database id... obviously (sorry)
+          groupTableColumn.email -> group.email,
+          groupTableColumn.updatedDate -> Option(Instant.now()),
+          groupTableColumn.synchronizedDate -> None
+        )
+    }.updateAndReturnGeneratedKey().apply())
+  }
+
+  private def insertAccessInstructions(groupId: GroupPK, accessInstructions: String)(implicit session: DBSession): Int = {
+    val accessInstructionsColumn = AccessInstructionsTable.column
+    withSQL {
+      insert
+        .into(AccessInstructionsTable)
+        .namedValues(
+          accessInstructionsColumn.groupId -> groupId,
+          accessInstructionsColumn.instructions -> accessInstructions
+        )
+    }.update().apply()
+  }
+
+  private def insertGroupMembers(groupId: GroupPK, members: Set[WorkbenchSubject])(implicit session: DBSession): Int = {
+    if (members.isEmpty) {
+      0
+    } else {
+      // multipleValues (used below to insert all the records into the GroupMemberTable) doesn't really handle implicit parameter binder factories
+      // like namedValues does because it takes a Seq[Any] and so it doesn't know to convert our case classes to ParameterBinders using the PBFs.
+      // Declaring the PBFs and then using them explicitly to convert our case classes to ParameterBinders enables scalike to properly bind our values
+      // to the PreparedStatement. Without these PBFs, scalike throws an error because it doesn't know what SQL type one of our case classes corresponds to
+      val groupIdPBF = databaseKeyPbf[GroupPK]
+      val userIdPBF = valueObjectPbf[WorkbenchUserId]
+      val optionalUserPBF = ParameterBinderFactory.optionalParameterBinderFactory(userIdPBF)
+      val optionalGroupPBF = ParameterBinderFactory.optionalParameterBinderFactory(groupIdPBF)
+
+      val memberUserRecords: Seq[(ParameterBinder, ParameterBinder, ParameterBinder)] = members.collect {
+        case userId@WorkbenchUserId(_) => (groupIdPBF(groupId), optionalUserPBF(Option(userId)), optionalGroupPBF(None))
+      }.toSeq
+
+      val memberGroupNames = members.collect {
+        case WorkbenchGroupName(name) => name
+      }
+
+      import SamTypeBinders._
+
+      val groupTable = GroupTable.syntax("g")
+      val memberGroupRecords: List[(ParameterBinder, ParameterBinder, ParameterBinder)] = withSQL {
+        select(groupTable.id)
+          .from(GroupTable as groupTable)
+          .where
+          .in(groupTable.name, memberGroupNames.toSeq)
+      }.map(rs => (groupIdPBF(groupId), optionalUserPBF(None), optionalGroupPBF(Option(rs.get[GroupPK](1))))).list().apply()
+
+      val allRecords = (memberUserRecords ++ memberGroupRecords).map(_.productIterator.toSeq)
+
+      val groupMemberColumn = GroupMemberTable.column
+      withSQL {
+        insert
+          .into(GroupMemberTable)
+          .columns(groupMemberColumn.groupId, groupMemberColumn.memberUserId, groupMemberColumn.memberGroupId)
+          .multipleValues(allRecords: _*)
+      }.update().apply()
+    }
+  }
+
+  override def loadGroup(groupName: WorkbenchGroupName): IO[Option[BasicWorkbenchGroup]] = {
+    for {
+      results <- runInTransaction { implicit session =>
+        val groupTable = GroupTable.syntax("g")
+        val subGroupTable = GroupTable.syntax("sG")
+        val groupMemberTable = GroupMemberTable.syntax("gM")
+
+        withSQL {
+          select(groupTable.email, groupMemberTable.memberUserId, subGroupTable.name)
+            .from(GroupTable as groupTable)
+            .leftJoin(GroupMemberTable as groupMemberTable)
+            .on(groupTable.id, groupMemberTable.groupId)
+            .leftJoin(GroupTable as subGroupTable)
+            .on(groupMemberTable.memberGroupId, subGroupTable.id)
+            .where
+            .eq(groupTable.name, groupName)
+        }.map(rs => (WorkbenchEmail(rs.string(1)), rs.stringOpt(2).map(WorkbenchUserId), rs.stringOpt(3).map(WorkbenchGroupName)))
+          .list().apply()
+      }
+    } yield {
+      if (results.isEmpty) {
+        None
+      } else {
+        val email = results.head._1
+        val members: Set[WorkbenchSubject] = results.collect {
+          case (_, Some(userId), None) => userId
+          case (_, None, Some(subGroupName)) => subGroupName
+        }.toSet
+
+        Option(BasicWorkbenchGroup(groupName, members, email))
+      }
+    }
+  }
+
+  override def loadGroups(groupNames: Set[WorkbenchGroupName]): IO[Stream[BasicWorkbenchGroup]] = ???
+
+  override def loadGroupEmail(groupName: WorkbenchGroupName): IO[Option[WorkbenchEmail]] = ???
+
+  override def batchLoadGroupEmail(groupNames: Set[WorkbenchGroupName]): IO[Stream[(WorkbenchGroupName, WorkbenchEmail)]] = ???
+
+  override def deleteGroup(groupName: WorkbenchGroupName): IO[Unit] = ???
+
+  /**
+    * @return true if the subject was added, false if it was already there
+    */
+  override def addGroupMember(groupId: WorkbenchGroupIdentity, addMember: WorkbenchSubject): IO[Boolean] = ???
+
+  /**
+    * @return true if the subject was removed, false if it was already gone
+    */
+  override def removeGroupMember(groupId: WorkbenchGroupIdentity, removeMember: WorkbenchSubject): IO[Boolean] = ???
+
+  override def isGroupMember(groupId: WorkbenchGroupIdentity, member: WorkbenchSubject): IO[Boolean] = ???
+
+  override def updateSynchronizedDate(groupId: WorkbenchGroupIdentity): IO[Unit] = ???
+
+  override def getSynchronizedDate(groupId: WorkbenchGroupIdentity): IO[Option[Date]] = ???
+
+  override def getSynchronizedEmail(groupId: WorkbenchGroupIdentity): IO[Option[WorkbenchEmail]] = ???
+
+  override def loadSubjectFromEmail(email: WorkbenchEmail): IO[Option[WorkbenchSubject]] = ???
+
+  override def loadSubjectEmail(subject: WorkbenchSubject): IO[Option[WorkbenchEmail]] = ???
+
+  override def loadSubjectEmails(subjects: Set[WorkbenchSubject]): IO[Stream[WorkbenchEmail]] = ???
+
+  override def loadSubjectFromGoogleSubjectId(googleSubjectId: GoogleSubjectId): IO[Option[WorkbenchSubject]] = ???
+
+  override def createUser(user: WorkbenchUser): IO[WorkbenchUser] = ???
+
+  override def loadUser(userId: WorkbenchUserId): IO[Option[WorkbenchUser]] = ???
+
+  override def loadUsers(userIds: Set[WorkbenchUserId]): IO[Stream[WorkbenchUser]] = ???
+
+  override def deleteUser(userId: WorkbenchUserId): IO[Unit] = ???
+
+  override def addProxyGroup(userId: WorkbenchUserId, proxyEmail: WorkbenchEmail): IO[Unit] = ???
+
+  override def readProxyGroup(userId: WorkbenchUserId): IO[Option[WorkbenchEmail]] = ???
+
+  override def listUsersGroups(userId: WorkbenchUserId): IO[Set[WorkbenchGroupIdentity]] = ???
+
+  override def listUserDirectMemberships(userId: WorkbenchUserId): IO[Stream[WorkbenchGroupIdentity]] = ???
+
+  override def listIntersectionGroupUsers(groupId: Set[WorkbenchGroupIdentity]): IO[Set[WorkbenchUserId]] = ???
+
+  override def listAncestorGroups(groupId: WorkbenchGroupIdentity): IO[Set[WorkbenchGroupIdentity]] = ???
+
+  override def enableIdentity(subject: WorkbenchSubject): IO[Unit] = ???
+
+  override def disableIdentity(subject: WorkbenchSubject): IO[Unit] = ???
+
+  override def isEnabled(subject: WorkbenchSubject): IO[Boolean] = ???
+
+  override def getUserFromPetServiceAccount(petSA: ServiceAccountSubjectId): IO[Option[WorkbenchUser]] = ???
+
+  override def createPetServiceAccount(petServiceAccount: PetServiceAccount): IO[PetServiceAccount] = ???
+
+  override def loadPetServiceAccount(petServiceAccountId: PetServiceAccountId): IO[Option[PetServiceAccount]] = ???
+
+  override def deletePetServiceAccount(petServiceAccountId: PetServiceAccountId): IO[Unit] = ???
+
+  override def getAllPetServiceAccountsForUser(userId: WorkbenchUserId): IO[Seq[PetServiceAccount]] = ???
+
+  override def updatePetServiceAccount(petServiceAccount: PetServiceAccount): IO[PetServiceAccount] = ???
+
+  override def getManagedGroupAccessInstructions(groupName: WorkbenchGroupName): IO[Option[String]] = ???
+
+  override def setManagedGroupAccessInstructions(groupName: WorkbenchGroupName, accessInstructions: String): IO[Unit] = ???
+
+  override def setGoogleSubjectId(userId: WorkbenchUserId, googleSubjectId: GoogleSubjectId): IO[Unit] = ???
+}
