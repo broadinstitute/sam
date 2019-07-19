4e05af7571fd061b9d93dcb2f90131ecb56f5fbf
Marc Talbott <mtalbott@broadinstitute.org>
2019-07-19 16:15:51 -0400

    more directory dao queries (#316)
    * add some more queries, addGroupMember untested; add unique constraints to schema
    
    * removeGroupMember
    
    * add createUser and loadUser; reference.conf settings to support logging SQL queries
    
    * deleteUser
    
    * loadUsers
    
    * add test that a user that is still a member of a group cannot be deleted
    
    * createPetServiceAccounts
    
    * unignore the most annoying test in the world
    
    * some cleanup; still need to convert more DSL queries into SQL

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/directory/PostgresDirectoryDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/directory/PostgresDirectoryDAO.scala
@@ -8,10 +8,12 @@ import org.broadinstitute.dsde.workbench.model._
 import org.broadinstitute.dsde.workbench.model.google.ServiceAccountSubjectId
 import org.broadinstitute.dsde.workbench.sam.db._
 import org.broadinstitute.dsde.workbench.sam.db.tables._
-import org.broadinstitute.dsde.workbench.sam.model.BasicWorkbenchGroup
+import org.broadinstitute.dsde.workbench.sam.model.{BasicWorkbenchGroup, FullyQualifiedPolicyId}
 import org.broadinstitute.dsde.workbench.sam.util.DatabaseSupport
+import org.broadinstitute.dsde.workbench.sam.errorReportSource
 import scalikejdbc._
 import SamParameterBinderFactory._
+import akka.http.scaladsl.model.StatusCodes
 
 import scala.concurrent.ExecutionContext
 
@@ -35,28 +37,17 @@ class PostgresDirectoryDAO(protected val dbRef: DbReference,
 
   private def insertGroup(group: BasicWorkbenchGroup)(implicit session: DBSession): GroupPK = {
     val groupTableColumn = GroupTable.column
-    GroupPK(withSQL {
-      insert
-        .into(GroupTable)
-        .namedValues(
-          groupTableColumn.name -> group.id, // the id of a BasicWorkbenchGroup is the name of the group and is a different id from the database id... obviously (sorry)
-          groupTableColumn.email -> group.email,
-          groupTableColumn.updatedDate -> Option(Instant.now()),
-          groupTableColumn.synchronizedDate -> None
-        )
-    }.updateAndReturnGeneratedKey().apply())
+    val insertGroupQuery = sql"""insert into ${GroupTable.table} (${groupTableColumn.name}, ${groupTableColumn.email}, ${groupTableColumn.updatedDate}, ${groupTableColumn.synchronizedDate})
+           values (${group.id.value}, ${group.email.value}, ${Option(Instant.now())}, ${None})"""
+
+    GroupPK(insertGroupQuery.updateAndReturnGeneratedKey().apply())
   }
 
   private def insertAccessInstructions(groupId: GroupPK, accessInstructions: String)(implicit session: DBSession): Int = {
     val accessInstructionsColumn = AccessInstructionsTable.column
-    withSQL {
-      insert
-        .into(AccessInstructionsTable)
-        .namedValues(
-          accessInstructionsColumn.groupId -> groupId,
-          accessInstructionsColumn.instructions -> accessInstructions
-        )
-    }.update().apply()
+    val insertAccessInstructionsQuery = sql"insert into ${AccessInstructionsTable.table} (${accessInstructionsColumn.groupId}, ${accessInstructionsColumn.instructions}) values (${groupId.value}, ${accessInstructions})"
+
+    insertAccessInstructionsQuery.update().apply()
   }
 
   private def insertGroupMembers(groupId: GroupPK, members: Set[WorkbenchSubject])(implicit session: DBSession): Int = {
@@ -72,33 +63,43 @@ class PostgresDirectoryDAO(protected val dbRef: DbReference,
       val optionalUserPBF = ParameterBinderFactory.optionalParameterBinderFactory(userIdPBF)
       val optionalGroupPBF = ParameterBinderFactory.optionalParameterBinderFactory(groupIdPBF)
 
-      val memberUserRecords: Seq[(ParameterBinder, ParameterBinder, ParameterBinder)] = members.collect {
-        case userId@WorkbenchUserId(_) => (groupIdPBF(groupId), optionalUserPBF(Option(userId)), optionalGroupPBF(None))
+      val parentGroupParameterBinder = groupIdPBF(groupId)
+
+      val memberUserParameterBinders: Seq[(ParameterBinder, ParameterBinder, ParameterBinder)] = members.collect {
+        case userId@WorkbenchUserId(_) => (parentGroupParameterBinder, optionalUserPBF(Option(userId)), optionalGroupPBF(None))
       }.toSeq
 
       val memberGroupNames = members.collect {
         case WorkbenchGroupName(name) => name
       }
 
-      import SamTypeBinders._
-
       val groupTable = GroupTable.syntax("g")
-      val memberGroupRecords: List[(ParameterBinder, ParameterBinder, ParameterBinder)] = withSQL {
-        select(groupTable.id)
+      val memberGroupRecords: List[GroupRecord]  = withSQL {
+        select
           .from(GroupTable as groupTable)
           .where
           .in(groupTable.name, memberGroupNames.toSeq)
-      }.map(rs => (groupIdPBF(groupId), optionalUserPBF(None), optionalGroupPBF(Option(rs.get[GroupPK](1))))).list().apply()
+      }.map(GroupTable(groupTable)).list().apply()
 
-      val allRecords = (memberUserRecords ++ memberGroupRecords).map(_.productIterator.toSeq)
+      val loadedGroupNames = memberGroupRecords.map(_.name.value).toSet
+      val missingNames = memberGroupNames -- loadedGroupNames
 
-      val groupMemberColumn = GroupMemberTable.column
-      withSQL {
-        insert
-          .into(GroupMemberTable)
-          .columns(groupMemberColumn.groupId, groupMemberColumn.memberUserId, groupMemberColumn.memberGroupId)
-          .multipleValues(allRecords: _*)
-      }.update().apply()
+      if (missingNames.nonEmpty) {
+        throw new WorkbenchExceptionWithErrorReport(ErrorReport(StatusCodes.BadRequest, s"Some group members not found: [${missingNames.mkString(", ")}]"))
+      } else {
+        val memberGroupParameterBinders: List[(ParameterBinder, ParameterBinder, ParameterBinder)] = memberGroupRecords.map { groupRecord =>
+          (parentGroupParameterBinder, optionalUserPBF(None), optionalGroupPBF(Option(groupRecord.id)))
+        }
+        val allMemberParameterBinders = (memberUserParameterBinders ++ memberGroupParameterBinders).map(_.productIterator.toSeq)
+
+        val groupMemberColumn = GroupMemberTable.column
+        withSQL {
+          insert
+            .into(GroupMemberTable)
+            .columns(groupMemberColumn.groupId, groupMemberColumn.memberUserId, groupMemberColumn.memberGroupId)
+            .multipleValues(allMemberParameterBinders: _*)
+        }.update().apply()
+      }
     }
   }
 
@@ -136,23 +137,149 @@ class PostgresDirectoryDAO(protected val dbRef: DbReference,
     }
   }
 
-  override def loadGroups(groupNames: Set[WorkbenchGroupName]): IO[Stream[BasicWorkbenchGroup]] = ???
+  override def loadGroups(groupNames: Set[WorkbenchGroupName]): IO[Stream[BasicWorkbenchGroup]] = {
+    for {
+      results <- runInTransaction { implicit session =>
+        val groupTable = GroupTable.syntax("g")
+        val subGroupTable = GroupTable.syntax("sG")
+        val groupMemberTable = GroupMemberTable.syntax("gM")
 
-  override def loadGroupEmail(groupName: WorkbenchGroupName): IO[Option[WorkbenchEmail]] = ???
+        val groupNamesValuesSeq = groupNames.map(_.value).toSeq
+        withSQL {
+          select(groupTable.name, groupTable.email, groupMemberTable.memberUserId, subGroupTable.name)
+            .from(GroupTable as groupTable)
+            .leftJoin(GroupMemberTable as groupMemberTable)
+            .on(groupTable.id, groupMemberTable.groupId)
+            .leftJoin(GroupTable as subGroupTable)
+            .on(groupMemberTable.memberGroupId, subGroupTable.id)
+            .where
+            .in(groupTable.name, groupNamesValuesSeq)
+        }.map { rs =>
+          (WorkbenchGroupName(rs.string(1)),
+            WorkbenchEmail(rs.string(2)),
+            rs.stringOpt(3).map(WorkbenchUserId),
+            rs.stringOpt(4).map(WorkbenchGroupName))
+        }.list().apply()
+      }
+    } yield {
+      results.groupBy(result => (result._1, result._2)).map { case ((groupName, email), results) =>
+        val members: Set[WorkbenchSubject] = results.collect {
+            case (_, _, Some(userId), None) => userId
+            case (_, _, None, Some(subGroupName)) => subGroupName
+        }.toSet
 
-  override def batchLoadGroupEmail(groupNames: Set[WorkbenchGroupName]): IO[Stream[(WorkbenchGroupName, WorkbenchEmail)]] = ???
+        BasicWorkbenchGroup(groupName, members, email)
+      }
+    }.toStream
+  }
 
-  override def deleteGroup(groupName: WorkbenchGroupName): IO[Unit] = ???
+  override def loadGroupEmail(groupName: WorkbenchGroupName): IO[Option[WorkbenchEmail]] = {
+    runInTransaction { implicit session =>
+      val groupTable = GroupTable.syntax("g")
+
+      import SamTypeBinders._
+      withSQL {
+        select(groupTable.email)
+          .from(GroupTable as groupTable)
+          .where
+          .eq(groupTable.name, groupName)
+      }.map(rs => rs.get[WorkbenchEmail]("email")).single().apply()
+    }
+  }
+
+  override def batchLoadGroupEmail(groupNames: Set[WorkbenchGroupName]): IO[Stream[(WorkbenchGroupName, WorkbenchEmail)]] = {
+    runInTransaction { implicit session =>
+      val groupTable = GroupTable.syntax("g")
+
+      import SamTypeBinders._
+      withSQL {
+        select(groupTable.name, groupTable.email)
+          .from(GroupTable as groupTable)
+          .where
+          .in(groupTable.name, groupNames.map(_.value).toSeq)
+      }.map(rs => (rs.get[WorkbenchGroupName]("name"), rs.get[WorkbenchEmail]("email"))).list().apply().toStream
+    }
+  }
+
+  override def deleteGroup(groupName: WorkbenchGroupName): IO[Unit] = {
+    runInTransaction { implicit session =>
+      val groupTable = GroupTable.syntax("g")
+
+      // foreign keys in accessInstructions and groupMember tables are set to cascade delete
+      // note: this will not remove this group from any parent groups and will throw a
+      // foreign key constraint violation error if group is still a member of any parent groups
+      withSQL {
+        delete
+          .from(GroupTable as groupTable)
+          .where
+          .eq(groupTable.name, groupName)
+      }.update().apply()
+    }
+  }
 
   /**
     * @return true if the subject was added, false if it was already there
     */
-  override def addGroupMember(groupId: WorkbenchGroupIdentity, addMember: WorkbenchSubject): IO[Boolean] = ???
+  override def addGroupMember(groupId: WorkbenchGroupIdentity, addMember: WorkbenchSubject): IO[Boolean] = {
+    runInTransaction { implicit session =>
+      val groupPKQuery = workbenchGroupIdentityToGroupPK(groupId)
+      val groupMemberColumn = GroupMemberTable.column
+
+      val addMemberQuery = addMember match {
+        case memberUser: WorkbenchUserId =>
+          sql"insert into ${GroupMemberTable.table} (${groupMemberColumn.groupId}, ${groupMemberColumn.memberUserId}) values (($groupPKQuery), ${memberUser.value})"
+        case memberGroup: WorkbenchGroupIdentity =>
+          val memberGroupPKQuery = workbenchGroupIdentityToGroupPK(memberGroup)
+          sql"insert into ${GroupMemberTable.table} (${groupMemberColumn.groupId}, ${groupMemberColumn.memberGroupId}) values ((${groupPKQuery}), (${memberGroupPKQuery}))"
+        case _ => throw new WorkbenchException(s"unexpected WorkbenchSubject $addMember")
+      }
+      addMemberQuery.update().apply() > 0
+    }
+  }
+
+  private def workbenchGroupIdentityToGroupPK(groupId: WorkbenchGroupIdentity): SQLSyntax = {
+    groupId match {
+      case group: WorkbenchGroupName => groupPKQueryForGroup(group)
+      case policy: FullyQualifiedPolicyId => groupPKQueryForPolicy(policy)
+      case _ => throw new WorkbenchException(s"unexpected WorkbenchGroupIdentity $groupId")
+    }
+  }
+
+  private def groupPKQueryForGroup(groupName: WorkbenchGroupName): SQLSyntax = {
+    val groupTable = GroupTable.syntax("gpk")
+
+    sqls"SELECT ${groupTable.id} from ${GroupTable.as(groupTable)} where ${groupTable.name} = ${groupName.value}"
+  }
+
+  private def groupPKQueryForPolicy(policyId: FullyQualifiedPolicyId): SQLSyntax = {
+    val resourceTable = ResourceTable.syntax("r")
+    val policyTable = PolicyTable.syntax("p")
+    // TODO: untested, needs to be done after implementing access policy DAO
+    sqls"""SELECT ${policyTable.groupId} from ${PolicyTable.as(policyTable)}
+           join ${ResourceTable.as(resourceTable)} on ${policyTable.resourceId} = ${resourceTable.id}
+           where ${policyTable.name} = ${policyId.accessPolicyName.value}
+           and ${resourceTable.name} = ${policyId.resource.resourceId}"""
+  }
 
   /**
     * @return true if the subject was removed, false if it was already gone
     */
-  override def removeGroupMember(groupId: WorkbenchGroupIdentity, removeMember: WorkbenchSubject): IO[Boolean] = ???
+  override def removeGroupMember(groupId: WorkbenchGroupIdentity, removeMember: WorkbenchSubject): IO[Boolean] = {
+    runInTransaction { implicit session =>
+      val groupPKQuery = workbenchGroupIdentityToGroupPK(groupId)
+      val groupMemberColumn = GroupMemberTable.column
+
+      val removeMemberQuery = removeMember match {
+        case memberUser: WorkbenchUserId =>
+          sql"delete from ${GroupMemberTable.table} where ${groupMemberColumn.groupId} = (${groupPKQuery}) and ${groupMemberColumn.memberUserId} = ${memberUser.value}"
+        case memberGroup: WorkbenchGroupIdentity =>
+          val memberGroupPKQuery = workbenchGroupIdentityToGroupPK(memberGroup)
+          sql"delete from ${GroupMemberTable.table} where ${groupMemberColumn.groupId} = (${groupPKQuery}) and ${groupMemberColumn.memberGroupId} = (${memberGroupPKQuery})"
+        case _ => throw new WorkbenchException(s"unexpected WorkbenchSubject $removeMember")
+      }
+      removeMemberQuery.update().apply() > 0
+    }
+  }
 
   override def isGroupMember(groupId: WorkbenchGroupIdentity, member: WorkbenchSubject): IO[Boolean] = ???
 
@@ -170,13 +297,48 @@ class PostgresDirectoryDAO(protected val dbRef: DbReference,
 
   override def loadSubjectFromGoogleSubjectId(googleSubjectId: GoogleSubjectId): IO[Option[WorkbenchSubject]] = ???
 
-  override def createUser(user: WorkbenchUser): IO[WorkbenchUser] = ???
+  override def createUser(user: WorkbenchUser): IO[WorkbenchUser] = {
+    runInTransaction { implicit session =>
+      val userColumn = UserTable.column
 
-  override def loadUser(userId: WorkbenchUserId): IO[Option[WorkbenchUser]] = ???
+      val insertUserQuery = sql"insert into ${UserTable.table} (${userColumn.id}, ${userColumn.email}, ${userColumn.googleSubjectId}) values (${user.id.value}, ${user.email.value}, ${user.googleSubjectId.map(_.value)})"
+      insertUserQuery.update.apply
+      user
+    }
+  }
 
-  override def loadUsers(userIds: Set[WorkbenchUserId]): IO[Stream[WorkbenchUser]] = ???
+  override def loadUser(userId: WorkbenchUserId): IO[Option[WorkbenchUser]] = {
+    runInTransaction { implicit session =>
+      val userTable = UserTable.syntax
+      val userColumn = UserTable.column
 
-  override def deleteUser(userId: WorkbenchUserId): IO[Unit] = ???
+      import SamTypeBinders._
+      val loadUserQuery = sql"select ${userTable.id}, ${userTable.email}, ${userTable.googleSubjectId} from ${UserTable.table} where ${userTable.id} = ${userId.value}"
+      loadUserQuery.map(rs => WorkbenchUser(rs.get[WorkbenchUserId](userColumn.id), rs.stringOpt(userColumn.googleSubjectId).map(GoogleSubjectId), rs.get[WorkbenchEmail](userColumn.email)))
+        .single().apply()
+    }
+  }
+
+  override def loadUsers(userIds: Set[WorkbenchUserId]): IO[Stream[WorkbenchUser]] = {
+    runInTransaction { implicit session =>
+      val userTable = UserTable.syntax
+      val userColumn = UserTable.column
+
+      import SamTypeBinders._
+
+      val loadUsersQuery = sql"select ${userTable.id}, ${userTable.email}, ${userTable.googleSubjectId} from ${UserTable.table} where ${userTable.id} in (${userIds.map(_.value)})"
+      loadUsersQuery.map(rs => WorkbenchUser(rs.get[WorkbenchUserId](userColumn.id), rs.stringOpt(userColumn.googleSubjectId).map(GoogleSubjectId), rs.get[WorkbenchEmail](userColumn.email)))
+        .list().apply().toStream
+    }
+  }
+
+  // Not worrying about cascading deletion of user's pet SAs because LDAP doesn't delete user's pet SAs automatically
+  override def deleteUser(userId: WorkbenchUserId): IO[Unit] = {
+    runInTransaction { implicit session =>
+      val userTable = UserTable.syntax
+      sql"delete from ${UserTable.table} where ${userTable.id} = ${userId.value}".update().apply()
+    }
+  }
 
   override def addProxyGroup(userId: WorkbenchUserId, proxyEmail: WorkbenchEmail): IO[Unit] = ???
 
@@ -198,7 +360,16 @@ class PostgresDirectoryDAO(protected val dbRef: DbReference,
 
   override def getUserFromPetServiceAccount(petSA: ServiceAccountSubjectId): IO[Option[WorkbenchUser]] = ???
 
-  override def createPetServiceAccount(petServiceAccount: PetServiceAccount): IO[PetServiceAccount] = ???
+  override def createPetServiceAccount(petServiceAccount: PetServiceAccount): IO[PetServiceAccount] = {
+    runInTransaction { implicit session =>
+      val petServiceAccountColumn = PetServiceAccountTable.column
+
+      sql"""insert into ${PetServiceAccountTable.table} (${petServiceAccountColumn.userId}, ${petServiceAccountColumn.project}, ${petServiceAccountColumn.googleSubjectId}, ${petServiceAccountColumn.email})
+           values (${petServiceAccount.id.userId.value}, ${petServiceAccount.id.project.value}, ${petServiceAccount.serviceAccount.subjectId.value}, ${petServiceAccount.serviceAccount.email.value})"""
+        .update().apply()
+      petServiceAccount
+    }
+  }
 
   override def loadPetServiceAccount(petServiceAccountId: PetServiceAccountId): IO[Option[PetServiceAccount]] = ???
 
--- src/test/scala/org/broadinstitute/dsde/workbench/sam/directory/PostgresDirectoryDAOSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/directory/PostgresDirectoryDAOSpec.scala
@@ -1,6 +1,7 @@
 package org.broadinstitute.dsde.workbench.sam.directory
 
-import org.broadinstitute.dsde.workbench.model.{WorkbenchEmail, WorkbenchGroupName, WorkbenchSubject}
+import org.broadinstitute.dsde.workbench.model._
+import org.broadinstitute.dsde.workbench.model.google.{GoogleProject, ServiceAccount, ServiceAccountDisplayName, ServiceAccountSubjectId}
 import org.broadinstitute.dsde.workbench.sam.TestSupport
 import org.broadinstitute.dsde.workbench.sam.model.BasicWorkbenchGroup
 import org.postgresql.util.PSQLException
@@ -13,12 +14,14 @@ class PostgresDirectoryDAOSpec extends FlatSpec with Matchers with BeforeAndAfte
 
   val defaultGroupName = WorkbenchGroupName("group")
   val defaultGroup = BasicWorkbenchGroup(defaultGroupName, Set.empty, WorkbenchEmail("foo@bar.com"))
+  val defaultUserId = WorkbenchUserId("testUser")
+  val defaultUser = WorkbenchUser(defaultUserId, Option(GoogleSubjectId("testGoogleSubject")), WorkbenchEmail("user@foo.com"))
 
   override protected def beforeEach(): Unit = {
     TestSupport.truncateAll
   }
 
-  "PostgresDirectoryDAO" should "create a group" in {
+"PostgresDirectoryDAO\ncreateGroup" should "create a group" in {
     dao.createGroup(defaultGroup).unsafeRunSync() shouldEqual defaultGroup
   }
 
@@ -48,7 +51,18 @@ class PostgresDirectoryDAOSpec extends FlatSpec with Matchers with BeforeAndAfte
     loadedGroup.members shouldEqual members
   }
 
-  it should "load a group" in {
+  it should "not allow nonexistent group members" in {
+    val subGroup1 = defaultGroup
+    val subGroup2 = BasicWorkbenchGroup(WorkbenchGroupName("subGroup2"), Set.empty, WorkbenchEmail("bar@baz.com"))
+    val members: Set[WorkbenchSubject] = Set(subGroup1.id, subGroup2.id)
+    val parentGroup = BasicWorkbenchGroup(WorkbenchGroupName("parentGroup"), members, WorkbenchEmail("baz@qux.com"))
+
+    assertThrows[WorkbenchExceptionWithErrorReport] {
+      dao.createGroup(parentGroup).unsafeRunSync()
+    }
+  }
+
+  "loadGroup" should "load a group" in {
     dao.createGroup(defaultGroup).unsafeRunSync()
     val loadedGroup = dao.loadGroup(defaultGroup.id).unsafeRunSync().getOrElse(fail(s"Failed to load group $defaultGroupName"))
     loadedGroup shouldEqual defaultGroup
@@ -56,5 +70,174 @@ class PostgresDirectoryDAOSpec extends FlatSpec with Matchers with BeforeAndAfte
 
   it should "return None when loading a nonexistent group" in {
     dao.loadGroup(WorkbenchGroupName("fakeGroup")).unsafeRunSync() shouldBe None
+  }
+
+  "loadGroupEmail" should "load a group's email" in {
+    dao.createGroup(defaultGroup).unsafeRunSync()
+    val loadedEmail = dao.loadGroupEmail(defaultGroup.id).unsafeRunSync().getOrElse(fail(s"Failed to load group ${defaultGroup.id}"))
+    loadedEmail shouldEqual defaultGroup.email
+  }
+
+  it should "return None when trying to load the email for a nonexistent group" in {
+    dao.loadGroupEmail(WorkbenchGroupName("fakeGroup")).unsafeRunSync() shouldBe None
+  }
+
+  "deleteGroup" should "delete groups" in {
+    dao.createGroup(defaultGroup).unsafeRunSync()
+
+    val loadedGroup = dao.loadGroup(defaultGroup.id).unsafeRunSync().getOrElse(fail(s"Failed to load group $defaultGroupName"))
+    loadedGroup shouldEqual defaultGroup
+
+    dao.deleteGroup(defaultGroup.id).unsafeRunSync()
+
+    dao.loadGroup(defaultGroup.id).unsafeRunSync() shouldBe None
+  }
+
+  it should "not delete a group that is still a member of another group" in {
+    val subGroup = defaultGroup.copy(id = WorkbenchGroupName("subGroup"))
+    val parentGroup = BasicWorkbenchGroup(WorkbenchGroupName("parentGroup"), Set(subGroup.id), WorkbenchEmail("bar@baz.com"))
+
+    dao.createGroup(subGroup).unsafeRunSync()
+    dao.createGroup(parentGroup).unsafeRunSync()
+
+    assertThrows[PSQLException] {
+      dao.deleteGroup(subGroup.id).unsafeRunSync()
+    }
+
+    dao.loadGroup(subGroup.id).unsafeRunSync() shouldEqual Option(subGroup)
+  }
+
+  "loadGroups" should "load multiple groups" in {
+    val group1 = BasicWorkbenchGroup(WorkbenchGroupName("group1"), Set.empty, WorkbenchEmail("group1@foo.com"))
+    val group2 = BasicWorkbenchGroup(WorkbenchGroupName("group2"), Set.empty, WorkbenchEmail("group2@foo.com"))
+
+    dao.createGroup(group1).unsafeRunSync()
+    dao.createGroup(group2).unsafeRunSync()
+
+    dao.loadGroups(Set(group1.id, group2.id)).unsafeRunSync() should contain theSameElementsAs Set(group1, group2)
+  }
+
+  it should "handle loading nonexistent groups" in {
+    val group1 = BasicWorkbenchGroup(WorkbenchGroupName("group1"), Set.empty, WorkbenchEmail("group1@foo.com"))
+    val group2 = BasicWorkbenchGroup(WorkbenchGroupName("group2"), Set.empty, WorkbenchEmail("group2@foo.com"))
+
+    dao.createGroup(group1).unsafeRunSync()
+    dao.createGroup(group2).unsafeRunSync()
+
+    dao.loadGroups(Set(group1.id, group2.id, WorkbenchGroupName("fakeGroup"))).unsafeRunSync() should contain theSameElementsAs Set(group1, group2)
+  }
+
+  private def emptyWorkbenchGroup(groupName: String): BasicWorkbenchGroup = BasicWorkbenchGroup(WorkbenchGroupName(groupName), Set.empty, WorkbenchEmail(s"$groupName@test.com"))
+
+  "addGroupMember" should "add groups to other groups" in {
+    val subGroup = emptyWorkbenchGroup("subGroup")
+    dao.createGroup(defaultGroup).unsafeRunSync()
+    dao.createGroup(subGroup).unsafeRunSync()
+
+    dao.addGroupMember(defaultGroup.id, subGroup.id).unsafeRunSync() shouldBe true
+
+    val loadedGroup = dao.loadGroup(defaultGroup.id).unsafeRunSync().getOrElse(fail(s"failed to load group ${defaultGroup.id}"))
+    loadedGroup.members should contain theSameElementsAs Set(subGroup.id)
+  }
+
+  it should "add users to groups" in {
+    dao.createGroup(defaultGroup).unsafeRunSync()
+    dao.createUser(defaultUser).unsafeRunSync()
+
+    dao.addGroupMember(defaultGroup.id, defaultUser.id).unsafeRunSync() shouldBe true
+
+    val loadedGroup = dao.loadGroup(defaultGroup.id).unsafeRunSync().getOrElse(fail(s"failed to load group ${defaultGroup.id}"))
+    loadedGroup.members should contain theSameElementsAs Set(defaultUser.id)
+  }
+
+  // TODO: pending policy creation implementation
+  it should "add policies to groups" is pending
+  it should "add groups to policies" is pending
+  it should "add users to policies" is pending
+  it should "add policies to other policies" is pending
+
+  "batchLoadGroupEmail" should "batch load multiple group emails" in {
+    val group1 = emptyWorkbenchGroup("group1")
+    val group2 = emptyWorkbenchGroup("group2")
+
+    dao.createGroup(group1).unsafeRunSync()
+    dao.createGroup(group2).unsafeRunSync()
+
+    dao.batchLoadGroupEmail(Set(group1.id, group2.id)).unsafeRunSync() should contain theSameElementsAs Set(group1, group2).map(group => (group.id, group.email))
+  }
+
+  "removeGroupMember" should "remove groups from other groups" in {
+    val subGroup = emptyWorkbenchGroup("subGroup")
+    dao.createGroup(defaultGroup).unsafeRunSync()
+    dao.createGroup(subGroup).unsafeRunSync()
+
+    dao.addGroupMember(defaultGroup.id, subGroup.id).unsafeRunSync() shouldBe true
+    dao.removeGroupMember(defaultGroup.id, subGroup.id).unsafeRunSync() shouldBe true
+
+    val loadedGroup = dao.loadGroup(defaultGroup.id).unsafeRunSync().getOrElse(fail(s"failed to load group ${defaultGroup.id}"))
+    loadedGroup.members shouldBe empty
+  }
+
+  it should "remove users from groups" in {
+    dao.createGroup(defaultGroup).unsafeRunSync()
+    dao.createUser(defaultUser).unsafeRunSync()
+
+    dao.addGroupMember(defaultGroup.id, defaultUser.id).unsafeRunSync() shouldBe true
+    dao.removeGroupMember(defaultGroup.id, defaultUser.id).unsafeRunSync() shouldBe true
+
+    val loadedGroup = dao.loadGroup(defaultGroup.id).unsafeRunSync().getOrElse(fail(s"failed to load group ${defaultGroup.id}"))
+    loadedGroup.members shouldBe empty
+  }
+
+  // TODO: pending policy creation implementation
+  it should "remove policies from groups" is pending
+  it should "remove groups from policies" is pending
+  it should "remove users from policies" is pending
+  it should "remove policies from other policies" is pending
+
+  "createUser and loadUser" should "create and load a user" in {
+    dao.createUser(defaultUser).unsafeRunSync() shouldEqual defaultUser
+    val loadedUser = dao.loadUser(defaultUser.id).unsafeRunSync().getOrElse(fail(s"failed to load user ${defaultUser.id}"))
+    loadedUser shouldEqual defaultUser
+  }
+
+  "deleteUser" should "delete users" in {
+    dao.createUser(defaultUser).unsafeRunSync() shouldEqual defaultUser
+    val loadedUser = dao.loadUser(defaultUser.id).unsafeRunSync().getOrElse(fail(s"failed to load user ${defaultUser.id}"))
+    loadedUser shouldEqual defaultUser
+    dao.deleteUser(defaultUser.id).unsafeRunSync()
+    dao.loadUser(defaultUser.id).unsafeRunSync() shouldBe None
+  }
+
+  it should "not delete a user that is still a member of a group" in {
+    val user = defaultUser
+    val parentGroup = BasicWorkbenchGroup(WorkbenchGroupName("parentGroup"), Set(user.id), WorkbenchEmail("bar@baz.com"))
+
+    dao.createUser(user).unsafeRunSync()
+    dao.createGroup(parentGroup).unsafeRunSync()
+
+    assertThrows[PSQLException] {
+      dao.deleteUser(user.id).unsafeRunSync()
+    }
+
+    dao.loadUser(user.id).unsafeRunSync() shouldEqual Option(user)
+  }
+
+  it should "not delete a user's pet SA when the user is deleted" is pending
+
+  "loadUsers" should "load multiple users at once" in {
+    val user1 = defaultUser
+    val user2 = WorkbenchUser(WorkbenchUserId("testUser2"), Option(GoogleSubjectId("testGoogleSubjectId2")), WorkbenchEmail("user2@test.com"))
+
+    dao.createUser(user1).unsafeRunSync() shouldEqual user1
+    dao.createUser(user2).unsafeRunSync() shouldEqual user2
+
+    dao.loadUsers(Set(user1.id, user2.id)).unsafeRunSync() should contain theSameElementsAs Set(user1, user2)
+  }
+
+  "createPetServiceAccount" should "create pet service accounts" in {
+    val petSA = PetServiceAccount(PetServiceAccountId(defaultUser.id, GoogleProject("testProject")), ServiceAccount(ServiceAccountSubjectId("testGoogleSubjectId"), WorkbenchEmail("test@pet.co"), ServiceAccountDisplayName("whoCares")))
+    dao.createUser(defaultUser).unsafeRunSync()
+    dao.createPetServiceAccount(petSA).unsafeRunSync() shouldBe petSA
   }
 }
