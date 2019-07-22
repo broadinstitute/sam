3b9742516351e7b2c559fb89bf84cc38b786a555
dvoet <dvoet@users.noreply.github.com>
2019-07-22 13:30:51 -0400

    implemented isGroupMember and tests (#315)

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/db/SamParameterBinderFactory.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/db/SamParameterBinderFactory.scala
@@ -1,7 +1,8 @@
 package org.broadinstitute.dsde.workbench.sam.db
 
 import org.broadinstitute.dsde.workbench.model.ValueObject
-import scalikejdbc.ParameterBinderFactory
+import scalikejdbc.{NoExtractor, ParameterBinderFactory, SQL, SQLInterpolationString}
+import scalikejdbc.interpolation.SQLSyntax
 
 object SamParameterBinderFactory {
   implicit def databaseKeyPbf[T <: DatabaseKey]: ParameterBinderFactory[T] = ParameterBinderFactory[T] {
@@ -10,5 +11,31 @@ object SamParameterBinderFactory {
 
   implicit def valueObjectPbf[T <: ValueObject]: ParameterBinderFactory[T] = ParameterBinderFactory[T] {
     value => (stmt, idx) => stmt.setString(idx, value.value)
+  }
+
+  implicit class SqlInterpolationWithSamBinders(val sc: StringContext) extends AnyVal {
+    def samsql[A](params: Any*): SQL[A, NoExtractor] = {
+      new SQLInterpolationString(sc).sql(addParameterBinders(params):_*)
+    }
+
+    def samsqls(params: Any*): SQLSyntax = {
+      new SQLInterpolationString(sc).sqls(addParameterBinders(params):_*)
+    }
+
+    /**
+      * Iterates through all params and replaces any with one of the above binders if appropriate.
+      * Called recursively for Option and Traversable.
+      * @param args
+      * @return
+      */
+    private def addParameterBinders(args: Seq[Any]): Seq[Any] = {
+      args.map {
+        case v: ValueObject => valueObjectPbf(v)
+        case pk: DatabaseKey => databaseKeyPbf(pk)
+        case t: Traversable[Any] => addParameterBinders(t.toSeq)
+        case opt: Option[Any] => opt.flatMap(value => addParameterBinders(Seq(value)).headOption)
+        case a => a
+      }
+    }
   }
 }
