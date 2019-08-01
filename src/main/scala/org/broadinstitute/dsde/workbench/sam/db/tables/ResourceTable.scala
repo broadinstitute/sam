f9001310bb587405b5fb5636e0dcab950d54ee43
Anu Singh <ansingh@broadinstitute.org>
2019-08-01 10:40:32 -0400

    AccessPolicyDAO createResource and createResourceType (#324)
    * convert remaining Futures in directory dao to IO (#304)
    
    * create resource type
    
    * Added PostgresAccessPolicyDAOSpec and first test
    
    * Adding createResource method with support for authDomains
    
    * Broken code - error creating auth domains
    
    * Fixed the test for adding a resource with Auth Domains
    
    * some review suggestions, not all
    
    * createResourceType should add roles, actions, and actionPatterns
    
    * remove mkstring
    
    * try without lazy
    
    * Removing DbReferenceSpec to fix tests that were failing because this spec destroyed our static DbReference instance.  (Also a few minor tweaks to the README)
    
    * implement loadResourceAuthDomain (#332)
    
    * implement loadResourceAuthDomain
    
    * make it lazy again

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/db/tables/ResourceTable.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/db/tables/ResourceTable.scala
@@ -1,7 +1,8 @@
 package org.broadinstitute.dsde.workbench.sam.db.tables
 
 import org.broadinstitute.dsde.workbench.sam.db.{DatabaseKey, SamTypeBinders}
-import org.broadinstitute.dsde.workbench.sam.model.ResourceId
+import org.broadinstitute.dsde.workbench.sam.model.{ResourceId, ResourceTypeName}
+import org.broadinstitute.dsde.workbench.sam.db.SamParameterBinderFactory.SqlInterpolationWithSamBinders
 import scalikejdbc._
 
 final case class ResourcePK(value: Long) extends DatabaseKey
@@ -18,4 +19,11 @@ object ResourceTable extends SQLSyntaxSupport[ResourceRecord] {
     rs.get(e.name),
     rs.get(e.resourceTypeId)
   )
+
+  def pkQuery(resourceId: ResourceId, resourceTypeName: ResourceTypeName, resourceTableAlias: String = "r"): SQLSyntax = {
+    val r = ResourceTable.syntax(resourceTableAlias)
+    samsqls"""select ${r.id}
+              from ${ResourceTable as r}
+              where ${r.name} = ${resourceId} and ${r.resourceTypeId} = (${ResourceTypeTable.pkQuery(resourceTypeName)})"""
+  }
 }
---
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/openam/PostgresAccessPolicyDAO.scala
@@ -0,0 +1,199 @@
+package org.broadinstitute.dsde.workbench.sam.openam
+
+import akka.http.scaladsl.model.StatusCodes
+import cats.data.NonEmptyList
+import cats.effect.{ContextShift, IO}
+import cats.implicits._
+import com.typesafe.scalalogging.LazyLogging
+import org.broadinstitute.dsde.workbench.sam.errorReportSource
+import org.broadinstitute.dsde.workbench.model._
+import org.broadinstitute.dsde.workbench.sam.db.DbReference
+import org.broadinstitute.dsde.workbench.sam.db.SamParameterBinderFactory._
+import org.broadinstitute.dsde.workbench.sam.db.tables._
+import org.broadinstitute.dsde.workbench.sam.model._
+import org.broadinstitute.dsde.workbench.sam.openam.LoadResourceAuthDomainResult.{Constrained, NotConstrained, ResourceNotFound}
+import org.broadinstitute.dsde.workbench.sam.util.DatabaseSupport
+import org.postgresql.util.PSQLException
+import scalikejdbc._
+
+import scala.concurrent.ExecutionContext
+
+class PostgresAccessPolicyDAO(protected val dbRef: DbReference,
+                              protected val ecForDatabaseIO: ExecutionContext)(implicit executionContext: ExecutionContext) extends AccessPolicyDAO with DatabaseSupport with LazyLogging {
+
+  implicit val cs: ContextShift[IO] = IO.contextShift(executionContext)
+
+  // Create resourceType should do the following:
+  // 1. Create the resource type
+  // 2. Add the roles
+  // 3. Add the actions
+  // 4. Add patterns
+  def createResourceType(resourceType: ResourceType): IO[ResourceType] = {
+    runInTransaction { implicit session =>
+      val resourceTypePK = insertResourceType(resourceType.name)
+      insertRolesAndActions(resourceType.roles, resourceTypePK)
+      insertActionPatterns(resourceType.actionPatterns, resourceTypePK)
+      resourceType
+    }
+  }
+
+  private def insertRolesAndActions(roles: Set[ResourceRole], resourceTypePK: ResourceTypePK)(implicit session: DBSession) = {
+    val resourceRoleTableColumn = ResourceRoleTable.column
+    val resourceActionTableColumn = ResourceActionTable.column
+    val roleActionTableColumn = RoleActionTable.column
+    val r  = ResourceRoleTable.syntax("r")
+    val a  = ResourceActionTable.syntax("a")
+    val ra = RoleActionTable.syntax("ra")
+
+    val uniqueActions = roles.map(_.actions).flatten
+    val actionPKs = uniqueActions map { action =>
+        val insertActionQuery =
+          samsql"""insert into ${ResourceActionTable.table}
+                   (${resourceActionTableColumn.resourceTypeId}, ${resourceActionTableColumn.action})
+                   values (${resourceTypePK}, ${action})"""
+
+        ResourceActionPK(insertActionQuery.updateAndReturnGeneratedKey().apply())
+    }
+
+    // 1. Add role to ResourceRole table
+    // 2. Add every action for each role to the ResourceAction table
+    // 3. Add each Resource Role and Resource Action to the RoleAction table <-- Is this what actually happens?
+    roles map { role =>
+      val insertRoleQuery =
+        samsql"""insert into ${ResourceRoleTable.table}
+                 (${resourceRoleTableColumn.resourceTypeId}, ${resourceRoleTableColumn.role})
+                 values (${resourceTypePK}, ${role.roleName})"""
+      val resourceRolePK = ResourceRolePK(insertRoleQuery.updateAndReturnGeneratedKey().apply())
+
+        val insertRoleActionQuery =
+          samsql"""insert into ${RoleActionTable.table}
+                   select ${r.id}, ${a.id}
+                   from ${ResourceRoleTable as r}, ${ResourceActionTable as a}
+                   where ${r.resourceTypeId} = ${resourceTypePK}
+                   and   ${a.resourceTypeId} = ${resourceTypePK}
+                   and   ${r.role}   = ${role.roleName}
+                   and   ${a.action} IN (${role.actions})
+            """
+        insertRoleActionQuery.update().apply()
+      }
+  }
+
+  private def insertActionPatterns(actionPatterns: Set[ResourceActionPattern], resourceTypePK: ResourceTypePK)(implicit session: DBSession) = {
+    val resourceActionPatternTableColumn = ResourceActionPatternTable.column
+    val actionPatternValues = actionPatterns map { actionPattern =>
+      samsqls"(${resourceTypePK}, ${actionPattern.value})"
+    }
+    val actionPatternQuery =
+      samsql"""insert into ${ResourceActionPatternTable.table}
+              (${resourceActionPatternTableColumn.resourceTypeId}, ${resourceActionPatternTableColumn.actionPattern})
+              values ${actionPatternValues}"""
+    actionPatternQuery.update().apply()
+  }
+
+  private def insertResourceType(resourceTypeName: ResourceTypeName)(implicit session: DBSession): ResourceTypePK = {
+    val resourceTypeTableColumn = ResourceTypeTable.column
+    val insertResourceTypeQuery = samsql"""insert into ${ResourceTypeTable.table} (${resourceTypeTableColumn.name}) values (${resourceTypeName.value})"""
+
+    ResourceTypePK(insertResourceTypeQuery.updateAndReturnGeneratedKey().apply())
+  }
+
+  // 1. Create Resource
+  // 2. Create the entries in the join table for the auth domains
+  def createResource(resource: Resource): IO[Resource] = {
+    runInTransaction { implicit session =>
+      val resourcePK = insertResource(resource)
+
+      if (resource.authDomain.nonEmpty) {
+        insertAuthDomainsForResource(resourcePK, resource.authDomain)
+      }
+
+      resource
+    }.recoverWith {
+      case sqlException: PSQLException => {
+        logger.debug(s"createResource psql exception on resource $resource", sqlException)
+        IO.raiseError(new WorkbenchExceptionWithErrorReport(ErrorReport(StatusCodes.Conflict, s"Resource ${resource.resourceTypeName} failed.", sqlException)))
+      }
+    }
+  }
+
+  private def insertResource(resource: Resource)(implicit session: DBSession): ResourcePK = {
+    val resourceTableColumn = ResourceTable.column
+    val insertResourceQuery =
+      samsql"""insert into ${ResourceTable.table} (${resourceTableColumn.name}, ${resourceTableColumn.resourceTypeId})
+               values (${resource.resourceId}, (${loadResourceTypePK(resource.resourceTypeName)}))"""
+    ResourcePK(insertResourceQuery.updateAndReturnGeneratedKey().apply())
+  }
+
+  private def insertAuthDomainsForResource(resourcePK: ResourcePK, authDomains: Set[WorkbenchGroupName])(implicit session: DBSession): Int = {
+    val authDomainValues = authDomains.map { authDomain =>
+      samsqls"(${resourcePK}, (${GroupTable.groupPKQueryForGroup(authDomain)}))"
+    }
+
+    val authDomainColumn = AuthDomainTable.column
+    val insertAuthDomainQuery = samsql"insert into ${AuthDomainTable.table} (${authDomainColumn.resourceId}, ${authDomainColumn.groupId}) values ${authDomainValues}"
+    insertAuthDomainQuery.update().apply()
+  }
+
+  private def loadResourceTypePK(resourceTypeName: ResourceTypeName, resourceTypeTableAlias: String = "rt"): SQLSyntax = {
+    val rt = ResourceTypeTable.syntax(resourceTypeTableAlias)
+    samsqls"""select ${rt.id} from ${ResourceTypeTable as rt} where ${rt.name} = ${resourceTypeName}"""
+  }
+
+  def deleteResource(resource: FullyQualifiedResourceId): IO[Unit] = ???
+
+  def loadResourceAuthDomain(resource: FullyQualifiedResourceId): IO[LoadResourceAuthDomainResult] = {
+    runInTransaction { implicit session =>
+      val ad = AuthDomainTable.syntax("ad")
+      val r = ResourceTable.syntax("r")
+      val rt = ResourceTypeTable.syntax("rt")
+      val g = GroupTable.syntax("g")
+
+      // left joins below so we can detect the difference between a resource does not exist vs.
+      // a resource exists but does not have any auth domains
+      val query = samsql"""select ${g.result.name}
+              from ${ResourceTable as r}
+              join ${ResourceTypeTable as rt} on ${r.resourceTypeId} = ${rt.id}
+              left outer join ${AuthDomainTable as ad} on ${r.id} = ${ad.resourceId}
+              left outer join ${GroupTable as g} on  ${ad.groupId} = ${g.id}
+              where ${r.name} = ${resource.resourceId} and ${rt.name} = ${resource.resourceTypeName}"""
+
+      val results = query.map(_.stringOpt(g.resultName.name)).list().apply()
+
+      // there are 3 expected forms for the results:
+      //   1) empty list - nothing matched inner joins
+      //   2) 1 row with a None - nothing matched left joins
+      //   3) non-empty list of non-None group names
+      // there are 2 unexpected forms
+      //   a) more than one row, all Nones
+      //   b) more than one row, some Nones
+      // a is treated like 2 and b is treated like 3 with Nones removed
+      val authDomains = NonEmptyList.fromList(results.flatten) // flatten removes Nones
+      authDomains match {
+        case None =>
+          if (results.isEmpty) {
+            ResourceNotFound // case 1
+          } else {
+            NotConstrained  // case 2
+          }
+        case Some(nel) => Constrained(nel.map(WorkbenchGroupName)) // case 3
+      }
+    }
+  }
+
+  def listResourcesConstrainedByGroup(groupId: WorkbenchGroupIdentity): IO[Set[Resource]] = ???
+  def createPolicy(policy: AccessPolicy): IO[AccessPolicy] = ???
+  def deletePolicy(policy: FullyQualifiedPolicyId): IO[Unit] = ???
+  def loadPolicy(resourceAndPolicyName: FullyQualifiedPolicyId): IO[Option[AccessPolicy]] = ???
+  def overwritePolicyMembers(id: FullyQualifiedPolicyId, memberList: Set[WorkbenchSubject]): IO[Unit] = ???
+  def overwritePolicy(newPolicy: AccessPolicy): IO[AccessPolicy] = ???
+  def listPublicAccessPolicies(resourceTypeName: ResourceTypeName): IO[Stream[ResourceIdAndPolicyName]] = ???
+  def listPublicAccessPolicies(resource: FullyQualifiedResourceId): IO[Stream[AccessPolicy]] = ???
+  def listResourcesWithAuthdomains(resourceTypeName: ResourceTypeName, resourceId: Set[ResourceId]): IO[Set[Resource]] = ???
+  def listResourceWithAuthdomains(resourceId: FullyQualifiedResourceId): IO[Option[Resource]] = ???
+  def listAccessPolicies(resourceTypeName: ResourceTypeName, userId: WorkbenchUserId): IO[Set[ResourceIdAndPolicyName]] = ???
+  def listAccessPolicies(resource: FullyQualifiedResourceId): IO[Stream[AccessPolicy]] = ???
+  def listAccessPoliciesForUser(resource: FullyQualifiedResourceId, user: WorkbenchUserId): IO[Set[AccessPolicy]] = ???
+  def listFlattenedPolicyMembers(policyId: FullyQualifiedPolicyId): IO[Set[WorkbenchUser]] = ???
+  def setPolicyIsPublic(policyId: FullyQualifiedPolicyId, isPublic: Boolean): IO[Unit] = ???
+  def evictIsMemberOfCache(subject: WorkbenchSubject): IO[Unit] = ???
+}
