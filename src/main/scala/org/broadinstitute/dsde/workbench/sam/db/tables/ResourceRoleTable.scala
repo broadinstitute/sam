e2c8058a2e2522bb0e392ffbdf4e4a30192d2701
Marcus Talbott <mtalbott@broadinstitute.org>
2019-09-03 15:55:28 -0400

    add deprecated column to resource role table

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/db/tables/ResourceRoleTable.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/db/tables/ResourceRoleTable.scala
@@ -7,7 +7,8 @@ import scalikejdbc._
 final case class ResourceRolePK(value: Long) extends DatabaseKey
 final case class ResourceRoleRecord(id: ResourceRolePK,
                                     resourceTypeId: ResourceTypePK,
-                                    role: ResourceRoleName)
+                                    role: ResourceRoleName,
+                                    deprecated: Boolean)
 
 object ResourceRoleTable extends SQLSyntaxSupportWithDefaultSamDB[ResourceRoleRecord] {
   override def tableName: String = "SAM_RESOURCE_ROLE"
@@ -16,6 +17,7 @@ object ResourceRoleTable extends SQLSyntaxSupportWithDefaultSamDB[ResourceRoleRe
   def apply(e: ResultName[ResourceRoleRecord])(rs: WrappedResultSet): ResourceRoleRecord = ResourceRoleRecord(
     rs.get(e.id),
     rs.get(e.resourceTypeId),
-    rs.get(e.role)
+    rs.get(e.role),
+    rs.get(e.deprecated)
   )
 }
