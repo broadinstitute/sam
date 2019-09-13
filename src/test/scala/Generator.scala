66a23246637a8fedf3de38a86fd1d4a3e09ff529
Douglas Voet <dvoet@users.noreply.github.com>
2019-09-13 10:07:00 -0400

    fix service tests

--- src/test/scala/Generator.scala
+++ src/test/scala/Generator.scala
@@ -78,7 +78,7 @@ object Generator {
   val genAccessPolicyName : Gen[AccessPolicyName] = Gen.oneOf("member", "admin", "admin-notifier").map(AccessPolicyName.apply) //there might be possible values
   def genAuthDomains: Gen[Set[WorkbenchGroupName]] = Gen.listOfN[ResourceId](3, genResourceId).map(x => x.map(v => WorkbenchGroupName(v.value)).toSet) //make it a list of 3 items so that unit test won't time out
   val genNonEmptyAuthDomains: Gen[Set[WorkbenchGroupName]] = Gen.nonEmptyListOf[ResourceId](genResourceId).map(x => x.map(v => WorkbenchGroupName(v.value)).toSet)
-  val genRoleName: Gen[ResourceRoleName] = Gen.oneOf("owner", "admin").map(ResourceRoleName.apply) //there might be possible values
+  val genRoleName: Gen[ResourceRoleName] = Gen.oneOf("owner", "other").map(ResourceRoleName.apply) //there might be possible values
   val genResourceAction: Gen[ResourceAction] = Gen.oneOf(readPolicies, alterPolicies, delete, notifyAdmins, setAccessInstructions)
 
   val genResource: Gen[Resource] = for{
