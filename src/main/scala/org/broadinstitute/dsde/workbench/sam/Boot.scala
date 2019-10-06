7b92eb49c7a44f93e487d8f5d465cd37f4d63875
Kristian Cibulskis <kcibul@broadinstitute.org>
2019-10-06 15:44:16 -0400

    exchange GIC for custom w/ 64 threads

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/Boot.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/Boot.scala
@@ -2,6 +2,7 @@ package org.broadinstitute.dsde.workbench.sam
 
 import java.io.File
 import java.net.URI
+import java.util.concurrent.Executors
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
@@ -32,11 +33,13 @@ import org.broadinstitute.dsde.workbench.util.{DelegatePool, ExecutionContexts}
 import org.ehcache.Cache
 import org.ehcache.config.builders.{CacheConfigurationBuilder, CacheManagerBuilder, ExpiryPolicyBuilder, ResourcePoolsBuilder}
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.util.control.NonFatal
 
 object Boot extends IOApp with LazyLogging {
+  implicit val ec : ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(64))
+
 
   def run(args: List[String]): IO[ExitCode] =
     (startup() *> ExitCode.Success.pure[IO]).recoverWith {
@@ -50,7 +53,6 @@ object Boot extends IOApp with LazyLogging {
     // we need an ActorSystem to host our application in
     implicit val system = ActorSystem("sam")
     implicit val materializer = ActorMaterializer()
-
     val config = ConfigFactory.load()
     val appConfig = AppConfig.readConfig(config)
 
