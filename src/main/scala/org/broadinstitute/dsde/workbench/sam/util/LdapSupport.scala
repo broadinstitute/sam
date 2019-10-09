6701634dba33a68656cf52c9353e9e916a7e4322
dvoet <dvoet@users.noreply.github.com>
2019-10-09 08:28:31 -0400

    CA-548 release connection at end of ldapSearchStream, don't close (#362)

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/util/LdapSupport.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/util/LdapSupport.scala
@@ -36,27 +36,35 @@ trait LdapSupport extends DirectorySubjectNameSupport {
   protected def ldapSearchStream[T](baseDn: String, searchScope: SearchScope, filters: Filter*)(unmarshaller: Entry => T): Stream[T] =
     filters.flatMap { filter =>
       val search = new SearchRequest(baseDn, searchScope, filter)
-      val entrySource = new LDAPEntrySource(ldapConnectionPool.getConnection, search, true)
-      ldapEntrySourceStream(entrySource)(unmarshaller)
+      val connection = ldapConnectionPool.getConnection
+      val closeConnection = false
+
+      val entrySource = new LDAPEntrySource(connection, search, closeConnection)
+      ldapEntrySourceStream(entrySource, connection)(unmarshaller)
     }.toStream
 
   // this is the magic recursive stream generator
-  private def ldapEntrySourceStream[T](entrySource: LDAPEntrySource)(unmarshaller: Entry => T): Stream[T] =
+  // releases connection back to the pool once the end of entrySource is reached
+  private def ldapEntrySourceStream[T](entrySource: LDAPEntrySource, connection: LDAPConnection)(unmarshaller: Entry => T): Stream[T] =
     Try(Option(entrySource.nextEntry)) match {
       case Success(None) =>
         // reached the last element, Stream.empty terminates the stream
+        ldapConnectionPool.releaseConnection(connection)
         Stream.empty
 
       case Success(Some(next)) =>
         // next element exists, return a Stream starting with unmarshalled next followed by the rest of the stream
         // (streams are smart and lazily evaluate the second parameter)
-        Stream.cons(unmarshaller(next), ldapEntrySourceStream(entrySource)(unmarshaller))
+        Stream.cons(unmarshaller(next), ldapEntrySourceStream(entrySource, connection)(unmarshaller))
 
       case Failure(ldape: EntrySourceException)
           if ldape.getCause.isInstanceOf[LDAPException] &&
             ldape.getCause.asInstanceOf[LDAPException].getResultCode == ResultCode.NO_SUCH_OBJECT =>
+        ldapConnectionPool.releaseConnection(connection)
         Stream.empty // the base dn does not exist, treat as empty search
-      case Failure(regrets) => throw regrets
+      case Failure(regrets) =>
+        ldapConnectionPool.releaseConnection(connection)
+        throw regrets
     }
 
   protected def getAttribute(result: Entry, key: String): Option[String] = {
