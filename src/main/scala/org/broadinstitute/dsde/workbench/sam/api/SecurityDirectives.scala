f6207c4a54e1f6944de90ff24d83cc2a7494a640
Douglas Voet <dvoet@users.noreply.github.com>
2019-10-22 09:13:01 -0400

    rework has permission check

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SecurityDirectives.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SecurityDirectives.scala
@@ -2,13 +2,14 @@ package org.broadinstitute.dsde.workbench.sam.api
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.onSuccess
-import akka.http.scaladsl.server.{Directive0, Directives}
+import akka.http.scaladsl.server.{Directive0, Directives, Route}
 import org.broadinstitute.dsde.workbench.model.{ErrorReport, WorkbenchExceptionWithErrorReport, WorkbenchUserId}
 import org.broadinstitute.dsde.workbench.sam._
 import org.broadinstitute.dsde.workbench.sam.model.{FullyQualifiedResourceId, ResourceAction}
 import org.broadinstitute.dsde.workbench.sam.service.PolicyEvaluatorService
 import ImplicitConversions.ioOnSuccessMagnet
 import cats.effect.IO
+import cats.implicits._
 
 trait SecurityDirectives {
   def policyEvaluatorService: PolicyEvaluatorService
@@ -18,20 +19,30 @@ trait SecurityDirectives {
 
   def requireOneOfAction(resource: FullyQualifiedResourceId, requestedActions: Set[ResourceAction], userId: WorkbenchUserId): Directive0 =
     Directives.mapInnerRoute { innerRoute =>
-      onSuccess(listActions(resource, userId, requestedActions)) { actions =>
-        if (hasAccess(requestedActions, actions)) innerRoute
-        else if (actions.isEmpty)
-          Directives.failWith(
-            new WorkbenchExceptionWithErrorReport(
-              ErrorReport(StatusCodes.NotFound, s"Resource ${resource.resourceTypeName.value}/${resource.resourceId.value} not found")))
-        else
-          Directives.failWith(
-            new WorkbenchExceptionWithErrorReport(ErrorReport(
-              StatusCodes.Forbidden,
-              s"You may not perform any of ${requestedActions.mkString("[", ", ", "]").toString.toUpperCase} on ${resource.resourceTypeName.value}/${resource.resourceId.value}"
-            )))
-      }
+      onSuccess(evalAccess(resource, requestedActions, userId, innerRoute))(identity)
     }
+
+  private def evalAccess(resource: FullyQualifiedResourceId, requestedActions: Set[ResourceAction], userId: WorkbenchUserId, innerRoute: Route): IO[Route] = {
+    requestedActions.toList.traverse(action => policyEvaluatorService.hasPermission(resource, action, userId)).flatMap { hasPermissionResults =>
+      if (hasPermissionResults.contains(true)) {
+        IO.pure(innerRoute)
+      } else {
+        listActions(resource, userId, requestedActions).map { actions =>
+          if (actions.isEmpty)
+            Directives.failWith(
+              new WorkbenchExceptionWithErrorReport(
+                ErrorReport(StatusCodes.NotFound, s"Resource ${resource.resourceTypeName.value}/${resource.resourceId.value} not found")))
+          else
+            Directives.failWith(
+              new WorkbenchExceptionWithErrorReport(ErrorReport(
+                StatusCodes.Forbidden,
+                s"You may not perform any of ${requestedActions.mkString("[", ", ", "]").toString.toUpperCase} on ${resource.resourceTypeName.value}/${resource.resourceId.value}"
+              )))
+        }
+      }
+
+    }
+  }
 
   private def listActions(resource: FullyQualifiedResourceId, userId: WorkbenchUserId, requestedActions: Set[ResourceAction]): IO[Set[ResourceAction]] =
     // this is optimized for the case where the user has permission since that is the usual case
