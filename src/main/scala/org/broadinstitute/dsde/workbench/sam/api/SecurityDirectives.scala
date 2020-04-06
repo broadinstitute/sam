cea5115f5dbd40d3a9d13a9aadc1dfe09138d493
Marcus Talbott <mtalbott@broadinstitute.org>
2020-04-06 15:28:40 -0400

    remove shallow permissions checks

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SecurityDirectives.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SecurityDirectives.scala
@@ -47,8 +47,7 @@ trait SecurityDirectives {
   private def hasPermissionOneOf(resource: FullyQualifiedResourceId, actions: Iterable[ResourceAction], userId: WorkbenchUserId): IO[Boolean] =
     //  first quickly check if we have permission using the shallow check across all actions, then try the full check
     for {
-      attempt1 <- actions.toList.existsM(policyEvaluatorService.hasPermissionShallowCheck(resource, _, userId))
-      attempt2 <- if (attempt1) IO.pure(attempt1) else actions.toList.existsM(policyEvaluatorService.hasPermissionFullCheck(resource, _, userId))
+      attempt2 <- actions.toList.existsM(policyEvaluatorService.hasPermissionFullCheck(resource, _, userId))
     } yield {
       attempt2
     }
