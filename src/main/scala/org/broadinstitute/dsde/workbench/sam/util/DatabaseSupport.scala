9c9406524e83e3a710c4738b7aecdbcbaf84326c
Andrew Jang <andy7i@users.noreply.github.com>
2020-04-14 15:50:27 -0400

    add tracing to all postgres calls

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/util/DatabaseSupport.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/util/DatabaseSupport.scala
@@ -2,6 +2,7 @@ package org.broadinstitute.dsde.workbench.sam.util
 
 import cats.effect.{ContextShift, IO}
 import org.broadinstitute.dsde.workbench.sam.db.DbReference
+import org.broadinstitute.dsde.workbench.sam.util.OpenCensusIOUtils.traceIOWithParent
 import scalikejdbc.DBSession
 
 import scala.concurrent.ExecutionContext
@@ -12,8 +13,17 @@ trait DatabaseSupport {
   protected val dbRef: DbReference
 
   protected def runInTransaction[A](databaseFunction: DBSession => A): IO[A] = {
-    cs.evalOn(ecForDatabaseIO)(IO {
-      dbRef.inLocalTransaction(databaseFunction)
-    })
+    val spanName = "postgres-" + parentMethodName()
+    traceIOWithParent (spanName, null) ( _ =>
+      cs.evalOn(ecForDatabaseIO)(IO {
+        dbRef.inLocalTransaction(databaseFunction)
+      })
+    )
   }
+
+  /*
+  This gets the name of the method that calls runInTransaction. It goes 5 levels up because the stack looks like this:
+  loadSubjectFromGoogleSubjectId->runInTransaction->runInTransaction$->runInTransaction->parentMethodName
+   */
+  private def parentMethodName() : String = Thread.currentThread.getStackTrace()(5).getMethodName
 }
