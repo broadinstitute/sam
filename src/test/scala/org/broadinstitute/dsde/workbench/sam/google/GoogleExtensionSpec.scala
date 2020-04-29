f1714f713db848d1297bd20cf473a5259fb5a5a2
Andrew Jang <andy7i@users.noreply.github.com>
2020-04-29 10:33:25 -0400

    fix last NPE -- due to this line that would fail compilation with the message "dead code following this construct". Apparently the intellij SBT catches this warning, reports it as an error, but the SBT used in cmdline and jenkins does not.

--- src/test/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleExtensionSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleExtensionSpec.scala
@@ -584,7 +584,8 @@ class GoogleExtensionSpec(_system: ActorSystem) extends TestKit(_system) with Fl
     val allUsersGroupMatcher = new ArgumentMatcher[BasicWorkbenchGroup] {
       override def matches(group: BasicWorkbenchGroup): Boolean = group.id == allUsersGroup.id
     }
-//    when(mockDirectoryDAO.createGroup(argThat(allUsersGroupMatcher), isNull(), samRequestContext = samRequestContext)).thenReturn(IO.pure(allUsersGroup))
+    // if this line has build errors, try building using commandline sbt.
+    when(mockDirectoryDAO.createGroup(argThat(allUsersGroupMatcher), isNull(), any[SamRequestContext])).thenReturn(IO.pure(allUsersGroup))
 
     when(mockGoogleDirectoryDAO.getGoogleGroup(any[WorkbenchEmail])).thenReturn(Future.successful(None))
     when(mockGoogleDirectoryDAO.createGroup(any[String], any[WorkbenchEmail], any[Option[Groups]])).thenReturn(Future.successful(()))
