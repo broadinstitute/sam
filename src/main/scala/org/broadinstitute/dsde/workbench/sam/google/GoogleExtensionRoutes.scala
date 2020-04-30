58f56ef4a2e75e5142246c6ec51322934876bfbf
Andrew Jang <andy7i@users.noreply.github.com>
2020-04-30 19:11:40 -0400

    Revert "try removing tracing to see if that's causing the issue"
    This reverts commit 2144d698

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleExtensionRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleExtensionRoutes.scala
@@ -14,7 +14,6 @@ import org.broadinstitute.dsde.workbench.sam.model.SamJsonSupport._
 import org.broadinstitute.dsde.workbench.sam.model._
 import org.broadinstitute.dsde.workbench.sam.service.CloudExtensions
 import org.broadinstitute.dsde.workbench.sam.util.OpenCensusIOUtils.completeWithTrace
-import org.broadinstitute.dsde.workbench.sam.util.SamRequestContext
 import spray.json.DefaultJsonProtocol._
 import spray.json.JsString
 
@@ -110,14 +109,12 @@ trait GoogleExtensionRoutes extends ExtensionRoutes with UserInfoDirectives with
                   } ~
                   pathEnd {
                     get {
-//                      completeWithTrace({samRequestContext =>
-                      complete (
-                        googleExtensions.createUserPetServiceAccount(WorkbenchUser(userInfo.userId, None, userInfo.userEmail, None), GoogleProject(project), SamRequestContext(null)).map {
+                      completeWithTrace({samRequestContext =>
+                        googleExtensions.createUserPetServiceAccount(WorkbenchUser(userInfo.userId, None, userInfo.userEmail, None), GoogleProject(project), samRequestContext).map {
                           petSA =>
                             StatusCodes.OK -> petSA.serviceAccount.email
                         }
-//                      })
-                      )
+                      })
                     } ~
                       delete { // NOTE: This endpoint is not visible in Swagger
                         completeWithTrace({samRequestContext =>
