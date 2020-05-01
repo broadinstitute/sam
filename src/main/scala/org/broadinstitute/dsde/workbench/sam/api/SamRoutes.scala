6d5fc73076c10627efaa0e78d3d28a0627fb4a1b
Andrew Jang <andy7i@users.noreply.github.com>
2020-05-01 18:34:51 -0400

    Revert "Out of curiosity, I tried wrapping it in a toStrict but it still errored with "Caused by: java.lang.IllegalStateException: Substream Source cannot be materialized more than once". ```     ExceptionHandler {       case withErrorReport: WorkbenchExceptionWithErrorReport =>         toStrictEntity(5.minutes) {           completeWithTrace(samRequestContext => (withErrorReport.errorReport.statusCode.getOrElse(StatusCodes.InternalServerError), withErrorReport.errorReport))         }       case e: Throwable =>         toStrictEntity(5.minutes) {           completeWithTrace(samRequestContext => (StatusCodes.InternalServerError, ErrorReport(e)))         }     }   } ```"
    This reverts commit 6615c209

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SamRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SamRoutes.scala
@@ -19,10 +19,8 @@ import org.broadinstitute.dsde.workbench.sam.api.SamRoutes._
 import org.broadinstitute.dsde.workbench.sam.config.{LiquibaseConfig, SwaggerConfig}
 import org.broadinstitute.dsde.workbench.sam.directory.DirectoryDAO
 import org.broadinstitute.dsde.workbench.sam.service._
-import org.broadinstitute.dsde.workbench.sam.util.OpenCensusIOUtils.completeWithTrace
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.duration._
 
 /**
   * Created by dvoet on 5/17/17.
@@ -91,13 +89,9 @@ object SamRoutes {
 
     ExceptionHandler {
       case withErrorReport: WorkbenchExceptionWithErrorReport =>
-        toStrictEntity(5.minutes) {
-          completeWithTrace(samRequestContext => (withErrorReport.errorReport.statusCode.getOrElse(StatusCodes.InternalServerError), withErrorReport.errorReport))
-        }
+        complete((withErrorReport.errorReport.statusCode.getOrElse(StatusCodes.InternalServerError), withErrorReport.errorReport))
       case e: Throwable =>
-        toStrictEntity(5.minutes) {
-          completeWithTrace(samRequestContext => (StatusCodes.InternalServerError, ErrorReport(e)))
-        }
+        complete((StatusCodes.InternalServerError, ErrorReport(e)))
     }
   }
 }
