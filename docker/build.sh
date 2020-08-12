3ed683b728294831501827a2d56de9a781aa6c7a
Zybjana Bedo <zbedo@broadinstitute.org>
2020-08-12 13:09:40 -0400

    [No ticket] Scan test docker image with trivy

--- docker/build.sh
+++ docker/build.sh
@@ -131,6 +131,10 @@ function docker_cmd()
         echo "building ${DOCKERHUB_TESTS_REGISTRY}:${HASH_TAG}..."
         cd automation
         docker build -f Dockerfile-tests -t $DOCKERHUB_TESTS_REGISTRY:${HASH_TAG} .
+
+        echo "scanning test docker image..."
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME"/Library/Caches:/root/.cache/ aquasec/trivy --exit-code 1 --severity CRITICAL "$DOCKERHUB_TESTS_REGISTRY":"${HASH_TAG}"
+
         cd ..
 
         if [ $DOCKER_CMD="push" ]; then
