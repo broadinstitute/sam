73012c95bbb6e8fd8329203391cbe19ff9ca0540
Joel Thibault <thibault@broad.dev>
2020-11-25 10:18:30 -0500

    loadGroup/LoadGroups

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/directory/FlatPostgresDirectoryDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/directory/FlatPostgresDirectoryDAO.scala
@@ -3,10 +3,11 @@ package org.broadinstitute.dsde.workbench.sam.directory
 import akka.http.scaladsl.model.StatusCodes
 import cats.effect.{ContextShift, IO}
 import org.broadinstitute.dsde.workbench.sam.errorReportSource
-import org.broadinstitute.dsde.workbench.model.{ErrorReport, WorkbenchException, WorkbenchExceptionWithErrorReport, WorkbenchGroupIdentity, WorkbenchGroupName, WorkbenchSubject, WorkbenchUserId}
+import org.broadinstitute.dsde.workbench.model.{ErrorReport, WorkbenchEmail, WorkbenchException, WorkbenchExceptionWithErrorReport, WorkbenchGroupIdentity, WorkbenchGroupName, WorkbenchSubject, WorkbenchUserId}
 import org.broadinstitute.dsde.workbench.sam.db.SamParameterBinderFactory.SqlInterpolationWithSamBinders
 import org.broadinstitute.dsde.workbench.sam.db._
-import org.broadinstitute.dsde.workbench.sam.db.tables.{FlatGroupMemberRecord, FlatGroupMemberTable, FlatGroupMembershipPath, GroupPK, GroupTable, PolicyTable, ResourceTable, ResourceTypeTable}
+import org.broadinstitute.dsde.workbench.sam.db.tables.{FlatGroupMemberRecord, FlatGroupMemberTable, FlatGroupMembershipPath, GroupPK, GroupRecord, GroupTable, PolicyTable, ResourceTable, ResourceTypeTable}
+import org.broadinstitute.dsde.workbench.sam.model.{AccessPolicyName, BasicWorkbenchGroup, FullyQualifiedPolicyId, FullyQualifiedResourceId, ResourceId, ResourceTypeName}
 import org.broadinstitute.dsde.workbench.sam.util.SamRequestContext
 import org.postgresql.util.PSQLException
 import scalikejdbc._
@@ -97,7 +98,7 @@ class FlatPostgresDirectoryDAO (override val dbRef: DbReference, override val ec
     runInTransaction("removeGroupMember", samRequestContext)({ implicit session =>
       val query =
         samsql"""DELETE FROM ${FlatGroupMemberTable as f}
-                     WHERE ${memberClause(removeMember)} AND ${directMembershipGroupClause(groupId)}"""
+                     WHERE ${memberClause(removeMember)} AND ${directMembershipClause(groupId)}"""
       query.update().apply()
     }).map(count => count > 0)
   }
@@ -162,6 +163,98 @@ class FlatPostgresDirectoryDAO (override val dbRef: DbReference, override val ec
     listMemberOfGroups(groupId, samRequestContext)
   }
 
+  // NOTE assumption from reading the original queries: loadGroup/loadGroups want *direct* membership only
+  override def loadGroup(groupName: WorkbenchGroupName, samRequestContext: SamRequestContext): IO[Option[BasicWorkbenchGroup]] = {
+    for {
+      results <- runInTransaction("loadGroup", samRequestContext)({ implicit session =>
+        val g = GroupTable.syntax("g")
+        val sg = GroupTable.syntax("sg")
+        val f = FlatGroupMemberTable.syntax("f")
+        val p = PolicyTable.syntax("p")
+        val r = ResourceTable.syntax("r")
+        val rt = ResourceTypeTable.syntax("rt")
+
+        import SamTypeBinders._
+
+        samsql"""select ${g.result.email}, ${f.result.memberUserId}, ${sg.result.name}, ${p.result.name}, ${r.result.name}, ${rt.result.name}
+                  from ${GroupTable as g}
+                  left join ${FlatGroupMemberTable as f} on ${g.id} = ${f.groupId} and ${directMembershipClause(g)}
+                  left join ${GroupTable as sg} on ${f.memberGroupId} = ${sg.id}
+                  left join ${PolicyTable as p} on ${p.groupId} = ${sg.id}
+                  left join ${ResourceTable as r} on ${p.resourceId} = ${r.id}
+                  left join ${ResourceTypeTable as rt} on ${r.resourceTypeId} = ${rt.id}
+                  where ${g.name} = ${groupName}"""
+          .map { rs =>
+            (rs.get[WorkbenchEmail](g.resultName.email),
+              rs.stringOpt(f.resultName.memberUserId).map(WorkbenchUserId),
+              rs.stringOpt(sg.resultName.name).map(WorkbenchGroupName),
+              rs.stringOpt(p.resultName.name).map(AccessPolicyName(_)),
+              rs.stringOpt(r.resultName.name).map(ResourceId(_)),
+              rs.stringOpt(rt.resultName.name).map(ResourceTypeName(_)))
+          }.list().apply()
+      })
+    } yield {
+      if (results.isEmpty) {
+        None
+      } else {
+        val email = results.head._1
+        val members: Set[WorkbenchSubject] = results.collect {
+          case (_, Some(userId), None, None, None, None) => userId
+          case (_, None, Some(subGroupName), None, None, None) => subGroupName
+          case (_, None, Some(_), Some(policyName), Some(resourceName), Some(resourceTypeName)) => FullyQualifiedPolicyId(FullyQualifiedResourceId(resourceTypeName, resourceName), policyName)
+        }.toSet
+
+        Option(BasicWorkbenchGroup(groupName, members, email))
+      }
+    }
+  }
+
+  override def loadGroups(groupNames: Set[WorkbenchGroupName], samRequestContext: SamRequestContext): IO[Stream[BasicWorkbenchGroup]] = {
+    if (groupNames.isEmpty) {
+      IO.pure(Stream.empty)
+    } else {
+      for {
+        results <- runInTransaction("loadGroups", samRequestContext)({ implicit session =>
+          val g = GroupTable.syntax("g")
+          val sg = GroupTable.syntax("sg")
+          val f = FlatGroupMemberTable.syntax("f")
+          val p = PolicyTable.syntax("p")
+          val r = ResourceTable.syntax("r")
+          val rt = ResourceTypeTable.syntax("rt")
+
+          import SamTypeBinders._
+          samsql"""select ${g.result.name}, ${g.result.email}, ${f.result.memberUserId}, ${sg.result.name}, ${p.result.name}, ${r.result.name}, ${rt.result.name}
+                    from ${GroupTable as g}
+                    left join ${FlatGroupMemberTable as f} on ${g.id} = ${f.groupId} and ${directMembershipClause(g)}
+                    left join ${GroupTable as sg} on ${f.memberGroupId} = ${sg.id}
+                    left join ${PolicyTable as p} on ${p.groupId} = ${sg.id}
+                    left join ${ResourceTable as r} on ${p.resourceId} = ${r.id}
+                    left join ${ResourceTypeTable as rt} on ${r.resourceTypeId} = ${rt.id}
+                    where ${g.name} in (${groupNames})"""
+            .map { rs =>
+              (rs.get[WorkbenchGroupName](g.resultName.name),
+                rs.get[WorkbenchEmail](g.resultName.email),
+                rs.stringOpt(f.resultName.memberUserId).map(WorkbenchUserId),
+                rs.stringOpt(sg.resultName.name).map(WorkbenchGroupName),
+                rs.stringOpt(p.resultName.name).map(AccessPolicyName(_)),
+                rs.stringOpt(r.resultName.name).map(ResourceId(_)),
+                rs.stringOpt(rt.resultName.name).map(ResourceTypeName(_)))
+            }.list().apply()
+        })
+      } yield {
+        results.groupBy(result => (result._1, result._2)).map { case ((groupName, email), results) =>
+          val members: Set[WorkbenchSubject] = results.collect {
+            case (_, _, Some(userId), None, None, None, None) => userId
+            case (_, _, None, Some(subGroupName), None, None, None) => subGroupName
+            case (_, _, None, Some(_), Some(policyName), Some(resourceName), Some(resourceTypeName)) => FullyQualifiedPolicyId(FullyQualifiedResourceId(resourceTypeName, resourceName), policyName)
+          }.toSet
+
+          BasicWorkbenchGroup(groupName, members, email)
+        }
+      }.toStream
+    }
+  }
+
   private def memberClause(member: WorkbenchSubject): SQLSyntax = {
     val f = FlatGroupMemberTable.syntax("f")
     member match {
@@ -173,7 +266,14 @@ class FlatPostgresDirectoryDAO (override val dbRef: DbReference, override val ec
 
   // selection clause for direct membership:
   // choose when the final `groupMembershipPath` array element is equal to groupId
-  private def directMembershipGroupClause(groupId: WorkbenchGroupIdentity): SQLSyntax = {
+  def directMembershipClause(g: QuerySQLSyntaxProvider[SQLSyntaxSupport[GroupRecord], GroupRecord]): SQLSyntax = {
+    val f = FlatGroupMemberTable.syntax("f")
+    samsqls"${f.groupMembershipPath}[array_upper(${f.groupMembershipPath}, 1)] = ${g.id}"
+  }
+
+  // selection clause for direct membership:
+  // choose when the final `groupMembershipPath` array element is equal to groupId
+  private def directMembershipClause(groupId: WorkbenchGroupIdentity): SQLSyntax = {
     val f = FlatGroupMemberTable.syntax("f")
     samsqls"${f.groupMembershipPath}[array_upper(${f.groupMembershipPath}, 1)] = ${workbenchGroupIdentityToGroupPK(groupId)}"
   }
