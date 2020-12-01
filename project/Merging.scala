cd72052f8f629dbd5d57359547bf95d093fc3e1c
Qi Wang <qi@broadinstitute.org>
2020-12-01 14:19:54 -0500

    fix assembly rule

--- project/Merging.scala
+++ project/Merging.scala
@@ -3,6 +3,10 @@ import sbtassembly.{MergeStrategy, PathList}
 object Merging {
   def customMergeStrategy(oldStrategy: (String) => MergeStrategy):(String => MergeStrategy) = {
     case PathList("org", "joda", "time", "base", "BaseDateTime.class") => MergeStrategy.first
+    case PathList("io", "sundr", _ @ _*) => MergeStrategy.first
+    case PathList("META-INF", "versions", "9", "module-info.class") => MergeStrategy.first
+    case "module-info.class" =>
+      MergeStrategy.discard // JDK 8 does not use the file module-info.class so it is safe to discard the file.
     case x => oldStrategy(x)
   }
 }
