3460ffde2e3a58610773190ecd805bcb91e0df42
Douglas Voet <dvoet@users.noreply.github.com>
2021-03-04 10:18:00 -0500

    workbenchLibs/google bump

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val workbenchUtilV   = "0.6-74c9fc2"
   val workbenchUtil2V   = "0.1-74c9fc2"
   val workbenchModelV  = "0.14-74c9fc2"
-  val workbenchGoogleV = "0.21-74c9fc2"
+  val workbenchGoogleV = "0.21-51d7fff"
   val workbenchGoogle2V = "0.18-74c9fc2"
   val workbenchNotificationsV = "0.3-d74ff96"
   val monocleVersion = "2.0.3"
