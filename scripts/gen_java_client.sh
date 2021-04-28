4552a0935e3e77b58f6e447cebdd8bc729ebd734
Sky Rubenstein <blrubenstein@wpi.edu>
2021-04-28 13:21:44 -0400

    Remove echos

--- scripts/gen_java_client.sh
+++ scripts/gen_java_client.sh
@@ -1,8 +1,5 @@
 set -e
 
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.0.3 generate -i /local/src/main/resources/swagger/api-docs.yaml -g java -o /local/codegen_java --api-package org.broadinstitute.dsde.workbench.client.sam.api --model-package org.broadinstitute.dsde.workbench.client.sam.model
-echo ranDockerCommand
 cd codegen_java
-echo attemptingTesting
 sbt test
-echo finishedTesting
