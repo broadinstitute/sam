a72477e2e191c5e8b9429d964d99e6de108d619c
Greg Rushton <grushton@broadinstitute.org>
2021-07-14 14:37:46 -0400

    fix compilation

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/AdminResourceRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/AdminResourceRoutes.scala
@@ -2,16 +2,16 @@ package org.broadinstitute.dsde.workbench.sam
 package api
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
-import org.broadinstitute.dsde.workbench.model.UserInfo
+import org.broadinstitute.dsde.workbench.model._
 import org.broadinstitute.dsde.workbench.sam.config.LiquibaseConfig
 import org.broadinstitute.dsde.workbench.sam.model.SamJsonSupport._
 import org.broadinstitute.dsde.workbench.sam.model._
 import org.broadinstitute.dsde.workbench.sam.service.ResourceService
 import org.broadinstitute.dsde.workbench.sam.util.SamRequestContext
+import spray.json.DefaultJsonProtocol._
 
 import scala.concurrent.ExecutionContext
 
