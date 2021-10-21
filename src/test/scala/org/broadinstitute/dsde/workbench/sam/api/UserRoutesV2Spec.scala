10d971e134816e9d821193a10f3384d9038d82c8
Matt Bemis <mbemis@broadinstitute.org>
2021-10-21 10:16:46 -0400

    add extra assertion per pr feedback

--- src/test/scala/org/broadinstitute/dsde/workbench/sam/api/UserRoutesV2Spec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/api/UserRoutesV2Spec.scala
@@ -6,6 +6,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
 import org.broadinstitute.dsde.workbench.model._
+import org.broadinstitute.dsde.workbench.model.ErrorReportJsonSupport._
 import org.broadinstitute.dsde.workbench.sam.dataAccess.{MockDirectoryDAO, MockRegistrationDAO}
 import org.broadinstitute.dsde.workbench.sam.model.SamJsonSupport._
 import org.broadinstitute.dsde.workbench.sam.model._
@@ -56,6 +57,7 @@ class UserRoutesV2Spec extends UserRoutesSpecHelper {
   it should "forbid the registration if ToS is enabled and the user doesn't specify a ToS parameter" in withTosEnabledRoutes { samRoutes =>
     Post("/register/user/v2/self") ~> samRoutes.route ~> check {
       status shouldEqual StatusCodes.Forbidden
+      responseAs[ErrorReport].message should startWith("You must accept the Terms of Service in order to register.")
     }
   }
 
@@ -63,6 +65,7 @@ class UserRoutesV2Spec extends UserRoutesSpecHelper {
     val tos = TermsOfServiceAcceptance("onemillionpats.com")
     Post("/register/user/v2/self", tos) ~> samRoutes.route ~> check {
       status shouldEqual StatusCodes.Forbidden
+      responseAs[ErrorReport].message should startWith("You must accept the Terms of Service in order to register.")
     }
   }
 
