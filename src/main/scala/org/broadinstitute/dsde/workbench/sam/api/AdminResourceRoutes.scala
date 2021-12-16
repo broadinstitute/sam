7fc02373e17eefc90c7811ff7e48a85e8e2c6517
Marcus Talbott <mtalbott@broadinstitute.org>
2021-12-16 17:16:47 -0500

    add a little extra logging

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/AdminResourceRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/AdminResourceRoutes.scala
@@ -1,10 +1,13 @@
 package org.broadinstitute.dsde.workbench.sam
 package api
 
+import java.time.LocalDateTime
+
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
+import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.model.{UserInfo, WorkbenchEmail, WorkbenchSubject}
 import org.broadinstitute.dsde.workbench.sam.config.LiquibaseConfig
 import org.broadinstitute.dsde.workbench.sam.model.SamJsonSupport._
@@ -15,7 +18,7 @@ import org.broadinstitute.dsde.workbench.sam.util.SamRequestContext
 
 import scala.concurrent.ExecutionContext
 
-trait AdminResourceRoutes extends UserInfoDirectives with SecurityDirectives with SamRequestContextDirectives with ResourceRoutes {
+trait AdminResourceRoutes extends UserInfoDirectives with SecurityDirectives with SamRequestContextDirectives with ResourceRoutes with LazyLogging{
   implicit val executionContext: ExecutionContext
   val resourceService: ResourceService
   val liquibaseConfig: LiquibaseConfig
@@ -23,72 +26,76 @@ trait AdminResourceRoutes extends UserInfoDirectives with SecurityDirectives wit
   def adminResourceRoutes: server.Route =
     pathPrefix("resourceTypeAdmin" / "v1") {
       withSamRequestContext { samRequestContext =>
-        requireUserInfo(samRequestContext) { userInfo =>
-          withResourceType(SamResourceTypes.resourceTypeAdminName) { resourceTypeAdmin =>
-            pathPrefix("resourceTypes") {
-              asSamSuperAdmin(userInfo) {
-                pathPrefix(Segment) { resourceTypeNameToAdminister =>
-                  withResourceType(ResourceTypeName(resourceTypeNameToAdminister)) { resourceTypeToAdminister =>
-                    val resource = FullyQualifiedResourceId(resourceTypeAdmin.name, ResourceId(resourceTypeToAdminister.name.value))
-                    pathPrefix("policies") {
-                      pathEndOrSingleSlash {
-                        get {
-                          complete(resourceService.listResourcePolicies(resource, samRequestContext).map { response =>
-                            StatusCodes.OK -> response.toSet
-                          })
-                        }
-                      } ~
-                        pathPrefix(Segment) { policyName =>
-                          val policyId = FullyQualifiedPolicyId(resource, AccessPolicyName(policyName))
-                          pathEndOrSingleSlash {
-                            put {
-                              entity(as[AccessPolicyMembership]) { membershipUpdate =>
-                                complete(resourceService.overwriteAdminPolicy(resourceTypeAdmin, policyId.accessPolicyName, policyId.resource, membershipUpdate, samRequestContext).map(_ => StatusCodes.Created))
-                              }
-                            } ~
-                              delete {
-                                complete(resourceService.deletePolicy(policyId, samRequestContext).map(_ => StatusCodes.NoContent))
-                              }
+        extractClientIP { ip =>
+          requireUserInfo(samRequestContext) { userInfo =>
+            withResourceType(SamResourceTypes.resourceTypeAdminName) { resourceTypeAdmin =>
+              pathPrefix("resourceTypes") {
+                asSamSuperAdmin(userInfo) {
+                  pathPrefix(Segment) { resourceTypeNameToAdminister =>
+                    withResourceType(ResourceTypeName(resourceTypeNameToAdminister)) { resourceTypeToAdminister =>
+                      val resource = FullyQualifiedResourceId(resourceTypeAdmin.name, ResourceId(resourceTypeToAdminister.name.value))
+                      pathPrefix("policies") {
+                        pathEndOrSingleSlash {
+                          get {
+                            complete(resourceService.listResourcePolicies(resource, samRequestContext).map { response =>
+                              StatusCodes.OK -> response.toSet
+                            })
                           }
-                        }
-                    }
-                  }
-                }
-              }
-            } ~
-            pathPrefix("resources") {
-              pathPrefix(Segment) { resourceTypeNameToAdminister =>
-                withResourceType(ResourceTypeName(resourceTypeNameToAdminister)) { resourceTypeToAdminister =>
-                  pathPrefix(Segment) { resourceId =>
-                    val resource = FullyQualifiedResourceId(resourceTypeToAdminister.name, ResourceId(resourceId))
-
-                    pathPrefix("policies") {
-                      pathEndOrSingleSlash {
-                        val resource = FullyQualifiedResourceId(ResourceTypeName(resourceTypeNameToAdminister), ResourceId(resourceId))
-                        getAdminResourcePolicies(resource, userInfo, samRequestContext)
-                      } ~
-                        pathPrefix(Segment) { policyName =>
-                          val policyId = FullyQualifiedPolicyId(resource, AccessPolicyName(policyName))
-
-                          pathPrefix("memberEmails" / Segment) { userEmail =>
-                            withSubject(WorkbenchEmail(userEmail), samRequestContext) { subject =>
+                        } ~
+                          pathPrefix(Segment) { policyName =>
+                            val policyId = FullyQualifiedPolicyId(resource, AccessPolicyName(policyName))
+                            pathEndOrSingleSlash {
                               put {
-                                requireActionsForAdminPutUserInPolicy(policyId, userInfo, samRequestContext) {
-                                  adminPutUserInPolicy(policyId, subject, samRequestContext)
+                                entity(as[AccessPolicyMembership]) { membershipUpdate =>
+                                  logger.info(s"IP: ${ip.toOption.map(_.getHostAddress)} OR ${ip.toIP.map(_.ip.getHostAddress).getOrElse("toIP doesn't know")} OR $ip")
+                                  logger.info(s"${LocalDateTime.now()} - ip - ${userInfo.userEmail.value} - lots of details about the difference b/w this policy and the existing policy")
+                                  complete(resourceService.overwriteAdminPolicy(resourceTypeAdmin, policyId.accessPolicyName, policyId.resource, membershipUpdate, samRequestContext).map(_ => StatusCodes.Created))
                                 }
                               } ~
                                 delete {
-                                  requireActionsForAdminRemoveUserFromPolicy(policyId, userInfo, samRequestContext) {
-                                    adminRemoveUserFromPolicy(policyId, subject, samRequestContext)
-                                  }
+                                  complete(resourceService.deletePolicy(policyId, samRequestContext).map(_ => StatusCodes.NoContent))
                                 }
                             }
                           }
-                        }
+                      }
                     }
                   }
                 }
-              }
+              } ~
+                pathPrefix("resources") {
+                  pathPrefix(Segment) { resourceTypeNameToAdminister =>
+                    withResourceType(ResourceTypeName(resourceTypeNameToAdminister)) { resourceTypeToAdminister =>
+                      pathPrefix(Segment) { resourceId =>
+                        val resource = FullyQualifiedResourceId(resourceTypeToAdminister.name, ResourceId(resourceId))
+
+                        pathPrefix("policies") {
+                          pathEndOrSingleSlash {
+                            val resource = FullyQualifiedResourceId(ResourceTypeName(resourceTypeNameToAdminister), ResourceId(resourceId))
+                            getAdminResourcePolicies(resource, userInfo, samRequestContext)
+                          } ~
+                            pathPrefix(Segment) { policyName =>
+                              val policyId = FullyQualifiedPolicyId(resource, AccessPolicyName(policyName))
+
+                              pathPrefix("memberEmails" / Segment) { userEmail =>
+                                withSubject(WorkbenchEmail(userEmail), samRequestContext) { subject =>
+                                  put {
+                                    requireActionsForAdminPutUserInPolicy(policyId, userInfo, samRequestContext) {
+                                      adminPutUserInPolicy(policyId, subject, samRequestContext)
+                                    }
+                                  } ~
+                                    delete {
+                                      requireActionsForAdminRemoveUserFromPolicy(policyId, userInfo, samRequestContext) {
+                                        adminRemoveUserFromPolicy(policyId, subject, samRequestContext)
+                                      }
+                                    }
+                                }
+                              }
+                            }
+                        }
+                      }
+                    }
+                  }
+                }
             }
           }
         }
