7adddfb3741f4d1b01c1f32977683fdbfb60190f
Matt Bemis <MatthewBemis@users.noreply.github.com>
2021-12-21 12:05:01 -0500

    AS-1050: If ToS enforcement is enabled, show the relevant models in Swagger (#566)
    * conditionally display ToS swagger

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SwaggerRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SwaggerRoutes.scala
@@ -1,18 +1,41 @@
 package org.broadinstitute.dsde.workbench.sam.api
 
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
 import akka.stream.scaladsl.Flow
 import akka.util.ByteString
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+import io.circe._
+import io.circe.yaml
+import io.circe.yaml.syntax._
+import org.broadinstitute.dsde.workbench.model.WorkbenchException
 import org.broadinstitute.dsde.workbench.sam.config.SwaggerConfig
+
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by dvoet on 7/18/17.
   */
-trait SwaggerRoutes {
+trait SwaggerRoutes extends LazyLogging {
   private val swaggerUiPath = "META-INF/resources/webjars/swagger-ui/3.52.3"
 
   val swaggerConfig: SwaggerConfig
+
+  // enable/disable ToS swagger based on a config flag
+  val enableTosInSwagger = Try(ConfigFactory.load().getBoolean("termsOfService.enabled")).toOption.getOrElse(false)
+
+  val swaggerContents: String = if (enableTosInSwagger) {
+    Try(mergeYamls("/swagger/api-docs.yaml", "/swagger/tos-enabled.yaml")) match {
+      case Success(merged) => merged
+      case Failure(ex) =>
+        logger.warn(s"Could not merge swagger yamls; defaulting to api-docs.yaml: ${ex.getMessage}", ex)
+        loadResource("/swagger/api-docs.yaml")
+    }
+  } else {
+    loadResource("/swagger/api-docs.yaml")
+  }
 
   val swaggerRoutes: server.Route = {
     path("") {
@@ -22,7 +45,7 @@ trait SwaggerRoutes {
     } ~
       path("api-docs.yaml") {
         get {
-          getFromResource("swagger/api-docs.yaml")
+          complete(HttpEntity(ContentTypes.`application/octet-stream`, swaggerContents.getBytes))
         }
       } ~
       // We have to be explicit about the paths here since we're matching at the root URL and we don't
@@ -65,6 +88,36 @@ trait SwaggerRoutes {
     } {
       getFromResource(s"$swaggerUiPath/index.html")
     }
+  }
+
+  private def loadResource(filename: String) = {
+    val source = scala.io.Source.fromInputStream(getClass.getResourceAsStream(filename))
+    try source.mkString finally source.close()
+  }
+
+  /**
+    * read the contents of multiple yaml files and return the merged value as a String
+    * @param filenames
+    * @return
+    */
+  private def mergeYamls(filenames: String*): String = {
+
+    // for each file, read it from disk, parse as yaml and return as a JsonObject
+    val swaggerJsons = filenames.map { filename =>
+      val contents: String = loadResource(filename)
+      yaml.parser.parse(contents) match {
+        case Right(json) => json.asObject.getOrElse(JsonObject.empty)
+        case Left(parsingFailure) => throw new WorkbenchException(parsingFailure.message, parsingFailure.underlying)
+      }
+    }
+
+    // merge all jsons, preferring rightmost
+    val mergedJson: JsonObject = swaggerJsons.foldRight(JsonObject.empty) { (x, y) =>
+      x.deepMerge(y)
+    }
+
+    // translate back to yaml and print as string
+    Json.fromJsonObject(mergedJson).asYaml.spaces2
   }
 
 }
