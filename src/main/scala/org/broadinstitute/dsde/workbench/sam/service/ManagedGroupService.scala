57b881a05bbc49c938a1c7b63faadfb6106c2db9
melissachang <10929390+melissachang@users.noreply.github.com>
2022-01-26 11:43:38 -0800

    Fix typos in group policy error message

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/service/ManagedGroupService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/service/ManagedGroupService.scala
@@ -230,7 +230,7 @@ object ManagedGroupService {
       case `adminNotifierValue` => ManagedGroupService.adminNotifierPolicyName
       case _ =>
         throw new WorkbenchExceptionWithErrorReport(
-          ErrorReport(StatusCodes.NotFound, "Policy name for managed groups must be one of: [\"admins\", \"members\"]"))
+          ErrorReport(StatusCodes.NotFound, s"Policy name for managed groups must be one of: ['$adminValue', '$memberValue']"))
     }
 
   def getRoleName(roleName: String): MangedGroupRoleName =
