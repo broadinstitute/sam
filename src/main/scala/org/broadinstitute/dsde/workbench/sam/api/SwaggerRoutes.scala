69a705b55a8925667428a8d890fcbe15b6c512de
Sky Rubenstein <blrubenstein@wpi.edu>
2022-03-04 13:09:32 -0500

    Upgrade to 4.5.2 swagger

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SwaggerRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/SwaggerRoutes.scala
@@ -19,7 +19,7 @@ import scala.util.{Failure, Success, Try}
   * Created by dvoet on 7/18/17.
   */
 trait SwaggerRoutes extends LazyLogging {
-  private val swaggerUiPath = "META-INF/resources/webjars/swagger-ui/4.1.3"
+  private val swaggerUiPath = "META-INF/resources/webjars/swagger-ui/4.5.2"
 
   val swaggerConfig: SwaggerConfig
 
