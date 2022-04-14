5180daad479da431500f58ac19a831f9e0d20817
Edmund Higham <ehigham@users.noreply.github.com>
2022-04-14 16:56:29 -0400

    [no-ticket] bump workbenchGoogle2 for new google-cloud-firestore version (#634)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,10 +12,10 @@ object Dependencies {
   val http4sVersion = "0.21.13"
 
   val workbenchUtilV   = "0.6-74c9fc2"
-  val workbenchUtil2V   = "0.1-74c9fc2"
+  val workbenchUtil2V   = "0.2-447afa5"
   val workbenchModelV  = "0.15-f9f0d4c"
   val workbenchGoogleV = "0.21-8ce5b9b"
-  val workbenchGoogle2V = "0.23-11a45ad"
+  val workbenchGoogle2V = "0.24-447afa5"
   val workbenchNotificationsV = "0.3-d74ff96"
   val monocleVersion = "2.0.3"
 
