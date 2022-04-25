bb84be0bc519416fd59f670488e7f83e17f3aac8
Scala Steward <me@scala-steward.org>
2022-04-25 09:13:32 +0200

    Update scalatest to 3.2.12

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
     "com.typesafe.akka"   %%  "akka-http"           % akkaHttpV,
     "com.typesafe.akka"   %%  "akka-testkit"        % akkaV     % "test",
     "com.typesafe.akka"   %%  "akka-slf4j"          % akkaV,
-    "org.scalatest"       %%  "scalatest"     % "3.2.3"   % Test,
+    "org.scalatest"       %%  "scalatest"     % "3.2.12"   % Test,
     "org.scalatestplus" %% "scalacheck-1-15" % "3.2.3.0" % Test,
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
 
