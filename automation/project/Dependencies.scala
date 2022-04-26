c1674c6940771d1187ff153946b5666d48fc9b15
broadbot <broadbot@broadinstitute.org>
2022-04-26 17:46:08 +0000

    Update scalacheck-1-15 to 3.2.11.0 in develop

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
     "com.typesafe.akka"   %%  "akka-testkit"        % akkaV     % "test",
     "com.typesafe.akka"   %%  "akka-slf4j"          % akkaV,
     "org.scalatest"       %%  "scalatest"     % "3.2.3"   % Test,
-    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.3.0" % Test,
+    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test,
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
 
     workbenchServiceTest,
