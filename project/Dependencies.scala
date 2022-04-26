ee32f7a83dcfb6b8aa5402b2cc1bf81f0e04d000
broadbot <broadbot@broadinstitute.org>
2022-04-26 17:35:58 +0000

    Update logback-classic to 1.2.11 in aj-scala-steward-branch

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   val jacksonCore: ModuleID =        "com.fasterxml.jackson.core" % "jackson-core"        % jacksonV
 
   val logstashLogback: ModuleID = "net.logstash.logback"      % "logstash-logback-encoder" % "6.6"
-  val logbackClassic: ModuleID = "ch.qos.logback"             %  "logback-classic" % "1.2.10"
+  val logbackClassic: ModuleID = "ch.qos.logback"             %  "logback-classic" % "1.2.11"
   val ravenLogback: ModuleID =   "com.getsentry.raven"        %  "raven-logback"   % "7.8.6"
   val scalaLogging: ModuleID =   "com.typesafe.scala-logging" %% "scala-logging"   % scalaLoggingV
   val swaggerUi: ModuleID =      "org.webjars.npm"                %  "swagger-ui-dist"      % "4.6.1"
