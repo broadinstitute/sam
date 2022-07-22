2006625a6a80c88bef63f84a91b76131a1aaaf1f
tlangs <tlangs@broadinstitute.org>
2022-07-22 19:52:17 -0400

    remove old distributed lock tests

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/sam/SamConfig.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/sam/SamConfig.scala
@@ -6,7 +6,6 @@ object SamConfig extends CommonConfig {
   // from common: qaEmail, pathToQAPem
   object GCS extends CommonGCS {
     val appsDomain = gcsConfig.getString("appsDomain")
-    val pathToSamTestFirestoreAccountPath = gcsConfig.getString("firestoreAccountPath")
     val serviceProject = gcsConfig.getString("serviceProject")
   }
 }
