18766fe4077dab24a4bcf439734dd26ea8bd8e34
broadbot <broadbot@broadinstitute.org>
2022-09-02 12:00:58 +0000

    Update workbench-google2 to 0.25-26f31cd

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val workbenchUtil2V   = "0.2-447afa5"
   val workbenchModelV  = "0.15-f9f0d4c"
   val workbenchGoogleV = "0.21-8ce5b9b"
-  val workbenchGoogle2V = "0.24-447afa5"
+  val workbenchGoogle2V = "0.25-26f31cd"
   val workbenchNotificationsV = "0.3-d74ff96"
   val workbenchOauth2V = "0.2-20f9225"
   val monocleVersion = "2.0.5"
