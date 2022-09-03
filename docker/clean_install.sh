f0e5d06f04a3d99a4431f1ed0e8df5c289340e62
Ivan <ichang@broadinstitute.org>
2022-09-03 10:32:57 -0400

    Simplified sam-build for IRWG under the assumption that legacy configs are ported into Helm.

--- docker/clean_install.sh
+++ docker/clean_install.sh
@@ -6,6 +6,6 @@ SAM_DIR=$1
 cd $SAM_DIR
 
 export SBT_OPTS="-Xms2g -Xmx2g"
-sbt 'set test in assembly := {}' clean assembly
+sbt 'set assembly / test := {}' clean assembly
 SAM_JAR=$(find target | grep 'sam.*\.jar')
 mv $SAM_JAR .
