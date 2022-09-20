89dca4bdab0ff5e1be94eb5b695976bec27ca6b5
Greg Polumbo <>
2022-09-20 10:24:33 -0400

    ID-243 User Info content test

--- smoke_test/tests/authenticated/sam_user_info_tests.py
+++ smoke_test/tests/authenticated/sam_user_info_tests.py
@@ -1,3 +1,5 @@
+import json
+
 from ..sam_smoke_tests import SamSmokeTests
 
 
@@ -9,3 +11,10 @@ class SamUserInfoTests(SamSmokeTests):
     def test_status_code_is_200(self):
         response = SamSmokeTests.call_sam(self.user_info_url(), SamSmokeTests.USER_TOKEN)
         self.assertEqual(response.status_code, 200, f"User info HTTP Status is not 200: {response.text}")
+
+    def test_content_contains_correct_keys(self):
+        response = SamSmokeTests.call_sam(self.user_info_url(), SamSmokeTests.USER_TOKEN)
+        user_info = json.loads(response.text)
+        self.assertListEqual(list(user_info.keys()),
+                             ["adminEnabled", "enabled", "userEmail", "userSubjectId"],
+                             "User Info object does not match expected fields")
