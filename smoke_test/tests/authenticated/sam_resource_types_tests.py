cfd64753fefc665dafa9ef527e50170235737c1a
Greg Polumbo <>
2022-09-22 16:41:30 -0400

    ID-243 PR feedback.  Copy pasta left some commented out code by accident

--- smoke_test/tests/authenticated/sam_resource_types_tests.py
+++ smoke_test/tests/authenticated/sam_resource_types_tests.py
@@ -11,10 +11,3 @@ class SamResourceTypesTests(SamSmokeTestCase):
     def test_status_code_is_200(self):
         response = SamSmokeTestCase.call_sam(self.resource_types_url(), SamSmokeTestCase.USER_TOKEN)
         self.assertEqual(response.status_code, 200, f"Resource Types HTTP Status is not 200: {response.text}")
-
-    # def test_content_contains_correct_keys(self):
-    #     response = SamSmokeTests.call_sam(self.user_info_url(), SamSmokeTests.USER_TOKEN)
-    #     user_info = json.loads(response.text)
-    #     self.assertListEqual(list(user_info.keys()),
-    #                          ["adminEnabled", "enabled", "userEmail", "userSubjectId"],
-    #                          "User Info object does not match expected fields")
