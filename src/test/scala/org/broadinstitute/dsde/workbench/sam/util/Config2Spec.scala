f3ed14d215bfd50556d6d333eb2790de3ff3bf2b
Kai <kfeldman@broadinstitute.org>
2022-10-13 14:17:07 -0400

    add test for sam2.conf load precedence

--- src/test/scala/org/broadinstitute/dsde/workbench/sam/util/Config2Spec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/util/Config2Spec.scala
@@ -4,6 +4,8 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.io.File
+
 class Config2Spec extends AnyFreeSpec with Matchers {
   "Config 2 test" - {
     "loads fruits.bananas" in {
@@ -12,9 +14,17 @@ class Config2Spec extends AnyFreeSpec with Matchers {
     }
 
     "loads variable from sam2.conf when the variable is present in both" in {
-      val config = ConfigFactory.load()
+      val config = ConfigFactory.defaultReference()
 
       config.getString("fruits.banana") shouldBe "yellow"
+      val file = new File("src/test/resources/sam2.conf")
+      val appConfig = ConfigFactory.parseFile(file)
+
+      appConfig.getString("fruits.banana") shouldBe "red"
+
+      val generalConfig = ConfigFactory.load(appConfig)
+      generalConfig.getString("fruits.banana") shouldBe "red"
+
     }
   }
 }
