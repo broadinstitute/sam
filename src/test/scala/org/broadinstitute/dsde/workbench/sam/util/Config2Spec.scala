442217fe24e4aaed4d2d9c07e8974e2b82e65f4d
Kai <kfeldman@broadinstitute.org>
2022-10-13 15:25:47 -0400

    Update Config2Spec.scala

--- src/test/scala/org/broadinstitute/dsde/workbench/sam/util/Config2Spec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/util/Config2Spec.scala
@@ -14,14 +14,16 @@ class Config2Spec extends AnyFreeSpec with Matchers {
     }
 
     "loads variable from sam2.conf when the variable is present in both" in {
+      // loads and combines all reference.conf files
       val config = ConfigFactory.defaultReference()
-
       config.getString("fruits.banana") shouldBe "yellow"
+
+      // force loads sam2.conf
       val file = new File("src/test/resources/sam2.conf")
       val appConfig = ConfigFactory.parseFile(file)
-
       appConfig.getString("fruits.banana") shouldBe "red"
 
+      // Assembles a standard configuration, overriding any values set in reference.conf
       val generalConfig = ConfigFactory.load(appConfig)
       generalConfig.getString("fruits.banana") shouldBe "red"
 
