eb43ab206f8b767675e2d822af9e5b5ec91a000f
Tristan Garwood <tristan.garwood@gmail.com>
2022-10-13 13:39:52 -0400

    Added second test, still WIP

--- src/test/scala/org/broadinstitute/dsde/workbench/sam/util/Config2Spec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/util/Config2Spec.scala
@@ -6,10 +6,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Config2Spec extends AnyFreeSpec with Matchers {
   "Config 2 test" - {
-    "loads connectionPoolSize" in {
+    "loads fruits.bananas" in {
       val config = ConfigFactory.load()
       config.getString("fruits.banana") shouldBe "yellow"
+    }
 
+    "loads variable from sam2.conf when the variable is present in both" in {
+      val config = ConfigFactory.load()
+
+      config.getString("fruits.banana") shouldBe "yellow"
     }
   }
 }
