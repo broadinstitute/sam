992cadb43188b1ee7c0c6752274d0da1d974e464
Trevyn Langsford <tlangs@broadinstitute.org>
2022-10-24 14:29:18 -0400

    ID-241 ID-244 override with env conf (#867)
    * Use env.conf file to map ENV_VARs to config options
    
    * Added AppConfig.load fn to be called in Boot.scala, added tests asserting override logic. Removed dead AppConfig test.
    
    * Moved env fallback readme to resources dir and linked in README.md.
    
    * manually parse and resolve env.conf
    
    * need to use any syntax
    
    Co-authored-by: Tristan Garwood <tristan.garwood@gmail.com>

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/Boot.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/Boot.scala
@@ -5,7 +5,6 @@ import akka.http.scaladsl.Http
 import cats.data.NonEmptyList
 import cats.effect._
 import cats.implicits._
-import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.dataaccess.PubSubNotificationDAO
 import org.broadinstitute.dsde.workbench.google.GoogleCredentialModes.{Json, Pem}
@@ -58,8 +57,7 @@ object Boot extends IOApp with LazyLogging {
     // we need an ActorSystem to host our application in
     implicit val system = ActorSystem("sam")
 
-    val config = ConfigFactory.load()
-    val appConfig = AppConfig.readConfig(config)
+    val appConfig = AppConfig.load
 
     val appDependencies = createAppDependencies(appConfig)
 
