6814a5f0801cdae24f2800563d1c350b7bb81dca
Greg Polumbo <>
2022-12-16 11:42:45 -0500

    ID-332 formatting/comments

--- src/test/scala/org/broadinstitute/dsde/workbench/sam/service/UserServiceSpecs/GetUserStatusSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/service/UserServiceSpecs/GetUserStatusSpec.scala
@@ -81,79 +81,28 @@ class GetUserStatusSpec extends UserServiceTestTraits {
     }
 
     describe("for an invited user") {
-        it("returns a status with all components disabled") {
-          // Setup
-          val samUser = genWorkbenchUserBoth.sample.get
-          val userService = TestUserServiceBuilder()
-            .withAllUsersGroup(allUsersGroup)
-            .withInvitedUser(samUser)
-            .withNoUsersHavingAcceptedTos()
-            .build
+      it("returns a status with all components disabled") {
+        // Setup
+        val samUser = genWorkbenchUserBoth.sample.get
+        val userService = TestUserServiceBuilder()
+          .withAllUsersGroup(allUsersGroup)
+          .withInvitedUser(samUser)
+          .withNoUsersHavingAcceptedTos()
+          .build
 
-          // Act
-          val resultingStatus = runAndWait(userService.getUserStatus(samUser.id, false, samRequestContext))
+        // Act
+        val resultingStatus = runAndWait(userService.getUserStatus(samUser.id, false, samRequestContext))
 
-          // Assert
-          inside(resultingStatus.value) { status =>
-            status should beForUser(samUser)
-            "google" shouldNot beEnabledIn(status)
-            "ldap" shouldNot beEnabledIn(status)
-            "allUsersGroup" shouldNot beEnabledIn(status)
-            "adminEnabled" shouldNot beEnabledIn(status)
-            "tosAccepted" shouldNot beEnabledIn(status)
-          }
+        // Assert
+        inside(resultingStatus.value) { status =>
+          status should beForUser(samUser)
+          "google" shouldNot beEnabledIn(status)
+          "ldap" shouldNot beEnabledIn(status)
+          "allUsersGroup" shouldNot beEnabledIn(status)
+          "adminEnabled" shouldNot beEnabledIn(status)
+          "tosAccepted" shouldNot beEnabledIn(status)
         }
       }
-
+    }
   }
-
-//  it should "return UserStatus.ldap and UserStatus.adminEnabled as false if user is disabled" in {
-//    when(dirDAO.isEnabled(disabledUser.id, samRequestContext)).thenReturn(IO(false))
-//    val status = service.getUserStatus(defaultUser.id, samRequestContext = samRequestContext).futureValue
-//    status.value.enabled("ldap") shouldBe false
-//    status.value.enabled("adminEnabled") shouldBe false
-//  }
-//
-//  it should "return UserStatus.allUsersGroup as false if user is not in the All_Users group" in {
-//    when(dirDAO.isGroupMember(allUsersGroup.id, defaultUser.id, samRequestContext)).thenReturn(IO(false))
-//    val status = service.getUserStatus(defaultUser.id, samRequestContext = samRequestContext).futureValue
-//    status.value.enabled("allUsersGroup") shouldBe false
-//  }
-//
-//  it should "return UserStatus.google as false if user is not a member of their proxy group on Google" in {
-//    when(googleExtensions.getUserStatus(enabledUser)).thenReturn(Future.successful(false))
-//    val status = service.getUserStatus(enabledUser.id, samRequestContext = samRequestContext).futureValue
-//    status.value.enabled("google") shouldBe false
-//  }
-//
-//  it should "not return UserStatus.tosAccepted or UserStatus.adminEnabled if user's TOS status is false" in {
-//    when(mockTosService.getTosStatus(enabledUser.id, samRequestContext)).thenReturn(IO(Option(false)))
-//    val status = service.getUserStatus(enabledUser.id, samRequestContext = samRequestContext).futureValue
-//    status.value.enabled shouldNot contain("tosAccepted")
-//    status.value.enabled shouldNot contain("adminEnabled")
-//  }
-//
-//  it should "not return UserStatus.tosAccepted or UserStatus.adminEnabled if user's TOS status is None" in {
-//    when(mockTosService.getTosStatus(enabledUser.id, samRequestContext)).thenReturn(IO(None))
-//    val status = service.getUserStatus(enabledUser.id, samRequestContext = samRequestContext).futureValue
-//    status.value.enabled shouldNot contain("tosAccepted")
-//    status.value.enabled shouldNot contain("adminEnabled")
-//  }
-//
-//  it should "return no status for a user that does not exist" in {
-//    when(dirDAO.loadUser(defaultUser.id, samRequestContext)).thenReturn(IO(None))
-//    service.getUserStatus(defaultUser.id, samRequestContext = samRequestContext).futureValue shouldBe None
-//  }
-//
-//  it should "return userDetailsOnly status when told to" in {
-//    val statusNoEnabled = service.getUserStatus(defaultUser.id, true, samRequestContext).futureValue
-//    statusNoEnabled shouldBe Some(UserStatus(UserStatusDetails(defaultUser.id, defaultUser.email), Map.empty))
-//  }
-//
-//  it should "return userDetailsOnly status for a disabled user" in {
-//    when(dirDAO.isEnabled(disabledUser.id, samRequestContext)).thenReturn(IO(false))
-//    val statusNoEnabled = service.getUserStatus(defaultUser.id, true, samRequestContext).futureValue
-//    statusNoEnabled shouldBe Some(UserStatus(UserStatusDetails(defaultUser.id, defaultUser.email), Map.empty))
-//  }
-
 }
