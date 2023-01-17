44b764245a36fbe7288f79998b597576e429b6f8
Ivan <ichang@broadinstitute.org>
2023-01-17 13:32:26 -0500

    revert cganges

--- project/Merging.scala
+++ project/Merging.scala
@@ -4,8 +4,8 @@ object Merging {
   def customMergeStrategy(oldStrategy: (String) => MergeStrategy): (String => MergeStrategy) = {
     case PathList("org", "joda", "time", "base", "BaseDateTime.class") => MergeStrategy.first
     case PathList("io", "sundr", _ @_*) => MergeStrategy.first
-    //case PathList("javax", "activation", _ @_*) => MergeStrategy.first
-    //case PathList("javax", "xml", _ @_*) => MergeStrategy.first
+    case PathList("javax", "activation", _ @_*) => MergeStrategy.first
+    case PathList("javax", "xml", _ @_*) => MergeStrategy.first
     case PathList("google", "protobuf", _ @_*) => MergeStrategy.first
     case x if x.endsWith("/ModuleUtil.class") => MergeStrategy.first
     case PathList("META-INF", "versions", "9", "module-info.class") => MergeStrategy.first
