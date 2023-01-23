862cd29a096256cd28a54aa43fe6d0dead3fe2ff
tlangs <tlangs@broadinstitute.org>
2023-01-23 11:31:29 -0500

    ID-407 Don't allow new users to use API

--- src/test/scala/org/broadinstitute/dsde/workbench/sam/service/TosServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/service/TosServiceSpec.scala
@@ -19,6 +19,7 @@ class TosServiceSpec extends AnyFlatSpec with TestSupport with BeforeAndAfterAll
   private val tosServiceEnabledV0 = new TosService(dirDAO, "example.com", TestSupport.tosConfig.copy(enabled = true, version = "0"))
   private val tosServiceEnabled = new TosService(dirDAO, "example.com", TestSupport.tosConfig.copy(enabled = true))
   private val tosServiceEnabledV2 = new TosService(dirDAO, "example.com", TestSupport.tosConfig.copy(enabled = true, version = "2"))
+  private val tosServiceGracePeriodEnabled = new TosService(dirDAO, "example.com", TestSupport.tosConfig.copy(enabled = true, version = "2", isGracePeriodEnabled = true))
   private val tosServiceDisabled = new TosService(dirDAO, "example.com", TestSupport.tosConfig.copy(enabled = false))
 
   override protected def beforeAll(): Unit = {
@@ -52,15 +53,7 @@ class TosServiceSpec extends AnyFlatSpec with TestSupport with BeforeAndAfterAll
     assertResult(expected = false, s"getTosStatus(${defaultUser.id}) should have returned false")(actual = getTosStatusResultRejected.get)
   }
 
-  it should "exclude service accounts from ToS checks" in {
-    assume(databaseEnabled, databaseEnabledClue)
-
-    dirDAO.createUser(serviceAccountUser, samRequestContext).unsafeRunSync()
-
-    tosServiceEnabledV0.isTermsOfServiceStatusAcceptable(serviceAccountUser) should be(true)
-  }
-
-  it should "accept new version of ToS" in {
+  it should "allow users to accept new version of ToS" in {
     assume(databaseEnabled, databaseEnabledClue)
 
     dirDAO.createUser(defaultUser, samRequestContext).unsafeRunSync()
@@ -72,5 +65,39 @@ class TosServiceSpec extends AnyFlatSpec with TestSupport with BeforeAndAfterAll
     tosServiceEnabledV2.getTosStatus(defaultUser.id, samRequestContext).unsafeRunSync() shouldBe Option(false)
     tosServiceEnabledV2.acceptTosStatus(defaultUser.id, samRequestContext).unsafeRunSync() shouldBe Option(true)
     tosServiceEnabledV2.getTosStatus(defaultUser.id, samRequestContext).unsafeRunSync() shouldBe Option(true)
+  }
+
+  "TosService.isTermsOfServiceStatusAcceptable" should "allow all requests to the API if TOS is disabled" in {
+    assume(databaseEnabled, databaseEnabledClue)
+    dirDAO.createUser(defaultUser, samRequestContext).unsafeRunSync()
+    tosServiceDisabled.isTermsOfServiceStatusAcceptable(defaultUser) should be(true)
+  }
+
+  it should "not allow users who have never accepted a ToS version to use the API, even with a grace period enabled" in {
+    assume(databaseEnabled, databaseEnabledClue)
+    dirDAO.createUser(defaultUser, samRequestContext).unsafeRunSync()
+    tosServiceGracePeriodEnabled.isTermsOfServiceStatusAcceptable(defaultUser) should be(false)
+  }
+
+  it should "allow users that have accepted a previous ToS version to use the API when the grace period is enabled" in {
+    assume(databaseEnabled, databaseEnabledClue)
+    dirDAO.createUser(defaultUser, samRequestContext).unsafeRunSync()
+    dirDAO.acceptTermsOfService(defaultUser.id, "1", samRequestContext).unsafeRunSync()
+    val userAcceptedPreviousVersion = dirDAO.loadUser(defaultUser.id, samRequestContext).unsafeRunSync().orNull
+    tosServiceGracePeriodEnabled.isTermsOfServiceStatusAcceptable(userAcceptedPreviousVersion) should be(true)
+  }
+
+  it should "not allow users that have accepted a previous ToS to use the API without a grace period" in {
+    assume(databaseEnabled, databaseEnabledClue)
+    dirDAO.createUser(defaultUser, samRequestContext).unsafeRunSync()
+    dirDAO.acceptTermsOfService(defaultUser.id, "1", samRequestContext).unsafeRunSync()
+    val userAcceptedPreviousVersion = dirDAO.loadUser(defaultUser.id, samRequestContext).unsafeRunSync().orNull
+    tosServiceEnabledV2.isTermsOfServiceStatusAcceptable(userAcceptedPreviousVersion) should be(false)
+  }
+
+  it should "exclude service accounts from ToS checks" in {
+    assume(databaseEnabled, databaseEnabledClue)
+    dirDAO.createUser(serviceAccountUser, samRequestContext).unsafeRunSync()
+    tosServiceEnabledV0.isTermsOfServiceStatusAcceptable(serviceAccountUser) should be(true)
   }
 }
