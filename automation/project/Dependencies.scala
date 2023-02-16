6af76bcd2983c35f889e1c8a4d0308f2ec431a3a
broadbot <broadbot@broadinstitute.org>
2023-02-16 12:03:17 +0000

    Revert commit(s) 30557440

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     "com.fasterxml.jackson.module" % ("jackson-module-scala_" + scalaV) % jacksonV,
     "ch.qos.logback" % "logback-classic" % "1.2.11",
     "org.slf4j" % "slf4j-api" % "2.0.3",
-    "net.logstash.logback" % "logstash-logback-encoder" % "7.2",
+    "net.logstash.logback" % "logstash-logback-encoder" % "6.6",
     "com.google.apis" % "google-api-services-oauth2" % "v1-rev112-1.20.0" excludeAll (
       ExclusionRule("com.google.guava", "guava-jdk5"),
       ExclusionRule("org.apache.httpcomponents", "httpclient")
