5ed5d5b9e500254a5cd04e5d49a85b8530d998d2
ichengchang <50877414+ichengchang@users.noreply.github.com>
2023-02-21 11:44:22 -0500

    QA-2050: Add Consumer Version Selector to SamProviderSpec to select consumer pacts from default branch. (#996)
    * Add consumer version selector based on branch.
    
    * Clean up

--- pact4s/src/test/scala/org/broadinstitute/dsde/workbench/sam/provider/SamProviderSpec.scala
+++ pact4s/src/test/scala/org/broadinstitute/dsde/workbench/sam/provider/SamProviderSpec.scala
@@ -134,6 +134,7 @@ class SamProviderSpec extends AnyFlatSpec with ScalatestRouteTest with MockTestS
       .PactBrokerWithSelectors(
         brokerUrl = pactBrokerUrl
       )
+      .withConsumerVersionSelectors(ConsumerVersionSelectors.mainBranch)
       .withAuth(BasicAuth(pactBrokerUser, pactBrokerPass))
   ).withHost("localhost").withPort(8080)
 
