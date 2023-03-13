3495d7d48b0c2344a166f528ea7ad384001b88f0
Tristan Garwood <tristan.garwood@gmail.com>
2023-03-13 11:55:36 -0400

    ID-361 Add sentry release and breadcrumbs. (#1016)

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/Boot.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/Boot.scala
@@ -45,10 +45,14 @@ import scala.util.control.NonFatal
 
 object Boot extends IOApp with LazyLogging {
   val sentryDsn: Option[String] = sys.env.get("SENTRY_DSN")
+  // This value comes from the sbt build as the value of the version key in Version.scala
+  val version: Option[String] = Option(getClass.getPackage.getImplementationVersion)
   private def initSentry(): Unit = sentryDsn.fold(logger.warn("No SENTRY_DSN found, not initializing Sentry.")) { dsn =>
     val options = new SentryOptions()
     options.setDsn(dsn)
     options.setEnvironment(sys.env.getOrElse("SENTRY_ENVIRONMENT", "unknown"))
+    options.setRelease(version.getOrElse("unknown"))
+
     Sentry.init(options)
     logger.info("Sentry initialized")
   }
