bf72600cd1fe7de26558572c17bd87e63c88b9c3
broadbot <broadbot@broadinstitute.org>
2023-03-17 12:03:02 +0000

    Update sentry, sentry-logback to 6.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val scalaCheckV = "1.14.3"
   val scalikejdbcVersion = "3.4.2"
   val postgresDriverVersion = "42.5.0"
-  val sentryVersion = "6.15.0"
+  val sentryVersion = "6.16.0"
 
   val workbenchLibV = "abd44a6"
   val workbenchUtilV = s"0.6-$workbenchLibV"
