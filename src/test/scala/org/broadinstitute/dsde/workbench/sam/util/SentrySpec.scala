dde450228c329e1ef6a9fff9d95972f82ab6e53f
Tristan Garwood <tristan.garwood@gmail.com>
2023-03-21 14:00:20 -0400

    ID-498 Filter spammy sentry issues v2. Add test. (#1027)
    * ID-498 Filter spammy sentry issues v2. Add test.
    
    * Update src/test/scala/org/broadinstitute/dsde/workbench/sam/util/SentrySpec.scala
    
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>

---
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/util/SentrySpec.scala
@@ -0,0 +1,33 @@
+package org.broadinstitute.dsde.workbench.sam.util
+
+import akka.http.scaladsl.model.StatusCodes
+import io.sentry.{Hint, SentryEvent}
+import org.broadinstitute.dsde.workbench.google.errorReportSource
+import org.broadinstitute.dsde.workbench.model.{ErrorReport, WorkbenchExceptionWithErrorReport}
+import org.broadinstitute.dsde.workbench.sam.TestSupport
+import org.broadinstitute.dsde.workbench.sam.util.Sentry.filterException
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class SentrySpec extends AnyFreeSpec with Matchers with BeforeAndAfterEach with TestSupport {
+
+  "Sentry" - {
+    "event filtering" - {
+      "should drop events based on status codes and statusCodesToSkip" in {
+        val throwable: Throwable = new WorkbenchExceptionWithErrorReport(ErrorReport(StatusCodes.BadRequest, "test 123"))
+        val event = new SentryEvent(throwable)
+
+        filterException(event, new Hint()) shouldEqual null
+      }
+
+      "should not drop events when status code is not in statusCodesToSkip" in {
+        val throwable: Throwable = new WorkbenchExceptionWithErrorReport(ErrorReport(StatusCodes.InternalServerError, "test 123"))
+        val event = new SentryEvent(throwable)
+
+        filterException(event, new Hint()) shouldEqual event
+      }
+    }
+  }
+
+}
