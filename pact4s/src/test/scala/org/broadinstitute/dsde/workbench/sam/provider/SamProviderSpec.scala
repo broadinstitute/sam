2b34ccb9c11736f3a74c253a93c6008dcdfc6086
Ivan <ichang@broadinstitute.org>
2023-04-06 13:23:43 -0400

    StO demo

--- pact4s/src/test/scala/org/broadinstitute/dsde/workbench/sam/provider/SamProviderSpec.scala
+++ pact4s/src/test/scala/org/broadinstitute/dsde/workbench/sam/provider/SamProviderSpec.scala
@@ -42,7 +42,7 @@ class SamProviderSpec extends AnyFlatSpec with ScalatestRouteTest with MockTestS
     when {
       userService.getUserStatusInfo(any[SamUser], any[SamRequestContext])
     } thenReturn {
-      // Add support for BPM consumer (Revision 3)
+      // Add support for BPM consumer (Revision 4)
       val userStatusInfo = UserStatusInfo("userSubjectId", "userEmail", true, false)
       IO.pure(userStatusInfo)
     }
