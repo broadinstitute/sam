f8750a4ba9e6ec5140f56a0e9b072a2c4b2af8f8
Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
2023-04-18 11:52:16 -0400

    Refactor CreateUser (#1008)
    * refactoring createUser
    
    * refactoring createUser
    
    * making new stuff the real stuff
    
    * Adding SamRequestContexts
    
    * New validate email method
    
    * New validate email method
    
    * logic for validateUserIds
    
    * cleaning up new createUser
    
    * First vanilla passing test
    
    * refactoring and cleaning up and getting tests to pass
    
    * more engoodening
    
    * more engoodening
    
    * more engoodening
    
    * more tests
    
    * more tests
    
    * Renamed previous iteration of MockDirectoryDaoBuilder to StatefulMockDirectoryDaoBuilder
    
    * Renamed previous iteration of MockDirectoryDaoBuilder to StatefulMockDirectoryDaoBuilder
    
    * Using refined MockDirectoryDaoBuilder in new tests
    
    * Using refined MockDirectoryDaoBuilder in new tests
    
    * Renamed MockCloudExtensionsBuilder to StatefulMockCloudExtensionsBuilder
    
    * Renamed MockCloudExtensionsBuilder to StatefulMockCloudExtensionsBuilder
    
    * Updated new tests to use mock service builders
    
    * MockDirectoryDaoBuilder was missing?!
    
    * MockDirectoryDaoBuilder file is being weird in my ide
    
    * covering scenarios
    
    * better matcher syntax for checking exceptions
    
    * added new and improved tests to UserServiceSpec
    
    * @DoNotDiscover
    
    * Removed old code from UserService
    
    * Fixed tests that were failing do to assume statement for skipping tests that need db
    
    * ID-486 final code cleanup and extracting methods
    
    * ID-486 IdiomaticMockito
    
    * ID-486 delete old CreateUserSpec use new one
    
    * ID-486 converted MockDirectoryDaoBuilder to use MockitoSugar stubbing syntax
    
    * ID-486 converted MockCloudExtensionsBuilder to use MockitoSugar stubbing syntax
    
    * ID-486 Use IdiomaticMockito instead of MockitoSugar
    
    * ID-486 refactored CloudExtensions.onGroupUpdate to return IO instead of converting IO to Future upon return
    
    * ID-486 commenting on a method that has caused a bunch of confusion
    
    * ID-486 rewrite updateUser - maybe clearer?
    
    * ID-486 PR feedback
    
    * ID-486 PR feedback
    
    * ID-486 scalafmt
    
    * ID-486 scalafmt
    
    * ID-486 fixing broken tests from merge with develop
    
    ---------
    
    Co-authored-by: Greg Polumbo <>

---
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/service/StatefulMockCloudExtensionsBuilder.scala
@@ -0,0 +1,117 @@
+package org.broadinstitute.dsde.workbench.sam.service
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import org.broadinstitute.dsde.workbench.model.{WorkbenchGroup, WorkbenchGroupIdentity, WorkbenchUserId}
+import org.broadinstitute.dsde.workbench.sam.dataAccess.DirectoryDAO
+import org.broadinstitute.dsde.workbench.sam.model.SamUser
+import org.broadinstitute.dsde.workbench.sam.util.SamRequestContext
+import org.mockito.Mockito.{RETURNS_SMART_NULLS, lenient}
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.scalatest.MockitoSugar
+import org.mockito.{ArgumentMatcher, ArgumentMatchers}
+
+import scala.concurrent.ExecutionContext
+
+// It is probably not a good thing that GoogleExtensions (the implementation of CloudExtensions) needs a DirectoryDAO
+// so that it can _update_ the database, but it does.  It makes things more complicated here.
+case class StatefulMockCloudExtensionsBuilder(directoryDAO: DirectoryDAO) extends MockitoSugar {
+  var maybeAllUsersGroup: Option[WorkbenchGroup] = None
+  val mockedCloudExtensions: CloudExtensions = mock[CloudExtensions](RETURNS_SMART_NULLS)
+
+  /** Constructor logic mocks up CloudExtensions as if in an "empty" state
+    */
+
+  // Surprisingly, the implementation will try to create the 'All Users' group in the Sam database if it does not
+  // already exist.  It probably shouldn't do that, but it does.  Mocking similar behavior here.
+  lenient()
+    .doAnswer { (invocation: InvocationOnMock) =>
+      val samRequestContext = invocation.getArgument[SamRequestContext](1)
+      val maybeGroup = directoryDAO.loadGroup(CloudExtensions.allUsersGroupName, samRequestContext).unsafeRunSync()
+      maybeGroup match {
+        case Some(group) => IO(group)
+        case None =>
+          throw new RuntimeException(
+            "Mocked exception.  Make sure the `directoryDAO` used to construct this " +
+              s"MockCloudExtensionsBuilder has an '${CloudExtensions.allUsersGroupName}' group in it.  If using a " +
+              s"mock `directoryDAO`, try building it with `MockDirectoryDaoBuilder.withAllUsersGroup()`"
+          )
+      }
+    }
+    .when(mockedCloudExtensions)
+    .getOrCreateAllUsersGroup(ArgumentMatchers.eq(directoryDAO), any[SamRequestContext])(any[ExecutionContext])
+
+  lenient()
+    .doAnswer { (invocation: InvocationOnMock) =>
+      val samUser = invocation.getArgument[SamUser](0)
+      makeUserAppearEnabled(samUser)
+      IO.unit
+    }
+    .when(mockedCloudExtensions)
+    .onUserCreate(any[SamUser], any[SamRequestContext])
+
+  lenient()
+    .doReturn(IO(false))
+    .when(mockedCloudExtensions)
+    .getUserStatus(any[SamUser])
+
+  lenient()
+    .doAnswer { (invocation: InvocationOnMock) =>
+      val samUser = invocation.getArgument[SamUser](0)
+      makeUserAppearEnabled(samUser)
+      IO.unit
+    }
+    .when(mockedCloudExtensions)
+    .onUserEnable(any[SamUser], any[SamRequestContext])
+
+  lenient()
+    .doReturn(IO.unit)
+    .when(mockedCloudExtensions)
+    .onGroupUpdate(any[Seq[WorkbenchGroupIdentity]], any[SamRequestContext])
+
+  lenient()
+    .doReturn(IO.unit)
+    .when(mockedCloudExtensions)
+    .onUserDelete(any[WorkbenchUserId], any[SamRequestContext])
+
+  def withEnabledUser(samUser: SamUser): StatefulMockCloudExtensionsBuilder = withEnabledUsers(Set(samUser))
+  def withEnabledUsers(samUsers: Iterable[SamUser]): StatefulMockCloudExtensionsBuilder = {
+    samUsers.foreach(makeUserAppearEnabled)
+    this
+  }
+
+  private def makeUserAppearEnabled(samUser: SamUser): Unit = {
+    // Real implementation just returns unit if the user already exists
+    lenient()
+      .doReturn(IO.unit)
+      .when(mockedCloudExtensions)
+      .onUserCreate(argThat(IsSameUserAs(samUser)), any[SamRequestContext])
+    lenient()
+      .doReturn(IO(true))
+      .when(mockedCloudExtensions)
+      .getUserStatus(argThat(IsSameUserAs(samUser)))
+  }
+
+  def withDisabledUser(samUser: SamUser): StatefulMockCloudExtensionsBuilder = withDisabledUsers(Set(samUser))
+
+  def withDisabledUsers(samUsers: Iterable[SamUser]): StatefulMockCloudExtensionsBuilder = {
+    samUsers.foreach(makeUserAppearDisabled)
+    this
+  }
+
+  private def makeUserAppearDisabled(samUser: SamUser): Unit = {
+    // Real implementation just returns unit if the user already exists
+    doReturn(IO.unit)
+      .when(mockedCloudExtensions)
+      .onUserCreate(argThat(IsSameUserAs(samUser)), any[SamRequestContext])
+    doReturn(IO(false))
+      .when(mockedCloudExtensions)
+      .getUserStatus(argThat(IsSameUserAs(samUser)))
+  }
+
+  def build: CloudExtensions = mockedCloudExtensions
+}
+
+case class IsSameUserAs(user: SamUser) extends ArgumentMatcher[SamUser] {
+  def matches(otherUser: SamUser): Boolean = user.id == otherUser.id
+}
