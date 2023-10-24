e34e2f0829dd0be21a14b8bcd678e3c4d17eb811
Greg Polumbo <>
2023-10-24 15:07:33 -0400

    ID-807 Adding route tests and some custom matchers

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/api/TermsOfServiceRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/api/TermsOfServiceRoutes.scala
@@ -1,10 +1,14 @@
 package org.broadinstitute.dsde.workbench.sam.api
 
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
+import org.broadinstitute.dsde.workbench.sam.model.TermsOfServiceDetails
+import org.broadinstitute.dsde.workbench.sam.model.api.SamJsonSupport._
 import org.broadinstitute.dsde.workbench.sam.service.TosService
 
+import java.time.Instant
 import scala.concurrent.ExecutionContext
 
 trait TermsOfServiceRoutes {
@@ -58,7 +62,7 @@ trait TermsOfServiceRoutes {
           pathPrefix("self") { // api/termsOfService/v1/user/self
             pathEndOrSingleSlash {
               get {
-                complete(StatusCodes.NotImplemented)
+                complete(StatusCodes.OK, TermsOfServiceDetails("", Instant.now, permitSystemUsage = false))
               }
             } ~
             pathPrefix("accept") { // api/termsOfService/v1/user/accept
@@ -80,7 +84,7 @@ trait TermsOfServiceRoutes {
           pathPrefix(Segment) { userId => // api/termsOfService/v1/user/{userId}
             pathEndOrSingleSlash {
               get {
-                complete(StatusCodes.NotImplemented)
+                complete(StatusCodes.OK, TermsOfServiceDetails("", Instant.now, permitSystemUsage = false))
               }
             } ~
             pathPrefix("history") { // api/termsOfService/v1/user/{userId}/history
--- src/test/scala/org/broadinstitute/dsde/workbench/sam/api/TermsOfServiceRouteSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/api/TermsOfServiceRouteSpec.scala
@@ -4,16 +4,20 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.broadinstitute.dsde.workbench.sam.TestSupport
 import org.broadinstitute.dsde.workbench.sam.TestSupport.{databaseEnabled, databaseEnabledClue}
+import org.broadinstitute.dsde.workbench.sam.model.TermsOfServiceDetails
 import org.scalatest.concurrent.Eventually.eventually
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import org.broadinstitute.dsde.workbench.sam.matchers.TermsOfServiceMatchers
+import org.broadinstitute.dsde.workbench.sam.model.api.SamJsonSupport._
 
-class TermsOfServiceRouteSpec extends AnyFlatSpec with Matchers with ScalatestRouteTest with TestSupport {
+class TermsOfServiceRouteSpec extends AnyFlatSpec with Matchers with ScalatestRouteTest with TestSupport with TermsOfServiceMatchers {
+  val samRoutes: TestSamRoutes = TestSamRoutes(Map.empty)
 
   "GET /tos/text" should "return the tos text" in {
     assume(databaseEnabled, databaseEnabledClue)
 
-    val samRoutes = TestSamRoutes(Map.empty)
     eventually {
       Get("/tos/text") ~> samRoutes.route ~> check {
         status shouldEqual StatusCodes.OK
@@ -23,11 +27,32 @@ class TermsOfServiceRouteSpec extends AnyFlatSpec with Matchers with ScalatestRo
   }
 
   "GET /privacy/text" should "return the privacy policy text" in {
-    val samRoutes = TestSamRoutes(Map.empty)
-    eventually {
-      Get("/privacy/text") ~> samRoutes.route ~> check {
-        status shouldEqual StatusCodes.OK
-        responseAs[String].isEmpty shouldBe false
+    Get("/privacy/text") ~> samRoutes.route ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[String].isEmpty shouldBe false
+    }
+  }
+
+  "GET /termsOfService/v1/user" should "not be handled" in {
+    Get("/termsOfService/v1/user") ~> samRoutes.route ~> check {
+      assert(!handled, "`GET /termsOfService/v1/user` should not be a handled route")
+    }
+  }
+
+  "GET /termsOfService/v1/user/self" should "return ToS status for the calling user" in {
+    Get("/termsOfService/v1/user/self") ~> samRoutes.route ~> check {
+      status shouldEqual StatusCodes.OK
+      withClue(s"${responseAs[String]} is not parsable as an instance of `TermsOfServiceDetails`.") {
+        responseAs[TermsOfServiceDetails]
+      }
+    }
+  }
+
+  "GET /termsOfService/v1/user/{USER_ID}" should "return an instance of `TermsOfServiceDetails`" in {
+    Get("/termsOfService/v1/user/123") ~> samRoutes.route ~> check {
+      status shouldEqual StatusCodes.OK
+      withClue(s"${responseAs[String]} is not parsable as an instance of `TermsOfServiceDetails`.") {
+        responseAs[TermsOfServiceDetails]
       }
     }
   }
