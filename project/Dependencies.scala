ddab253e689053957a64f21c7d07fff732122e80
broadbot <broadbot@broadinstitute.org>
2023-11-29 12:03:28 +0000

    Update exporter-trace to 0.26.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -134,7 +134,7 @@ object Dependencies {
   val otelPrometheusExporter: ModuleID = "io.opentelemetry" % "opentelemetry-exporter-prometheus" % (openTelemetryVersion + "-alpha")
 
   // Google cloud open telemetry exporters
-  var gcpOpenTelemetryExporterVersion = "0.25.2"
+  var gcpOpenTelemetryExporterVersion = "0.26.0"
   var googleTraceExporter: ModuleID = "com.google.cloud.opentelemetry" % "exporter-trace" % gcpOpenTelemetryExporterVersion
 
   val openTelemetryDependencies = Seq(
