9c4f11e379a28ecdcf53681694c9e81e46b134d1
Tristan Garwood <tristan.garwood@gmail.com>
2024-02-02 15:42:44 -0500

    deliberately fail as a test, REVERT ME

--- smoke_test/tests/authenticated/sam_resource_types_tests.py
+++ smoke_test/tests/authenticated/sam_resource_types_tests.py
@@ -9,5 +9,4 @@ class SamResourceTypesTests(SamSmokeTestCase):
         return SamSmokeTestCase.build_sam_url("/api/config/v1/resourceTypes")
 
     def test_status_code_is_200(self):
-        response = SamSmokeTestCase.call_sam(self.resource_types_url(), SamSmokeTestCase.USER_TOKEN)
-        self.assertEqual(response.status_code, 200, f"Resource Types HTTP Status is not 200: {response.text}")
+       self.assertEqual(1, 2)
