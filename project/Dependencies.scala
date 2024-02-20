cac92b03f905179af43ac74cb37aa956b77adb3d
dvoet <dvoet@users.noreply.github.com>
2024-02-20 16:20:08 -0500

    update TCL to get fewer metrics buckets (#1359)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   val workbenchOauth2V = s"0.5-$workbenchLibV"
   val monocleVersion = "2.0.5"
   val crlVersion = "1.2.30-SNAPSHOT"
-  val tclVersion = "0.1.11-SNAPSHOT"
+  val tclVersion = "0.1.19-SNAPSHOT"
   val slf4jVersion = "2.0.6"
 
   val excludeAkkaActor = ExclusionRule(organization = "com.typesafe.akka", name = "akka-actor_2.12")
