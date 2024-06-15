c3f1cb5f2edae8db4f138e8cc21589f953ee862a
broadbot <broadbot@broadinstitute.org>
2024-06-15 12:01:20 +0000

    Reformat with scalafmt 3.8.2
    Executed command: scalafmt --non-interactive

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -34,8 +34,10 @@ object Dependencies {
       ExclusionRule("com.google.guava", "guava-jdk5"),
       ExclusionRule("org.apache.httpcomponents", "httpclient")
     ),
-    "com.google.api-client" % "google-api-client" % "1.22.0" excludeAll (ExclusionRule("com.google.guava", "guava-jdk5"),
-    ExclusionRule("org.apache.httpcomponents", "httpclient")),
+    "com.google.api-client" % "google-api-client" % "1.22.0" excludeAll (
+      ExclusionRule("com.google.guava", "guava-jdk5"),
+      ExclusionRule("org.apache.httpcomponents", "httpclient")
+    ),
     "com.typesafe.akka" %% "akka-http-core" % akkaHttpV,
     "com.typesafe.akka" %% "akka-stream-testkit" % akkaV,
     "com.typesafe.akka" %% "akka-http" % akkaHttpV,
