0ba176c49d125d4807b9bbaad3e459a963ed4c00
tlangs <tlangs@broadinstitute.org>
2024-09-17 10:16:39 -0400

    Correct error status code

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleExtensions.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleExtensions.scala
@@ -561,10 +561,18 @@ class GoogleExtensions(
       destinationProjectNumber <- petServiceAgents
         .map(psa => IO.pure(Option(psa.destinationProjectNumber)))
         .getOrElse(IO.fromFuture(IO(googleProjectDAO.getProjectNumber(destinationProject.value))))
-        .map(_.getOrElse(throw new WorkbenchException(s"Could not find project number for project ${destinationProject.value}")))
+        .map(
+          _.getOrElse(
+            throw new WorkbenchExceptionWithErrorReport(
+              ErrorReport(StatusCodes.BadRequest, s"Could not find project number for project ${destinationProject.value}")
+            )
+          )
+        )
       ancestry <- IO.fromFuture(IO(googleProjectDAO.getAncestry(destinationProject.value)))
       organization = ancestry.find(_.getResourceId.getType.equals("organization")).map(_.getResourceId)
-      _ <- IO.raiseWhen(organization.isEmpty)(new WorkbenchException(s"Project $destinationProject is not in an organization"))
+      _ <- IO.raiseWhen(organization.isEmpty)(
+        new WorkbenchExceptionWithErrorReport(ErrorReport(StatusCodes.BadRequest, s"Project $destinationProject is not in an organization"))
+      )
       _ <- IO.raiseUnless(organization.exists(_.getId.equals(googleServicesConfig.terraGoogleOrgNumber)))(
         new WorkbenchException(s"Project $destinationProject is not in organization ${googleServicesConfig.terraGoogleOrgNumber}")
       )
