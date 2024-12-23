31dd8e2df07cae96c5e43f5610dd0b39459da4e4
broadbot <broadbot@broadinstitute.org>
2024-12-23 12:11:14 +0000

    CORE-69: Update logback-classic from 1.5.12 to 1.5.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
   val jacksonCore: ModuleID = "com.fasterxml.jackson.core" % "jackson-core" % jacksonV
 
   val logstashLogback: ModuleID = "net.logstash.logback" % "logstash-logback-encoder" % "8.0"
-  val logbackClassic: ModuleID = "ch.qos.logback" % "logback-classic" % "1.5.12"
+  val logbackClassic: ModuleID = "ch.qos.logback" % "logback-classic" % "1.5.15"
   val ravenLogback: ModuleID = "com.getsentry.raven" % "raven-logback" % "7.8.6"
   val scalaLogging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % scalaLoggingV
   val ficus: ModuleID = "com.iheart" %% "ficus" % "1.5.2"
