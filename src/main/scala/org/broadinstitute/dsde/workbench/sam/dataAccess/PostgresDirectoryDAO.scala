c016616e9759134494f06ab5c9bb227840f72073
David An <davidan@broadinstitute.org>
2025-01-09 15:53:14 -0500

    fix unsynced count

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/dataAccess/PostgresDirectoryDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/dataAccess/PostgresDirectoryDAO.scala
@@ -25,6 +25,7 @@ import org.broadinstitute.dsde.workbench.sam.db._
 import org.broadinstitute.dsde.workbench.sam.db.tables._
 import org.broadinstitute.dsde.workbench.sam.model._
 import org.broadinstitute.dsde.workbench.sam.model.api.{AdminUpdateUserRequest, SamUser, SamUserAttributes}
+import org.broadinstitute.dsde.workbench.sam.service.CloudExtensions
 import org.broadinstitute.dsde.workbench.sam.util.{DatabaseSupport, SamRequestContext}
 import org.postgresql.util.PSQLException
 import scalikejdbc._
@@ -756,10 +757,13 @@ class PostgresDirectoryDAO(protected val writeDbRef: DbReference, protected val 
 
   override def countUnsynchronizedGroupMemberships(samUser: SamUser, samRequestContext: SamRequestContext): IO[Int] =
     readOnlyTransaction("countUnsynchronizedGroupMemberships", samRequestContext) { implicit session =>
+      // this query special-cases for the All_Users group, which otherwise appears out of sync
       val query = samsql"""select count(distinct g.id) unsynchronizedMembershipCount
                           from sam_group g
                           join sam_group_member_flat gmf on g.id = gmf.group_id
-                          where g.synchronized_date is null
+                          where g.synchronized_date is not null
+                          and g.version > g.last_synchronized_version
+                          and g.name != ${CloudExtensions.allUsersGroupName}
                           and gmf.member_user_id = ${samUser.id}"""
 
       query.map(rs => rs.int(1)).single().apply().getOrElse(0)
--- src/test/scala/org/broadinstitute/dsde/workbench/sam/dataAccess/PostgresDirectoryDAOSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/sam/dataAccess/PostgresDirectoryDAOSpec.scala
@@ -756,12 +756,19 @@ class PostgresDirectoryDAOSpec extends RetryableAnyFreeSpec with Matchers with B
       "calculate the unsynchronized count" in {
         assume(databaseEnabled, databaseEnabledClue)
 
-        // create but do not synchronize the test groups;
-        // countUnsynchronizedGroupMemberships() only counts unsynchronized groups.
-        createDirectAndIndirectGroups(syncGroups = false)
+        val (parentGroup, _) = createDirectAndIndirectGroups(syncGroups = true)
 
+        // with all groups synced, unsynchronized count should be 0
         val unsyncedCount = dao.countUnsynchronizedGroupMemberships(defaultUser, samRequestContext).unsafeRunSync()
-        unsyncedCount shouldBe 2
+        unsyncedCount shouldBe 0
+
+        // add a user to the group, causing it to be out of sync
+        dao.addGroupMember(parentGroup.id, defaultUser.id, samRequestContext).unsafeRunSync() shouldBe true
+
+        // unsynchronized count should now find the unsynced group
+        val unsyncedCount2 = dao.countUnsynchronizedGroupMemberships(defaultUser, samRequestContext).unsafeRunSync()
+        unsyncedCount2 shouldBe 1
+
       }
     }
 
