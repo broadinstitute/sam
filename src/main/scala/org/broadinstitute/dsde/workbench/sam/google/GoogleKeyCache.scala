d5b0fa99b858b03b31c0dbb3799b1e43bd750762
David An <davidan@broadinstitute.org>
2025-02-06 14:37:32 -0500

    cleanup and logging

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleKeyCache.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/google/GoogleKeyCache.scala
@@ -13,7 +13,7 @@ import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.google.{GoogleIamDAO, GooglePubSubDAO, GoogleStorageDAO}
 import org.broadinstitute.dsde.workbench.google2.{GcsBlobName, GoogleStorageService}
 import org.broadinstitute.dsde.workbench.model._
-import org.broadinstitute.dsde.workbench.model.google.{GcsObjectName, GoogleProject, ServiceAccountKey, ServiceAccountKeyId}
+import org.broadinstitute.dsde.workbench.model.google.{GoogleProject, ServiceAccountKey, ServiceAccountKeyId}
 import org.broadinstitute.dsde.workbench.sam.config.{GoogleServicesConfig, PetServiceAccountConfig}
 import org.broadinstitute.dsde.workbench.sam.service.KeyCache
 import fs2.Stream
@@ -21,8 +21,7 @@ import org.broadinstitute.dsde.workbench.sam.dataAccess.{LockDetails, PostgresDi
 import org.broadinstitute.dsde.workbench.sam.model.CachedKey
 
 import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
-import org.broadinstitute.dsde.workbench.sam.model.CachedKey.keyPathPattern
+import scala.concurrent.ExecutionContext
 
 import java.time.Instant
 
@@ -97,42 +96,55 @@ class GoogleKeyCache(
     } yield ()
   }
 
+  /** Retrieve a key for this pet, creating keys as necessary.
+    */
   override def getKey(pet: PetServiceAccount): IO[String] = {
-    def maybeCreateKey(createKey: (List[GcsObjectName], List[ServiceAccountKey]) => IO[String]): IO[String] =
+    def maybeCreateKey(withinLock: Boolean, fallback: () => IO[String]): IO[String] =
       for {
-        (maybeActiveKey, keysFromCache, keysFromIam) <- retrieveActiveKey(pet)
+        maybeActiveKey <- findActiveKey(pet, withinLock)
         activeKey <- maybeActiveKey match {
           case Some(existingActiveKey) => IO.pure(existingActiveKey)
-          case None => createKey(keysFromCache, keysFromIam)
+          case None => fallback()
         }
       } yield activeKey
 
-    def cleanupAndCreateKey(keysFromCache: List[GcsObjectName], keysFromIam: List[ServiceAccountKey]): IO[String] = {
-      logger.info(
-        s"cleanupAndCreateKey: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} with ${keysFromCache.length} in cache and ${keysFromIam.length} in IAM"
-      )
-      for {
-        _ <- IO.fromFuture(IO(cleanupUnknownKeys(pet, keysFromCache, keysFromIam)))
-        key <- furnishNewKey(pet)
-      } yield key
-    }
+    def failed(): IO[String] =
+      throw new WorkbenchException("Could not create or retrieve key")
 
     // 5 minute lock timeout chosen to match how long key creation polling can take
     val lockDetails = LockDetails(s"${pet.id.project.value}-getKey", pet.serviceAccount.subjectId.value, 5 minutes)
-    maybeCreateKey((_, _) => distributedLock.withLock(lockDetails).use(_ => maybeCreateKey(cleanupAndCreateKey)))
+    maybeCreateKey(withinLock = false, () => distributedLock.withLock(lockDetails).use(_ => maybeCreateKey(withinLock = true, () => failed())))
   }
 
+  /** List all cached keys from the Google bucket for this pet.
+    */
   private def fetchKeysFromCache(pet: PetServiceAccount): IO[List[CachedKey]] =
     googleStorageAlg
       .unsafeListObjectsWithPrefix(googleServicesConfig.googleKeyCacheConfig.bucketName, keyNamePrefix(pet.id.project, pet.serviceAccount.email))
       .map(gcsObjectList => gcsObjectList.map(CachedKey(_)))
 
+  /** List all keys in IAM for this pet.
+    */
+  private def fetchKeysFromIam(pet: PetServiceAccount): IO[List[ServiceAccountKey]] =
+    IO.fromFuture(IO(googleIamDAO.listUserManagedServiceAccountKeys(pet.id.project, pet.serviceAccount.email).map(_.toList)))
+
+  /** List all cached keys from the Google bucket, then try to find a usable key from that list.
+    */
   private def findActiveKey(pet: PetServiceAccount, withinLock: Boolean): IO[Option[String]] =
     for {
       keysInCache <- fetchKeysFromCache(pet)
       maybeActiveKey = searchCachedKeys(pet, keysInCache, withinLock)
     } yield maybeActiveKey
 
+  /** Given a list of cached keys, return the most appropriate cached key. The priority order of keys is:
+    *   1. keys older than 15 minutes but younger than 12 days 2. keys older than 12 days, which may be nearing the end of their life 3. keys younger than 15
+    *      minutes, which may not yet be functional due to Google eventual-consistency
+    *
+    * When executing outside a lock, based on the withinLock parameter, this function operates in read-only mode and will return None in any cases where we
+    * should be creating a new key.
+    *
+    * When executing inside a lock, based on the withinLock parameter, this function operates in read/write mode. It will create new keys when necessary.
+    */
   protected[google] def searchCachedKeys(pet: PetServiceAccount, keysInCache: List[CachedKey], withinLock: Boolean): Option[String] = {
     // helpers
     def oldestOf(keys: List[CachedKey]): Option[String] =
@@ -158,9 +170,15 @@ class GoogleKeyCache(
       newestOf(idealKeys)
     } else if (nascentKeys.nonEmpty && retiredKeys.isEmpty) {
       // if any nascent keys exist but no retired keys exist, return the oldest nascent key
+      logger.warn(
+        s"searchCachedKeys: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} is using a nascent key"
+      )
       oldestOf(nascentKeys)
     } else if (nascentKeys.nonEmpty && retiredKeys.nonEmpty) {
       // if both nascent and retired keys exist, return the newest retired key
+      logger.warn(
+        s"searchCachedKeys: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} is using a retired key; nascent keys exist"
+      )
       newestOf(retiredKeys)
     } else if (!withinLock) {
       // if not within lock, return None. This will signal callers to obtain a lock and re-call this function.
@@ -168,43 +186,32 @@ class GoogleKeyCache(
     } else if (nascentKeys.isEmpty && retiredKeys.nonEmpty) {
       // if within lock, retired keys exist, and no nascent keys exist:
       // return newest retired key and trigger key creation
-      furnishNewKey(pet).map(_ => newestOf(retiredKeys)).unsafeRunSync()
+      cleanupAndCreateKey(pet, keysInCache)
+        .map { _ =>
+          logger.warn(
+            s"searchCachedKeys: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} is using a retired key; no nascent keys exist"
+          )
+          newestOf(retiredKeys)
+        }
+        .unsafeRunSync()
     } else {
       // no keys exist; create one and return it
-      Option(furnishNewKey(pet).unsafeRunSync())
+      Option(
+        cleanupAndCreateKey(pet, keysInCache)
+          .map { createdKey =>
+            logger.warn(
+              s"searchCachedKeys: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} is using a just-created key"
+            )
+            createdKey
+          }
+          .unsafeRunSync()
+      )
     }
 
   }
 
-  @deprecated
-  private def fetchKeysFromCacheAndIam(pet: PetServiceAccount): IO[(List[GcsObjectName], List[ServiceAccountKey])] = {
-    val fetchKeyFromCache = googleStorageAlg
-      .unsafeListObjectsWithPrefix(googleServicesConfig.googleKeyCacheConfig.bucketName, keyNamePrefix(pet.id.project, pet.serviceAccount.email))
-    val fetchKeyFromIam = IO.fromFuture(IO(googleIamDAO.listUserManagedServiceAccountKeys(pet.id.project, pet.serviceAccount.email).map(_.toList)))
-
-    (fetchKeyFromCache, fetchKeyFromIam).parTupled
-  }
-
-  // TODO CORE-278: refactor this
-  @deprecated
-  private def retrieveActiveKey(pet: PetServiceAccount): IO[(Option[String], List[GcsObjectName], List[ServiceAccountKey])] =
-    for {
-      (keysFromCache, keysFromIam) <- fetchKeysFromCacheAndIam(pet)
-
-      // TODO CORE-278: this could result in log spam
-      _ = if (keysFromIam.length >= 10)
-        logger.warn(s"danger: pet ${pet.serviceAccount.displayName.value} has ${keysFromIam.length} keys (cache has ${keysFromCache.length})")
-
-      maybeActiveKey = keysFromCache.sortBy(_.timeCreated.toEpochMilli).findLast(isKeyActive(_, keysFromIam))
-      activeKey <- maybeActiveKey
-        .map { key =>
-          googleStorageAlg
-            .unsafeGetBlobBody(googleServicesConfig.googleKeyCacheConfig.bucketName, GcsBlobName(key.value))
-        }
-        .getOrElse(IO.pure(None))
-
-    } yield (activeKey, keysFromCache, keysFromIam)
-
+  /** Delete a key from IAM and cache.
+    */
   override def removeKey(pet: PetServiceAccount, keyId: ServiceAccountKeyId): IO[Unit] =
     for {
       _ <- googleStorageAlg
@@ -219,19 +226,40 @@ class GoogleKeyCache(
   private def keyNameFull(project: GoogleProject, saEmail: WorkbenchEmail, keyId: ServiceAccountKeyId): GcsBlobName =
     GcsBlobName(s"${keyNamePrefix(project, saEmail)}${keyId.value}")
 
+  /** Cleans up keys by deleting orphans and bringing total key count down to 9, then creates a new key.
+    */
+  private def cleanupAndCreateKey(pet: PetServiceAccount, cachedKeyObjects: List[CachedKey]): IO[String] =
+    for {
+      keysInIam <- fetchKeysFromIam(pet)
+      _ <- cleanupKeys(pet, cachedKeyObjects, keysInIam)
+      newKey <- furnishNewKey(pet)
+    } yield newKey
+
+  /** Create a new key for this pet. Do not call this directly, as it has no protection against running over the 10-key limit. Call cleanupAndCreateKey()
+    * instead.
+    */
   private def furnishNewKey(pet: PetServiceAccount): IO[String] =
     for {
       key <- IO.fromFuture(IO(googleIamDAO.createServiceAccountKey(pet.id.project, pet.serviceAccount.email))) recover {
-        // TODO CORE-278: on error, check the number of existing keys and purge as necessary
         case e: GoogleJsonResponseException =>
+          logger.error(
+            s"furnishNewKey: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} ${e.getMessage}",
+            e
+          )
           if (e.getDetails.getCode == StatusCodes.TooManyRequests.intValue)
-            // 2025-01-29: TooManyRequests is not returned by Google for this error any more. Leaving this in place
+            // 2025-01-29: TooManyRequests is not returned by Google for this error anymore. Leaving this in place
             //  in case Google switches back to it
             throw new WorkbenchException("You have reached the 10 key limit on service accounts. Please remove one to create another.")
           else if (e.getDetails.getCode == StatusCodes.BadRequest.intValue && e.getDetails.getMessage == "Precondition check failed.")
             throw new WorkbenchException("You may have reached the 10 key limit on service accounts.")
           else
             throw new WorkbenchException(s"Error creating key for service account: ${e.getDetails.getCode}: ${e.getDetails.getMessage}")
+        case t: Throwable =>
+          logger.error(
+            s"furnishNewKey: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} ${t.getMessage}",
+            t
+          )
+          throw new WorkbenchException(s"Error creating key for service account: ${t.getMessage}")
       }
       decodedKey <- IO.fromEither(key.privateKeyData.decode.toRight(new WorkbenchException("Failed to decode retrieved key")))
       _ <- (Stream.emits(decodedKey.getBytes(utf8Charset)).covary[IO] through googleStorageAlg.streamUploadBlob(
@@ -240,25 +268,39 @@ class GoogleKeyCache(
       )).compile.drain
     } yield decodedKey
 
-  private def isKeyActive(mostRecentKey: GcsObjectName, serviceAccountKeys: List[ServiceAccountKey]): Boolean = {
-    val keyRetired = System.currentTimeMillis() - mostRecentKey.timeCreated.toEpochMilli > 86400000L * googleServicesConfig.googleKeyCacheConfig.activeKeyMaxAge
+  /** Cleans up keys by deleting orphans and bringing total key count down to 9.
+    */
+  private def cleanupKeys(pet: PetServiceAccount, cachedKeyObjects: List[CachedKey], serviceAccountKeys: List[ServiceAccountKey]): IO[Unit] = {
+    // if 10 or more keys, delete until we are below 10.
+    val tooManyFunction: IO[_] = if (cachedKeyObjects.size >= 10) {
+      val numToDelete = cachedKeyObjects.size - 9
+      val toDelete = cachedKeyObjects.sortBy(_.timeCreated).takeRight(numToDelete)
+      logger.info(
+        s"cleanupKeys: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} is reaping ${toDelete.size} keys due to quota"
+      )
+      toDelete.parTraverse(cachedKey => removeKey(pet, cachedKey.keyId))
+    } else {
+      IO.unit
+    }
 
-    val keyPathPattern(project, petSaEmail, keyId) = mostRecentKey.value
+    // if any keys in IAM which are not in cache, delete those orphans.
+    val cachedKeyIds: Set[ServiceAccountKeyId] = cachedKeyObjects.map(_.keyId).toSet
+    val unknownKeyIds: Set[ServiceAccountKeyId] = serviceAccountKeys.map(_.id).toSet -- cachedKeyIds
 
-    // The key may exist in the Google bucket cache, but could have been deleted from the SA directly
-    val keyExistsForSA = serviceAccountKeys.exists(_.id.value.contentEquals(keyId))
+    val orphansFunction: IO[_] = if (unknownKeyIds.nonEmpty) {
+      logger.info(
+        s"cleanupKeys: ${pet.id.project.value}-${pet.serviceAccount.subjectId.value} is reaping ${unknownKeyIds.size} orphaned keys"
+      )
+      unknownKeyIds.toList.parTraverse(keyId => IO.fromFuture(IO(googleIamDAO.removeServiceAccountKey(pet.id.project, pet.serviceAccount.email, keyId))))
+    } else {
+      IO.unit
+    }
 
-    !keyRetired && keyExistsForSA
+    for {
+      _ <- tooManyFunction
+      _ <- orphansFunction
+    } yield ()
+
   }
 
-  private def cleanupUnknownKeys(pet: PetServiceAccount, cachedKeyObjects: List[GcsObjectName], serviceAccountKeys: List[ServiceAccountKey]): Future[Unit] = {
-    val cachedKeyIds = cachedKeyObjects.map(_.value).collect { case keyPathPattern(_, _, keyId) => ServiceAccountKeyId(keyId) }
-    val unknownKeyIds: Set[ServiceAccountKeyId] = serviceAccountKeys.map(_.id).toSet -- cachedKeyIds.toSet
-
-    Future
-      .traverse(unknownKeyIds) { keyId =>
-        googleIamDAO.removeServiceAccountKey(pet.id.project, pet.serviceAccount.email, keyId)
-      }
-      .void
-  }
 }
