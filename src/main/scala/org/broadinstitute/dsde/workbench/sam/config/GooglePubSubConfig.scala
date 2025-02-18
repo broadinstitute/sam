64372058c2adc754c06803c25e1f1b777f429fff
Douglas Voet <dvoet@users.noreply.github.com>
2025-02-18 16:24:28 -0500

    configure deadline extension, auth domain sync shortcut

--- src/main/scala/org/broadinstitute/dsde/workbench/sam/config/GooglePubSubConfig.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/sam/config/GooglePubSubConfig.scala
@@ -1,3 +1,5 @@
 package org.broadinstitute.dsde.workbench.sam.config
 
-case class GooglePubSubConfig(project: String, topic: String, subscription: String, workerCount: Int)
+import java.time.Duration
+
+case class GooglePubSubConfig(project: String, topic: String, subscription: String, workerCount: Int, maxAckExtensionPeriod: Duration)
