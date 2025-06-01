9695a35b0dcb1826f683b751032e97b0b6da54fb
broadbot <broadbot@broadinstitute.org>
2025-06-01 12:20:48 +0000

    CORE-69: Update grpc-core from 1.69.1 to 1.73.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -69,7 +69,7 @@ object Dependencies {
   val reactorNetty: ModuleID = "io.projectreactor.netty" % "reactor-netty" % "1.2.2"
 
   val excludIoGrpc = ExclusionRule(organization = "io.grpc", name = "grpc-core")
-  val ioGrpc: ModuleID = "io.grpc" % "grpc-core" % "1.69.1"
+  val ioGrpc: ModuleID = "io.grpc" % "grpc-core" % "1.73.0"
 
   val googleOAuth2: ModuleID = "com.google.auth" % "google-auth-library-oauth2-http" % "0.18.0" excludeAll excludIoGrpc
   val googleStorage: ModuleID = "com.google.apis" % "google-api-services-storage" % "v1-rev20250420-2.0.0" excludeAll excludIoGrpc // force this version
