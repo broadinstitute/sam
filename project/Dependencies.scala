e15bdc460355080180f56925614b6f548f57a177
broadbot <broadbot@broadinstitute.org>
2025-06-01 12:21:03 +0000

    CORE-69: Update postgresql from 42.7.5 to 42.7.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scalaTestV = "3.2.19"
   val scalaCheckV = "1.18.1"
   val scalikejdbcVersion = "3.4.2"
-  val postgresDriverVersion = "42.7.5"
+  val postgresDriverVersion = "42.7.6"
   val sentryVersion = "6.15.0"
 
   val workbenchLibV = "fdb7b18" // If updating this, make sure googleStorageLocal in test dependencies is up-to-date
