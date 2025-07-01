95c13fd090bbb28f5b1a5a66b471a93f513f94ce
broadbot <broadbot@broadinstitute.org>
2025-07-01 12:23:09 +0000

    CORE-69: Update reactor-netty from 1.2.6 to 1.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -66,7 +66,7 @@ object Dependencies {
   val scalaCheck: ModuleID = "org.scalacheck" %% "scalacheck" % scalaCheckV % "test"
 
   val nettyAll: ModuleID = "io.netty" % "netty-all" % "4.2.1.Final"
-  val reactorNetty: ModuleID = "io.projectreactor.netty" % "reactor-netty" % "1.2.6"
+  val reactorNetty: ModuleID = "io.projectreactor.netty" % "reactor-netty" % "1.2.7"
 
   val excludIoGrpc = ExclusionRule(organization = "io.grpc", name = "grpc-core")
   val ioGrpc: ModuleID = "io.grpc" % "grpc-core" % "1.73.0"
