bcf0ef42a7cb396adf6cdd6d331a53c77e7eb50b
Bria Morgan <bmorgan@broadinstitute.org>
2025-07-10 15:44:39 -0400

    publish to gar

--- codegen_java/project/Publishing.scala
+++ codegen_java/project/Publishing.scala
@@ -6,32 +6,23 @@ import Artifactory._
   */
 
 object Publishing {
-  private val buildTimestamp = System.currentTimeMillis() / 1000
 
-  private def artifactoryResolver(isSnapshot: Boolean): Resolver = {
+  private val garBase = "artifactregistry://us-central1-maven.pkg.dev/dsp-artifact-registry/"
+
+  private def garResolver(): Resolver = {
+    val isSnapshot = sys.props.getOrElse("project.isSnapshot", "false").toBoolean
     val repoType = if (isSnapshot) "snapshot" else "release"
-    val repoUrl =
-      s"${artifactory}libs-$repoType-local;build.timestamp=$buildTimestamp"
-    val repoName = "artifactory-publish"
+    val repoUrl = s"${garBase}libs-$repoType-standard"
+    val repoName = "gar-publish"
     repoName at repoUrl
   }
 
-  private val artifactoryCredentials: Credentials = {
-    val username = sys.env.getOrElse("ARTIFACTORY_USERNAME", "")
-    val password = sys.env.getOrElse("ARTIFACTORY_PASSWORD", "")
-    Credentials("Artifactory Realm", artifactoryHost, username, password)
-  }
-
-  val publishSettings: Seq[Setting[_]] =
-    // we only publish to libs-release-local because of a bug in sbt that makes snapshots take
-    // priority over the local package cache. see here: https://github.com/sbt/sbt/issues/2687#issuecomment-236586241
-    Seq(
-      publishTo := Option(artifactoryResolver(false)),
-      publishArtifact in Compile := true,
-      publishArtifact in Test := true,
-      credentials += artifactoryCredentials
-    )
-
+  val publishSettings: Seq[Setting[_]] = Seq(
+    publishTo := Some(garResolver()), // Use release repo
+    Compile / publishArtifact := true,
+    Test / publishArtifact := true
+  )
+  
   val noPublishSettings: Seq[Setting[_]] =
     Seq(
       publish := {},
