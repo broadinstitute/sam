4cbbb3c5e929cb3dad56e05d0562acf363691398
David An <davidan@broadinstitute.org>
2025-08-04 09:49:25 -0400

    sbt to 1.11.3 (#1707)

--- docker/build_jar.sh
+++ docker/build_jar.sh
@@ -8,7 +8,7 @@ set -e
 # Get the last commit hash of the model directory and set it as an environment variable
 GIT_MODEL_HASH=$(git log -n 1 --pretty=format:%h)
 
-docker run --rm -e GIT_MODEL_HASH=$GIT_MODEL_HASH -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 sbtscala/scala-sbt:eclipse-temurin-17.0.15_6_1.11.1_2.13.16 /working/docker/clean_install.sh /working
+docker run --rm -e GIT_MODEL_HASH=$GIT_MODEL_HASH -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 sbtscala/scala-sbt:eclipse-temurin-17.0.15_6_1.11.3_2.13.16 /working/docker/clean_install.sh /working
 EXIT_CODE=$?
 
 if [ $EXIT_CODE != 0 ]; then
