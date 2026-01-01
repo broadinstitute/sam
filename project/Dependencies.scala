98b20b2a504fce10b375d9a39363a44e8f49ab6c
broadbot <broadbot@broadinstitute.org>
2026-01-01 12:30:57 +0000

    CORE-69: Update google-cloud-nio from 0.128.5 to 0.128.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -115,7 +115,7 @@ object Dependencies {
       excludeWorkbenchModel
     )
   val googleStorageLocal: ModuleID =
-    "com.google.cloud" % "google-cloud-nio" % "0.128.5" % "test" // needed for mocking google cloud storage. Should use same version as wb-libs
+    "com.google.cloud" % "google-cloud-nio" % "0.128.9" % "test" // needed for mocking google cloud storage. Should use same version as wb-libs
 
   val liquibaseCore: ModuleID = "org.liquibase" % "liquibase-core" % "4.33.0"
 
