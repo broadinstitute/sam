01ed0dd69740323af3feeeefff00f4d0c829bf5b
broadbot <broadbot@broadinstitute.org>
2026-01-01 12:31:12 +0000

    CORE-69: Update reactor-netty from 1.2.10 to 1.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
   val akkaHttpTestKit: ModuleID = "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpV % "test"
   val scalaCheck: ModuleID = "org.scalacheck" %% "scalacheck" % scalaCheckV % "test"
 
-  val reactorNetty: ModuleID = "io.projectreactor.netty" % "reactor-netty" % "1.2.10"
+  val reactorNetty: ModuleID = "io.projectreactor.netty" % "reactor-netty" % "1.2.13"
   val nettyAll: ModuleID = "io.netty" % "netty-all" % "4.2.7.Final"
 
   val excludIoGrpc = ExclusionRule(organization = "io.grpc", name = "grpc-core")
