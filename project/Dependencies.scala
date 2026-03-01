4a15a5a67826a4373ade72282ac35780e249ee0f
broadbot <broadbot@broadinstitute.org>
2026-03-01 12:13:43 +0000

    CORE-69: Update snakeyaml from 2.5 to 2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -120,7 +120,7 @@ object Dependencies {
   val liquibaseCore: ModuleID = "org.liquibase" % "liquibase-core" % "4.33.0"
 
   val circeYAML: ModuleID = "io.circe" %% "circe-yaml" % "0.16.1"
-  val snakeYAML: ModuleID = "org.yaml" % "snakeyaml" % "2.5"
+  val snakeYAML: ModuleID = "org.yaml" % "snakeyaml" % "2.6"
 
   val scalikeCore = "org.scalikejdbc" %% "scalikejdbc" % scalikejdbcVersion
   val scalikeCoreConfig = "org.scalikejdbc" %% "scalikejdbc-config" % scalikejdbcVersion
