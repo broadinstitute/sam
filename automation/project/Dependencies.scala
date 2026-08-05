1f786b04553acbb6944115c982bb2ab7dcd5c249
David An <davidan@broadinstitute.org>
2026-08-05 11:07:05 -0400

    bump jackson 2 to 2.22.1

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val scalaV = "2.13"
 
-  val jacksonV = "2.18.3"
+  val jacksonV = "2.22.1"
   val akkaV = "2.6.19"
   val akkaHttpV = "10.2.2"
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val akkaV = "2.6.19"
   val akkaHttpV = "10.2.9"
-  val jacksonV = "2.17.0"
+  val jacksonV = "2.22.1"
   val scalaLoggingV = "3.9.6"
   val scalaTestV = "3.2.19"
   val scalaCheckV = "1.19.0"
@@ -247,8 +247,12 @@ object Dependencies {
   val rootDependencyOverrides = Seq(
     "org.apache.commons" % "commons-compress" % "1.28.0",
     "com.google.guava" % "guava" % "33.5.0-jre", // Force JRE version, not Android version from grpc-core
+    // Jackson 2 and Jackson 3 co-exist. Set versions for both:
     "tools.jackson.core" % "jackson-core" % "3.2.1",
     "tools.jackson.core" % "jackson-databind" % "3.2.1",
+    jacksonCore,
+    jacksonAnnotations,
+    jacksonDatabind,
     // override bouncycastle to address CVE-2026-5598 (requires >= 1.84)
     "org.bouncycastle" % "bcprov-jdk18on" % "1.85",
     "org.bouncycastle" % "bcpkix-jdk18on" % "1.85",
